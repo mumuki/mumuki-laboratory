7100c24f7d66b668c2d7866999cbf252f42b1e18
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-02 20:52:06 -0300

    Adding some style to homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,5 +7,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    redirect_to :back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
 
   get 'auth/:provider/callback', to: 'sessions#create'
+
   get 'logout', to: 'sessions#destroy'
 
 end
