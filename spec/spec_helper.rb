b71574bdf228980c2c23eaf985554ca3435cb9ce
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-07 15:54:49 -0300

    Removing annoying warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require 'minitest/autorun'
 require 'rspec/rails'
 require 'rspec/autorun'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
