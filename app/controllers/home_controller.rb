23cc18a8df4c6c04e94dcadba99585aa1c4fea70
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-08 02:29:47 -0300

    Limiting user submissions and exercises on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
     @latest_exercises = Exercise.last(5).reverse
     @hottest_exercises = Exercise.order(:submissions_count).limit(5)
     if current_user?
-      @my_exercises = current_user.exercises
-      @my_submissions = current_user.submissions
+      @my_exercises = current_user.exercises.first(10)
+      @my_submissions = current_user.submissions.first(10)
     end
   end
 end
