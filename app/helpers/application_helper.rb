d4367621c2bea17261a9bd9c51c23b44548230f3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-07 23:56:49 -0300

    Using images for languages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def highlighted_code(lang, code)
     "<pre><code class=\"hljs #{lang}\">#{code}</code></pre>".html_safe
   end
+
+  def language_image_tag(lang)
+    image_tag language_image_url(lang), alt: lang, height: 16
+  end
+
+  def language_image_url(lang)
+    case lang
+      when 'prolog' then
+        'http://cdn.portableapps.com/SWI-PrologPortable_128.png'
+      when 'haskell' then
+        'https://www.haskell.org/wikistatic/haskellwiki_logo.png'
+    end
+  end
 end
