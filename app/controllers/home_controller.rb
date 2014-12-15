dc350ea0a3854df81c06c12ee2cf0b6266a92fa1
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-15 00:55:08 -0300

    Fix to elements order in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
     @latest_exercises = Exercise.last(5).reverse
     @hottest_exercises = Exercise.order(:submissions_count).limit(5)
     if current_user?
-      @my_exercises = current_user.exercises.first(10).reverse
-      @my_submissions = current_user.submissions.first(10).reverse
+      @my_exercises = current_user.exercises.last(10).reverse
+      @my_submissions = current_user.submissions.last(10).reverse
     end
   end
 end
