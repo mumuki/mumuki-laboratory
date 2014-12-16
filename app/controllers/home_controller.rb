4997bbe9347045b721bc10343a6f221e75bb235b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-16 02:37:39 -0300

    Fixing order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @latest_exercises = Exercise.last(5).reverse
-    @hottest_exercises = Exercise.order(:submissions_count).limit(5)
+    @hottest_exercises = Exercise.order(submissions_count: :desc).limit(5)
     if current_user?
       @my_exercises = current_user.exercises.last(10).reverse
       @my_submissions = current_user.submissions.last(10).reverse
