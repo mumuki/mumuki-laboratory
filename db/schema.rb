58c3b377fe2179a1702714f91d1d7fd52155c22b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-25 18:15:05 -0300

    Adding status to import

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225195510) do
+ActiveRecord::Schema.define(version: 20141225211350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20141225195510) do
     t.integer  "guide_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status",     default: 0
   end
 
   create_table "submissions", force: true do |t|
