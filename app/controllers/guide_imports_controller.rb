b58742c3f08679d3bc65780f147449f3970e6d37
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-26 21:07:30 -0300

    Enabling content negotiation at import controller

--- app/controllers/guide_imports_controller.rb
+++ app/controllers/guide_imports_controller.rb
@@ -1,9 +1,14 @@
 class GuideImportsController < ApplicationController
   before_action :set_guide
 
+  skip_before_filter :verify_authenticity_token
+
+  respond_to :json, :html
+
   def create
-    @guide.imports.create!
-    redirect_to @guide, notice: t(:import_created)
+    @import = @guide.imports.create!
+    flash[:notice] = t(:import_created)
+    respond_with @import, location: @guide
   end
 
   private
