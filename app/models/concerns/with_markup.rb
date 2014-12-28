5516651db076da9a1383be9eba2455b2f4530e45
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-28 18:46:45 -0300

    Allowing fenced code blocks

--- app/models/concerns/with_markup.rb
+++ app/models/concerns/with_markup.rb
@@ -1,5 +1,5 @@
 module WithMarkup
-  @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+  @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true)
 
   def with_markup(text)
     @@markdown.render(text).html_safe
