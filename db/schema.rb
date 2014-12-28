9b61a939c4681d2a768b0e35e2cf577960d030cf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-28 12:17:09 -0300

    Changing description column type. Fixes #32

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226010922) do
+ActiveRecord::Schema.define(version: 20141228151351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "exercises", force: true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.text     "test"
     t.datetime "created_at"
     t.datetime "updated_at"
