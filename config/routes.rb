da6c02e407c9253d532fbc358786a082bfd8146b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-03 16:41:49 -0300

    Some comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,24 @@ Rails.application.routes.draw do
 
   scope '(:locale)' do
     root to: 'home#index'
-
+    # All users
     resources :exercises do
+      # Current user
       resources :submissions, controller: 'exercise_submissions', only: [:new, :create, :show, :index]
     end
 
+    # Current user
     resources :guides, only: [:new, :create, :show, :index] do
+      # Current user
       resources :imports, controller: 'guide_imports', only: [:create]
     end
 
+    # Current user
     resources :submissions, only: :index
 
+    # All users
     resources :users, only: :show do
+      # Nested user
       resources :exercises, controller: 'user_exercises', only: :index
     end
   end
