1670a3eff5832f056458a5160cca38ea06f6fbd5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-03 11:35:19 -0300

    Adding tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,14 +9,25 @@ describe ApplicationHelper do
     it { expect(limit([1, 2, 3], true)).to eq [1, 2, 3] }
   end
 
-  describe '#language_image_tag' do\
-    let(:haskell) { create(:language, name: 'Haskell', image_url: 'https://foo/foo.png')}
+  describe '#language_image_tag' do
+    let(:haskell) { create(:language, name: 'Haskell', image_url: 'https://foo/foo.png') }
     let(:haskell_img_tag) { '<img alt="Haskell" height="16" src="https://foo/foo.png" />' }
     it { expect(language_image_tag(haskell)).to eq haskell_img_tag }
   end
 
-  it '#link_to_exercise'
-  it '#link_to_guide'
-  it '#link_to_github'
-  it '#paginate'
+  describe '#link_to_exercise' do
+    let(:exercise) { create(:exercise, title: 'foo', id: 1) }
+    it { expect(link_to_exercise(exercise)).to eq '<a href="/exercises/1">foo</a>' }
+  end
+
+  describe '#link_to_guide' do
+    let(:guide) { create(:guide, name: 'foo', id: 1 ) }
+    it { expect(link_to_guide(guide)).to eq '<a href="/guides/1">foo</a>' }
+  end
+
+  describe '#link_to_github' do
+    let(:guide) { create(:guide, github_repository: 'foo/bar') }
+    it { expect(link_to_github(guide)).to eq '<a href="https://github.com/foo/bar">foo/bar</a>' }
+  end
+
 end
