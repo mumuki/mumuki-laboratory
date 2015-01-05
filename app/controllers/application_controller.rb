a97ae99553d2c6f33fa693ed00710e95312c2453
Agustin Pina <aguspina87@gmail.com>
2015-01-05 20:32:54 -0300

    Add flags to chose locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
                 :restricted_to_author, :restricted_to_current_user
 
   def set_locale
-    I18n.locale = extract_locale_from_accept_language_header || params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
   end
 
   def default_url_options
