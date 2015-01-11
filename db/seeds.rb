ee480f4e8a73c1a8eda8292f2e04ba8b2a6c985f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-11 14:41:01 -0300

    Adding Ruby seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@ Language.create!(name: 'Prolog',
                  test_runner_url: 'http://mumuki-plunit-server.herokuapp.com',
                  extension: 'pl',
                  image_url: 'http://cdn.portableapps.com/SWI-PrologPortable_128.png')
+
+Language.create!(name: 'Ruby',
+                 test_runner_url: 'http://mumuki-rspec-server.herokuapp.com/',
+                 extension: 'rb',
+                 image_url: 'https://www.ruby-lang.org/images/header-ruby-logo.png')
