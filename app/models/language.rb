e3c58c22a1a8872c49cba0ae36426ae7af7ab2f1
Agustin Pina <aguspina87@gmail.com>
2015-01-12 16:25:41 -0300

    Refactor usin WithAuthor module

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,7 +1,8 @@
 require 'rest_client'
 
 class Language < ActiveRecord::Base
-  belongs_to :plugin_author, class_name: 'User'
+  include WithAuthor
+  belongs_to :author, class_name: 'User'
 
   validates_presence_of :name, :test_runner_url, :extension, :image_url
 
@@ -14,14 +15,6 @@ class Language < ActiveRecord::Base
 
   def created_by? user
     user.id == plugin_author.id
-  end
-
-  def can_edit? user
-    self.created_by? user
-  end
-
-  def can_destroy? user
-    self.created_by? user
   end
 
   def to_s
