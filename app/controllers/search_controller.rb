45e6169c184119b56f2ada836fe9b517ee530e55
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-17 23:41:21 -0300

    Showing query in search box

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def show
-    @results = paginated Exercise.by_full_text(params[:q]).order(submissions_count: :desc)
+    @q = params[:q]
+    @results = paginated Exercise.by_full_text(@q).order(submissions_count: :desc)
   end
 end
