342a001a6b9e141f0dc1e88472b5bf56b9476e33
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-15 13:18:53 -0300

    Adding test about searching by guide

--- spec/features/search_flow_spec.rb
+++ spec/features/search_flow_spec.rb
@@ -7,11 +7,11 @@ feature 'Search Flow' do
     create(:exercise, tag_list: [], title: 'Bar', language: haskell)
     create(:exercise, tag_list: [], title: 'haskelloid')
     create(:exercise, tag_list: [], title: 'Baz', description: 'do it in haskell')
-    create(:exercise, tag_list: [], title: 'nothing')
-
+    create(:exercise, tag_list: [], title: 'nothing', guide: guide)
   }
+  let(:guide) { create(:guide, name: 'awesomeGuide', description: 'Haskelloid baz guide') }
 
-  scenario 'search from home' do
+  scenario 'search from home, by language' do
     visit '/'
 
     within('.jumbotron') do
@@ -29,4 +29,19 @@ feature 'Search Flow' do
     expect(page).to_not have_text('nothing')
 
   end
+
+  scenario 'search by guide' do
+    visit '/en/exercises'
+
+    fill_in 'q', with: 'awesomeGuide'
+    click_on 'Search'
+
+    expect(page).to have_text('Title')
+    expect(page).to_not have_text('Bar')
+    expect(page).to_not have_text('Baz')
+    expect(page).to_not have_text('Foo')
+    expect(page).to_not have_text('haskelloid')
+    expect(page).to have_text('nothing')
+  end
+
 end
