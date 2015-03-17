81df8da5e6c4e708a6908a6210acb6ec76cd4896
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-17 20:05:05 -0300

    Fixing permitted paras for exercise admin

--- app/admin/exercise.rb
+++ app/admin/exercise.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Exercise do
-  
+
+  permit_params :author_id, :language, :guide_id, :title, :description, :test, :hint, :locale
+
   filter :title
   filter :locale
   filter :created_at
@@ -8,9 +10,9 @@ ActiveAdmin.register Exercise do
   index do
     column(:id)
     column(:title)
-    column(:language){|ex| I18n.t("#{ex.language.name}")}
+    column(:language) { |ex| I18n.t("#{ex.language.name}") }
     column(:submissions_count)
-    column(:author_name){|ex| ex.author.name}
+    column(:author_name) { |ex| ex.author.name }
     column(:locale)
 
     actions
