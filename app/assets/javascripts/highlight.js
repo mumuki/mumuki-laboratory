ef26610c40ac762233c218ffc27b61d04d09e646
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-21 21:17:59 -0300

    Fix: disabling highlight js

--- app/assets/javascripts/highlight.js
+++ app/assets/javascripts/highlight.js
@@ -1,6 +1,1 @@
-$(document).on('page:change',  function() {
-  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
-});
-$(document).on('page:restore', function() {
-  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
-});
+//hljs.initHighlightingOnLoad();
