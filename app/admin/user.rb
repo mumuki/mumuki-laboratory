1023899e06cc9d03a7edc0004cb66b4153c20af9
Agustin Pina <aguspina87@gmail.com>
2015-03-27 19:52:10 -0300

    Change column

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,9 +4,7 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
+    column :name
     column :created_at
     column (:submissions_count) {|user| Submission.where(submitter_id: user.id).count}
     column (:exercises_count) {|user| user.exercises.count}
@@ -14,14 +12,14 @@ ActiveAdmin.register User do
     actions
   end
 
-  filter :email
+  filter :name
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
 
   form do |f|
     f.inputs "User Details" do
-      f.input :email
+      f.input :name
       f.input :password
       f.input :password_confirmation
     end
