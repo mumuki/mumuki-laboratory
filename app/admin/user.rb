a4281c37954abd50c76ffab13e4cb79770fbc65e
Agustin Pina <aguspina87@gmail.com>
2015-03-26 22:07:24 -0300

    Add user to active admin

---
+++ app/admin/user.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    column :submissions_count {|user| Submissions.where(submitter_id: user.id).count}
+    column :exercises_count {|user| user.exercises.count}
+    column :guides_count {|user| user.guides.count}
+    actions
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+end
