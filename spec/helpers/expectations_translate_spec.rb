799c1e62eae8a552396e9b10eb5d5c266484cb8f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-02 22:28:06 -0300

    Making tests that use local repetible

--- spec/helpers/expectations_translate_spec.rb
+++ spec/helpers/expectations_translate_spec.rb
@@ -3,9 +3,22 @@ require 'spec_helper'
 describe ExpectationsTranslate do
   helper ExpectationsTranslate
 
-  it { expect(t_expectation_result('foo', 'HasBinding')).to eq('foo must be defined') }
-  it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('foo must not use baz') }
-  it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('foo must not use lambda expressions') }
+  context 'en locale' do
+    before { I18n.locale = :en }
+
+    it { expect(t_expectation_result('foo', 'HasBinding')).to eq('foo must be defined') }
+    it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('foo must not use baz') }
+    it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('foo must not use lambda expressions') }
+  end
+
+  context 'es locale' do
+    before { I18n.locale = :es }
+
+    it { expect(t_expectation_result('foo', 'HasBinding')).to eq('foo debe estar definido') }
+    it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('foo no debe definir baz') }
+    it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('foo no debe usar expresiones lambda') }
+  end
+
 
   it { expect(parse_inspection('HasBinding')).to eq(inspection: 'HasBinding', type: 'must') }
   it { expect(parse_inspection('Not:HasBinding')).to eq(inspection: 'HasBinding', type: 'must_not') }
