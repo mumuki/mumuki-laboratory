7c555dbc7814a54a707df955e58b0c34fcc2967b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-04 15:55:55 -0300

    Adding stats methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ActiveRecord::Base
   has_many :exercises, foreign_key: :author_id
   has_many :guides, foreign_key: :author_id
 
+  has_many :submitted_exercises,
+           -> { uniq },
+           through: :submissions,
+           class_name: 'Exercise',
+           source: :exercise
+
+  has_many :solved_exercises,
+           -> { where('submissions.status' => Submission.passed_status).uniq },
+           through: :submissions,
+           class_name: 'Exercise',
+           source: :exercise
+
   def self.omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
@@ -15,5 +27,33 @@ class User < ActiveRecord::Base
       end
       user.save!
     end
+  end
+
+  def last_submission_date
+    submissions.last.try(&:created_at)
+  end
+
+  def submissions_count
+    submissions.count
+  end
+
+  def passed_submissions_count
+    submissions.where(status: Submission.passed_status).count
+  end
+
+  def submitted_exercises_count
+    submitted_exercises.count
+  end
+
+  def solved_exercises_count
+    solved_exercises.count
+  end
+
+  def submissions_success_rate
+    "#{passed_submissions_count}/#{submissions_count}"
+  end
+
+  def exercises_success_rate
+    "#{solved_exercises_count}/#{submitted_exercises_count}"
   end
 end
