e159c14aaff5cfe53d16bf771cff8d92903aefba
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-04 16:51:56 -0300

    Refactor: extracting omniauth to its own module

---
+++ app/models/concerns/with_omniauth.rb
@@ -0,0 +1,19 @@
+module WithOmniauth
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def omniauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.nickname
+        user.email = auth.info.email
+        user.token = auth.credentials.token
+        auth.credentials.expires_at.try do |expiration|
+          user.expires_at = Time.at(expiration)
+        end
+        user.save!
+      end
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   }
 
   include WithSearch
+  include WithOmniauth
 
   has_many :submissions, foreign_key: :submitter_id
   has_many :exercises, foreign_key: :author_id
@@ -23,20 +24,6 @@ class User < ActiveRecord::Base
            through: :submissions,
            class_name: 'Exercise',
            source: :exercise
-
-  def self.omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.nickname
-      user.email = auth.info.email
-      user.token = auth.credentials.token
-      auth.credentials.expires_at.try do |expiration|
-        user.expires_at = Time.at(expiration)
-      end
-      user.save!
-    end
-  end
 
   def last_submission_date
     submissions.last.try(&:created_at)
