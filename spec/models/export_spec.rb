213437384b49ee1a3300661d9781b4d7e9519d19
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-07 23:19:10 -0300

    Adding smoke test

--- spec/models/export_spec.rb
+++ spec/models/export_spec.rb
@@ -1,10 +1,32 @@
 require 'spec_helper'
 
 describe Export do
-  let(:guide) { create(:guide, ) }
   let!(:exercise_1) { create(:exercise, guide: guide, title: 'foo') }
   let!(:exercise_2) { create(:exercise, guide: guide, title: 'bar') }
 
-  it { guide.exports.create! }
+
+  describe '#status' do
+    let(:guide) { create(:guide) }
+    let(:export) { guide.exports.create! }
+
+    it { expect(export.status).to eq 'pending' }
+  end
+
+  describe '#run_export!' do
+    let(:guide) { create(:guide, github_repository: 'flbulgarelli/never-existent-repo') }
+    let(:export) { guide.exports.create! }
+
+    before do
+      begin
+        export.run_export!
+      rescue
+      end
+    end
+
+    it do
+      expect(export.status).to eq 'failed'
+      expect(export.result).to eq 'Repository is private or does not exist'
+    end
+  end
 
 end
