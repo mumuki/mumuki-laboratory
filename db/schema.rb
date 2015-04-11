b25114db37aacefd06e78556fb6851632499659e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-11 18:56:27 -0300

    Change guide descripton to text
    Fixes #122

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406020152) do
+ActiveRecord::Schema.define(version: 20150411215351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 20150406020152) do
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "description"
+    t.text     "description"
   end
 
   add_index "guides", ["author_id"], name: "index_guides_on_author_id", using: :btree
