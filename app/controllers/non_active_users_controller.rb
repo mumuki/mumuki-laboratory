615accdaa7f66fdf5ba73cb8f7c2679b71aab1d5
iluetich <ivan.luetich93@gmail.com>
2015-04-17 21:08:07 -0300

    Fixing deliver method since deliver_later and _now are deprecated.

--- app/controllers/non_active_users_controller.rb
+++ app/controllers/non_active_users_controller.rb
@@ -1,12 +1,12 @@
 class NonActiveUsersController < ApplicationController
 
-	def send_email_non_active_users
+	def self.send_email_non_active_users
 		users = User.where(" created_at < :date ", date: 30.days.ago)
 		users.each do |user|
 			if user.has_submissions?
-				UserMail.motivation_email(user).deliver_later
+				UserMailer.motivation_email(user).deliver
 			end
-		end			
+		end	
 	end
 	
 end
