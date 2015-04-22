163885c79876c13dfad8aa82feeaebc7ad49defe
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-22 12:22:30 -0300

    Extracting method

--- app/models/concerns/with_omniauth.rb
+++ app/models/concerns/with_omniauth.rb
@@ -4,17 +4,21 @@ module WithOmniauth
   module ClassMethods
     def omniauth(auth)
       where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.name = auth.info.nickname
-        user.email = auth.info.email
-        user.image_url = auth.info.image
-        user.token = auth.credentials.token
+        extract_profile!(auth, user)
         auth.credentials.expires_at.try do |expiration|
           user.expires_at = Time.at(expiration)
         end
         user.save!
       end
+    end
+
+    def extract_profile!(auth, user)
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.nickname
+      user.email = auth.info.email
+      user.image_url = auth.info.image
+      user.token = auth.credentials.token
     end
   end
 end
