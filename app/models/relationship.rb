104c2cf5b255d1914f9936024bc8c3df1615f5bf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-07 18:58:11 -0300

    Remove relationships

--- app/models/relationship.rb
+++
@@ -1,7 +0,0 @@
-class Relationship < ActiveRecord::Base
-  belongs_to :follower, class_name: "User"
-  belongs_to :followed, class_name: "User"
-  
-  validates :follower_id, presence: true
-  validates :followed_id, presence: true
-end
