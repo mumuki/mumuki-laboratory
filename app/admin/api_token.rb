4bb555895091d38e5fdcbdfddbd91880fabe746e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-14 18:29:18 -0300

    Fixing admin permitted_params

--- app/admin/api_token.rb
+++ app/admin/api_token.rb
@@ -1,15 +1,16 @@
 ActiveAdmin.register ApiToken do
 
-  permit_params :description
+  permit_params :name, :value, :description
 
+  filter :name
   filter :value
   filter :description
-
   filter :created_at
   filter :updated_at
 
   index do
     column(:id)
+    column(:name)
     column(:value)
     column(:description)
 
