609c1decebf75000d9fadd62873e13aa66b0b777
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-18 15:21:21 -0300

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512235847) do
+ActiveRecord::Schema.define(version: 20150518174912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 20150512235847) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "visible_success_output", default: false
   end
 
   add_index "languages", ["extension"], name: "index_languages_on_extension", unique: true, using: :btree
