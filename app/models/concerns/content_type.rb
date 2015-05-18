78cd1885af5e5a73e5b784ebd0c9b191cc5390a3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-18 15:44:06 -0300

    Refactor: Converting Markdown into a ContentType

--- app/models/concerns/content_type.rb
+++ app/models/concerns/content_type.rb
@@ -5,13 +5,21 @@ module ContentType
 
   module Html
     def self.render(content)
-      content.html_safe
+      content.html_safe if content
     end
   end
 
   module Plain
     def self.render(content)
       content
+    end
+  end
+
+  module Markdown
+    @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true)
+
+    def self.render(content)
+      @@markdown.render(content).html_safe if content
     end
   end
 end
--- spec/models/content_type_spec.rb
+++ spec/models/content_type_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe ContentType do
-  it { expect(ContentType.for(:html).render('foo').html_safe?).to be true }
+  it { expect(ContentType.for(:html).render('<h1>foo</h1>').html_safe?).to be true }
   it { expect(ContentType.for(:plain).render('foo').html_safe?).to be false }
+  it { expect(ContentType.for(:markdown).render('#foo').html_safe?).to be true }
 end
