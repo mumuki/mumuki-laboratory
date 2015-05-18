7b3ab181ee8351d36e9327e4bb26b04fd2ac2f42
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2015-05-18 07:42:04 +0000

    Adding export logic

--- app/models/export.rb
+++ app/models/export.rb
@@ -39,7 +39,7 @@ class Export < RepositoryOperation
     write_file!(dirname, 'meta.yml', metadata_yaml(e))
 
     write_file!(dirname, 'hint.md', e.hint) if e.hint.present?
-    write_file!(dirname, "extra.#{language.extension}", e[:extra_code]) if e[:extra_code].present?
+    write_file!(dirname, extra_filename, e[:extra_code]) if e[:extra_code].present?
     write_file!(dirname, 'expectations.yml', expectations_yaml(e)) if e.expectations.present?
     write_file!(dirname, 'corollary.md', e.corollary) if e.corollary.present?
 
@@ -63,7 +63,7 @@ class Export < RepositoryOperation
   end
 
   def write_extra!(dir)
-    write_file!(dir, format_extension('extra'), guide.extra_code) if guide.extra_code.present?
+    write_file!(dir, extra_filename, guide.extra_code) if guide.extra_code.present?
   end
 
   private
@@ -76,10 +76,11 @@ class Export < RepositoryOperation
     {'expectations' => e.expectations.as_json(only: [:binding, :inspection])}.to_yaml
   end
 
+  def extra_filename
+    "extra.#{language.extension}"
+  end
 
   def write_file!(dirname, name, content)
     File.write(File.join(dirname, name), content)
   end
-
-
 end
