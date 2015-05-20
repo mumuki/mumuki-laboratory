4bd4b995ee08c8a8d7d5c6be5964c4601362ff22
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-20 02:42:15 -0300

    Supporting Path edition in admin

--- app/admin/guide.rb
+++ app/admin/guide.rb
@@ -1,15 +1,18 @@
 ActiveAdmin.register Guide do
 
-  permit_params :name, :description, :github_repository
+  permit_params :name, :description, :github_repository, :position, :path_id
 
   filter :name
   filter :description
+  filter :path
   filter :created_at
   filter :updated_at
 
   index do
     column(:id)
     column(:name)
+    column(:path)
+    column(:position)
     column(:description)
     column(:github_repository)
 
--- app/models/path.rb
+++ app/models/path.rb
@@ -3,4 +3,8 @@ class Path < ActiveRecord::Base
 
   belongs_to :category
   belongs_to :language
+
+  def to_s
+    "#{language.name}/#{category.name}"
+  end
 end
