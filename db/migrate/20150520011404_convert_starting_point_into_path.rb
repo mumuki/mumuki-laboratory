5a58ecd810c2b63f6b03059b64e0312b33b81537
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-19 22:51:11 -0300

    Migrating schema

---
+++ db/migrate/20150520011404_convert_starting_point_into_path.rb
@@ -0,0 +1,12 @@
+class ConvertStartingPointIntoPath < ActiveRecord::Migration
+  def change
+    rename_table :starting_points, :paths
+
+    remove_column :paths, :guide_id
+
+    drop_table :suggested_guides
+
+    add_column :guides, :path_id, :integer, index: true
+    add_column :guides, :position, :integer
+  end
+end
