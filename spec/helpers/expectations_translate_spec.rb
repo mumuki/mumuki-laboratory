f22dc9c8ff8624ad6f27926452de3e92df8e0467
Federico Aloi <federico.aloi@gmail.com>
2015-06-10 01:06:12 -0300

    Bindings and targets are sorrounded by <strong>

--- spec/helpers/expectations_translate_spec.rb
+++ spec/helpers/expectations_translate_spec.rb
@@ -6,16 +6,16 @@ describe ExpectationsTranslate do
   context 'en locale' do
     before { I18n.locale = :en }
 
-    it { expect(t_expectation_result('foo', 'HasBinding')).to eq('foo must be defined') }
-    it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('foo must not use baz') }
-    it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('foo must not use lambda expressions') }
+    it { expect(t_expectation_result('foo', 'HasBinding')).to eq('<strong>foo</strong> must be defined') }
+    it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('<strong>foo</strong> must not use <strong>baz</strong>') }
+    it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('<strong>foo</strong> must not use lambda expressions') }
   end
 
   context 'es locale' do
     before { I18n.locale = :es }
 
-    it { expect(t_expectation_result('foo', 'HasBinding')).to eq('foo debe estar definido') }
-    it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('foo no debe utilizar baz') }
-    it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('foo no debe emplear expresiones lambda') }
+    it { expect(t_expectation_result('foo', 'HasBinding')).to eq('<strong>foo</strong> debe estar definido') }
+    it { expect(t_expectation_result('foo', 'Not:HasUsage:baz')).to eq('<strong>foo</strong> no debe utilizar <strong>baz</strong>') }
+    it { expect(t_expectation_result('foo', 'Not:HasLambda')).to eq('<strong>foo</strong> no debe emplear expresiones lambda') }
   end
 end
