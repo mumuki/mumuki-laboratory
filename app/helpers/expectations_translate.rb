c3278e53a81082a05d8565c04f5b92f2d39419a4
Federico Aloi <federico.aloi@gmail.com>
2015-06-10 02:23:47 -0300

    expectations_result returns raw html

--- app/helpers/expectations_translate.rb
+++ app/helpers/expectations_translate.rb
@@ -14,7 +14,7 @@ module ExpectationsTranslate
 
   def t_expectation_result(binding, inspection)
     inspection = Mumukit::Inspection.parse inspection
-    t "expectation_#{inspection.type}", binding: "<strong>#{binding}</strong>", target: "<strong>#{inspection.target}</strong>", must: t_must(inspection)
+    raw t "expectation_#{inspection.type}", binding: "<strong>#{binding}</strong>", target: "<strong>#{inspection.target}</strong>", must: t_must(inspection)
   end
 
   def t_must(parsed)
