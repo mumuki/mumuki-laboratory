e37e21114bbf9293daf73cf80ce4eaae0d856486
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-14 16:27:43 -0300

    Setting last exercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,11 @@ describe User do
       it { expect(user.submissions_count).to eq 0 }
       it { expect(user.passed_submissions_count).to eq 0 }
       it { expect(user.has_submissions?).to eq false}
+      it { expect(user.last_exercise).to be_nil }
+      it { expect(user.last_guide).to be_nil }
     end
 
-    context 'when there are submissions' do
+    context 'when there are submissions from orphan exercise' do
       let!(:last_submission) do
         user.submissions.create!(exercise: exercise_1, status: :failed, content: '')
         user.submissions.create!(exercise: exercise_1, status: :passed, content: '')
@@ -31,6 +33,21 @@ describe User do
       it { expect(user.submissions_count).to eq 4 }
       it { expect(user.passed_submissions_count).to eq 2 }
       it { expect(user.has_submissions?).to eq true}
+      it { expect(user.last_exercise).to eq exercise_3 }
+      it { expect(user.last_guide).to be_nil }
+    end
+
+
+    context 'when there are submissions from a non orphan guide' do
+      let(:guide) { create(:guide) }
+      let!(:exercise_4) { create(:exercise, guide: guide) }
+
+      let!(:last_submission) do
+        user.submissions.create!(exercise: exercise_4, status: :failed, content: '')
+      end
+
+      it { expect(user.last_exercise).to eq exercise_4 }
+      it { expect(user.last_guide).to eq guide }
     end
   end
 end
