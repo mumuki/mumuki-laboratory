6e79f79e4e39c713b68ae121bde8522fbe14bc88
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-14 16:28:11 -0300

    Redirecting to last guide

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
 
+  before_action :redirect_to_last_guide
+
   def index
+  end
+
+  private
+
+  def redirect_to_last_guide
+    redirect_to current_user.last_guide if visitor_recurrent?
+  end
+
+  def visitor_recurrent?
+    current_user? && current_user.last_guide.present? && visitor_comes_from_internet?
+  end
+
+  def visitor_comes_from_internet?
+    !Addressable::URI.parse(request.referer).host.include? 'mumuki'
   end
 end
