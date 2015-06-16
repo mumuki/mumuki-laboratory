0a59fd1f45a2e9ea2c97cb430975293f7ab8e547
jvillarejo <arzivian87@gmail.com>
2015-06-16 02:49:27 -0300

    added exercise-assignment component style

--- app/assets/javascripts/application/collapse.js
+++ app/assets/javascripts/application/collapse.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#collapsible-width").on("click", function() {
-    $(".exercise-description").toggleClass("hidden");
+    $(".exercise-assignment").toggleClass("hidden");
     $(".ace-editor-col").toggleClass("col-md-6");
     $(".ace-editor-col").toggleClass("col-md-12");
   })  
