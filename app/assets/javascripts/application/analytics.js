92fa1ad1c60d75d7d42d3645639a2ba86708ad79
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-19 15:15:27 -0300

    Making analytics works with turbolinks

--- app/assets/javascripts/application/analytics.js
+++ app/assets/javascripts/application/analytics.js
@@ -3,5 +3,8 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-58353823-1', 'auto');
-ga('send', 'pageview');
+$(document).on('ready page:load', function(){
+  ga('create', 'UA-58353823-1', 'auto');
+  ga('send', 'pageview');
+  console.log('pageview sent')
+});
