e03da9fd42885467b15de6b0cbdf65ab5e384147
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-26 11:05:21 -0300

    Adding navbar
    Fixes #198

--- app/models/concerns/with_path.rb
+++ app/models/concerns/with_path.rb
@@ -9,4 +9,12 @@ module WithPath
   def next_guides
    path ? path.guides.where('guides.position > :position', position: position).order('guides.position asc') : []
   end
+
+  def next
+    path.guides.where(position: position + 1).first if path #FIXME duplicated in WithGuide
+  end
+
+  def previous
+    path.guides.where(position: position - 1).first if path
+  end
 end
