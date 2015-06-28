a2ebd923089f976b3fbabe2f6bae5995f6eafadd
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-28 20:04:40 -0300

    Removing unused selectors

--- app/controllers/exercise_submissions_controller.rb
+++ app/controllers/exercise_submissions_controller.rb
@@ -1,9 +1,9 @@
 class ExerciseSubmissionsController < ApplicationController
   before_action :authenticate!
 
-  before_action :set_submission, only: [:show, :status, :results]
-  before_action :set_exercise, only: [:create, :show, :index, :results]
-  before_action :set_guide, only: [:show, :results]
+  before_action :set_submission, only: [:show]
+  before_action :set_exercise, only: [:create, :show, :index]
+  before_action :set_guide, only: [:show]
 
   def index
     @submissions = paginated @exercise.submissions_for(current_user).order(created_at: :desc)
