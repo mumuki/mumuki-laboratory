281dbe509526dc34f1d8872e385d96ce0560fcb3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-03 15:16:46 -0300

    Extracting test_results_rendering file

--- spec/helpers/test_results_rendering_spec.rb
+++ spec/helpers/test_results_rendering_spec.rb
@@ -1,16 +1,6 @@
 require 'spec_helper'
 require 'ostruct'
 
-module TestResultsRendering
-  def render_test_results(submission)
-    if submission.test_results.present?
-      render partial: 'layouts/test_results', locals: {test_results: submission.test_results}
-    else
-      submission.result_html
-    end
-  end
-end
-
 describe TestResultsRendering do
   helper Icons
   helper TestResultsRendering
