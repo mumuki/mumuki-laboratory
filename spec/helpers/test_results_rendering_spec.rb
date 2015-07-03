1d0cef3b452bc945a38a5d00789eb9b154a92da4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-03 15:39:11 -0300

    Rendering example's results

--- spec/helpers/test_results_rendering_spec.rb
+++ spec/helpers/test_results_rendering_spec.rb
@@ -6,18 +6,29 @@ describe TestResultsRendering do
   helper TestResultsRendering
 
   let(:html) { render_test_results submission }
-  let(:html_s) { html.strip }
 
   context 'structured results' do
     context 'when single passed submission' do
       let(:submission) { OpenStruct.new(test_results: [{title: '2 is 2', status: :passed, result: ''}]) }
       it { expect(html).to be_html_safe }
-      it { expect(html_s).to eq "<i class=\"fa fa-check text-success special-icon\"></i>\n  <span class=\"test-result-title\">2 is 2</span>" }
+      it { expect(html).to include "<i class=\"fa fa-check text-success special-icon\"></i>" }
+      it { expect(html).to include "<span class=\"example-title\">2 is 2</span>" }
     end
 
     context 'when single failed submission' do
-      let(:submission) { OpenStruct.new(test_results: [{title: '2 is 2', status: :failed, result: ''}]) }
-      it { expect(html_s).to eq "<i class=\"fa fa-times text-danger special-icon\"></i>\n  <strong class=\"test-result-title\">2 is 2</strong>" }
+      context 'when plain results' do
+        let(:submission) { OpenStruct.new(test_results: [{title: '2 is 2', status: :failed, result: 'something _went_ wrong'}]) }
+        it { expect(html).to include "<i class=\"fa fa-times text-danger special-icon\"></i>" }
+        it { expect(html).to include "<strong class=\"example-title\">2 is 2</strong>" }
+        it { expect(html).to include "<pre><code>something _went_ wrong</code></pre>" }
+      end
+
+      context 'when markdown results' do
+        let(:submission) { OpenStruct.new(test_results: [{title: '2 is 2', status: :failed, result: 'something went _really_ wrong'}]) }
+        it { expect(html).to include "<i class=\"fa fa-times text-danger special-icon\"></i>" }
+        it { expect(html).to include "<strong class=\"example-title\">2 is 2</strong>" }
+        it { expect(html).to include "<pre><code>something <em>really</em> wrong</code></pre>" }
+      end
     end
   end
 
@@ -25,6 +36,6 @@ describe TestResultsRendering do
     let(:submission) { OpenStruct.new(result_html: '<pre>ooops, something went wrong</pre>'.html_safe) }
 
     it { expect(html).to be_html_safe }
-    it { expect(html_s).to eq '<pre>ooops, something went wrong</pre>' }
+    it { expect(html).to eq '<pre>ooops, something went wrong</pre>' }
   end
 end
