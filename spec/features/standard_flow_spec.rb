716f2c4572dbd4538b36144270db49f0b1efa37f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-08 18:48:37 -0300

    Adding test for multi-path categories

--- spec/features/standard_flow_spec.rb
+++ spec/features/standard_flow_spec.rb
@@ -3,27 +3,51 @@ require 'spec_helper'
 feature 'Standard Flow' do
   let(:haskell) { create(:haskell) }
   let!(:exercises) {
-    create(:exercise, title: 'Succ',        guide: guide, position: 1, description: 'Description of foo')
+    create(:exercise, title: 'Succ', guide: guide, position: 1, description: 'Description of foo')
   }
   let!(:category) { create(:category, name: 'Functional Programming') }
   let!(:path) { create(:path, category: category, language: haskell) }
   let!(:guide) { create(:guide, name: 'getting-started', description: 'An awesome guide',
                         language: haskell, position: 1, path: path) }
 
-  scenario 'do a guide for first time, starting from home' do
+
+  before do
     visit '/'
+  end
 
-    within('.jumbotron') do
+  context 'single path' do
+    scenario 'do a guide for first time, starting from home' do
+      within('.jumbotron') do
+        click_on 'Start Practicing!'
+      end
+
+      within('.category-panel') do
+        click_on 'Start Practicing!'
+      end
+
       click_on 'Start Practicing!'
+      expect(page).to have_text('Succ')
     end
+  end
 
-    within('.category-panel') do
-      click_on 'haskell'
+  context 'multiple paths' do
+    let(:js) { create(:language, name: 'js') }
+    let!(:path_js) { create(:path, category: category, language: js) }
+    let!(:guide_js) { create(:guide, name: 'getting-started-js', description: 'An awesome JS guide',
+                             language: js, position: 1, path: path_js) }
+
+    scenario 'do a guide for first time, starting from home' do
+      within('.jumbotron') do
+        click_on 'Start Practicing!'
+      end
+
+      within('.category-panel') do
+        click_on 'haskell'
+      end
+
+      click_on 'Start Practicing!'
+      expect(page).to have_text('Succ')
     end
-
-    click_on 'Start Practicing!'
-
-    expect(page).to have_text('Succ')
   end
 
 
