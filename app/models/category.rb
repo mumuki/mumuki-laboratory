c17075550ec22518678f69bddebfd743f72fb007
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-08 18:28:12 -0300

    Render category description with markdown

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ActiveRecord::Base
-
   include WithLocale
+  include WithMarkup
 
   has_many :paths
 
   validates_presence_of :name, :description, :image_url
+
+  markup_on :description
 end
