02eab32cc08bc08a89c767a5ba3fff11d888b4bc
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-07 21:48:30 -0300

    Extracting concern

---
+++ app/helpers/concerns/with_stats_rendering.rb
@@ -0,0 +1,13 @@
+module WithStatsRendering
+  def stats(stats, k)
+    "#{stats.send k} #{status_icon(k)} ".html_safe
+  end
+
+  def practice_key_for(stats)
+    if stats && stats.started?
+      :continue_practicing
+    else
+      :start_practicing
+    end
+  end
+end
--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -1,15 +1,3 @@
 module GuidesHelper
-#FIXME extract to concern
-  def stats(stats, k)
-    "#{stats.send k} #{status_icon(k)} ".html_safe
-  end
-
-  def practice_key_for(stats)
-    if stats && stats.started?
-      :continue_practicing
-    else
-      :start_practicing
-    end
-  end
-
+  include WithStatsRendering
 end
