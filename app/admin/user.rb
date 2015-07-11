4d3961d81580717a08792db49576d62d9fd045de
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-11 18:32:17 -0300

    Removing user mailer

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,11 +1,6 @@
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
-  collection_action :notify_non_active, method: :post do
-    User.inactive.each { |user| UserMailer.motivation_email(user).deliver_later }
-    redirect_to collection_path, notice: 'Non active users notified'
-  end
-
   index do
     selectable_column
     id_column
@@ -16,10 +11,6 @@ ActiveAdmin.register User do
     column (:exercises_count) {|user| user.exercises.count}
     column (:guides_count) {|user| user.guides.count}
     actions
-  end
-
-  action_item :notify_non_active_users do
-    link_to 'Send email to non active users', notify_non_active_admin_users_path, method: :post
   end
 
   filter :name
