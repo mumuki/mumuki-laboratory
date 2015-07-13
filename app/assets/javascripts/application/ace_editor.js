6f04d24d3afca9357d395873ab723fe36255b851
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-13 09:30:08 -0300

    Supporting fixed language editors

--- app/assets/javascripts/application/ace_editor.js
+++ app/assets/javascripts/application/ace_editor.js
@@ -1,8 +1,14 @@
-var editors = [];
+var dynamicEditors = [];
 
 function setupAceEditors() {
-  $(".editor").each(function(index, textarea){
-    editors.push(setupAceEditor(textarea));
+  $(".editor").each(function (index, textarea) {
+    var editor = setupAceEditor(textarea);
+    var language = $(textarea).data('editor-language');
+    if (language) {
+      setEditorLanguage(editor, language);
+    } else {
+      dynamicEditors.push(editor);
+    }
   });
 }
 
@@ -17,7 +23,7 @@ function setupAceEditor(textarea) {
   });
   editor.setFontSize(13);
 
-  form.addEventListener("submit", function() {
+  form.addEventListener("submit", function () {
     textarea.style.visibility = "hidden";
     textarea.value = editor.getValue();
     form.appendChild(textarea)
@@ -33,13 +39,17 @@ function onSelectUpdateAceEditor() {
 function updateAceEditorLanguage() {
   var language = $("#exercise_language_id").find(":selected").html() || $('#exercise_language').val();
   if (language !== undefined) {
-    editors.forEach(function (e) {
-      e.getSession().setMode("ace/mode/" + language.toLowerCase())
+    dynamicEditors.forEach(function (e) {
+      setEditorLanguage(e, language);
     })
   }
 }
 
-$(document).on('ready page:load', function(){
+function setEditorLanguage(editor, language) {
+  editor.getSession().setMode("ace/mode/" + language.toLowerCase())
+}
+
+$(document).on('ready page:load', function () {
   setupAceEditors();
   updateAceEditorLanguage();
   onSelectUpdateAceEditor();
