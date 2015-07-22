cc8e92887e3053f8677dea6794c09d8c95a2a49a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-21 21:44:38 -0300

    Notifying subscribers

--- app/models/concerns/with_test_running.rb
+++ app/models/concerns/with_test_running.rb
@@ -3,6 +3,7 @@ module WithTestRunning
     run_update! do
       language.run_tests!(new_test_server_request).except(:response_type)
     end
+    EventSubscriber.notify_submission!(self)
   end
 
   def new_test_server_request
--- app/models/event_subscriber.rb
+++ app/models/event_subscriber.rb
@@ -1,2 +1,20 @@
 class EventSubscriber < ActiveRecord::Base
+
+  def notify_submission!(submission)
+    response = JSON.parse(
+        RestClient.post(
+            "#{url}/events/submissions",
+            submission.to_json,
+            content_type: :json))
+
+    if response != {status: 'ok'}
+      Rails.logger.info "response from server #{response}"
+    end
+  end
+
+  def self.notify_submission(submission)
+    all.each do |it|
+      it.notify_submission!(submission)
+    end
+  end
 end
