3fb6b93ff6c8a4abf7374d5e55b138033c3a0c45
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-24 12:53:51 -0300

    Adding exercise breadcrumbs

---
+++ app/helpers/concerns/with_breadcrumbs.rb
@@ -0,0 +1,23 @@
+module WithBreadcrumbs
+  def exercise_breadcrumb(e)
+    base = link_to_exercise e
+    if e.guide
+      "#{guide_breadcrumb(e.guide)}/#{base}".html_safe
+    else
+      base
+    end
+  end
+
+  def guide_breadcrumb(g)
+    base = link_to_guide g
+    if g.path
+      "#{path_breadcrumb(g.path)}/#{base}".html_safe
+    else
+      base
+    end
+  end
+
+  def path_breadcrumb(p)
+    link_to_path p
+  end
+end
