2b8b675ace53fecd1ecd2cf430f2e3236e2d0d39
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-24 11:45:06 -0300

    Removing duplications on Stats

--- spec/models/stats_spec.rb
+++ spec/models/stats_spec.rb
@@ -5,12 +5,20 @@ describe Stats do
   context 'with good stats' do
     let(:stats) { Stats.new(passed: 6, passed_with_warnings: 3, failed: 1, unknown: 0) }
     it { expect(stats.passed_ratio).to eq 60 }
+    it { expect(stats.resolved_ratio).to eq 90 }
     it { expect(stats.passed_with_warnings_ratio).to eq 30 }
     it { expect(stats.failed_ratio).to eq 10 }
     it { expect(stats.resolved).to eq 9 }
     it { expect(stats.good_progress?).to be true }
     it { expect(stats.stuck?).to be false }
     it { expect(stats.done?).to be false }
+  end
+
+
+  context 'with irrational stats' do
+    let(:stats) { Stats.new(passed: 1, passed_with_warnings: 0, failed: 2, unknown: 0) }
+    it { expect(stats.passed_ratio).to eq 33.33 }
+    it { expect(stats.failed_ratio).to eq 66.67 }
   end
 
 
