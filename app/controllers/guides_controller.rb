9b939576156710032f027c0a8a015bea88b51a9f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-24 11:48:54 -0300

    Rename stats -> stats_for

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -32,7 +32,7 @@ class GuidesController < ApplicationController
 
   def show
     if current_user?
-      @stats = @guide.stats(current_user)
+      @stats = @guide.stats_for(current_user)
       @next_exercise = @guide.next_exercise(current_user)
     else
       @next_exercise = @guide.first_exercise
--- app/models/guide.rb
+++ app/models/guide.rb
@@ -32,7 +32,7 @@ class Guide < ActiveRecord::Base
     exercises.flat_map(&:search_tags).uniq
   end
 
-  def stats(user)
+  def stats_for(user)
     Stats.from_statuses exercises.map { |it| it.status_for(user) }
   end
 
