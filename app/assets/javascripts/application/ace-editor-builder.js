900982d43f75415f86994a35a52e67c460597b78
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-08-29 17:58:58 -0300

    Using closure namespaces

--- app/assets/javascripts/application/ace-editor-builder.js
+++ app/assets/javascripts/application/ace-editor-builder.js
@@ -1,45 +1,49 @@
 var mumuki = mumuki || {};
-mumuki.page = mumuki.page || {};
-mumuki.editor = mumuki.editor || {};
 
-mumuki.editor.AceEditorBuilder = function(textarea) {
-  this.textarea = textarea;
-};
+(function (mumuki) {
 
-mumuki.editor.AceEditorBuilder.prototype = {
-  setupEditor: function() {
-    this.form = this.textarea.form;
-    this.editor = ace.edit(this.textarea);
-    this.editor.container.id = "editor-container";//FIXME
-
-  },
-  setupLanguage: function () {
-    var language = $(this.textarea).data('editor-language');
-    if (language === 'dynamic') {
-      mumuki.page.dynamicEditors.push(this.editor);
-    } else {
-      mumuki.editor.setEditorLanguage(this.editor, language);
-    }
-  },
-  setupOptions: function () {
-    this.editor.setOptions({
-      minLines: 15,
-      maxLines: Infinity,
-      wrap: true
-    });
-    this.editor.setFontSize(13);
-  },
-  setupSubmit: function () {
-    var textarea = this.textarea;
-    var form = this.form;
-    var editor = this.editor;
-    this.form.addEventListener("submit", function () {
-      textarea.style.visibility = "hidden";//FIXME
-      textarea.value = editor.getValue();
-      form.appendChild(textarea)
-    });
-  },
-  build: function () {
-    return this.editor;
+  function AceEditorBuilder(textarea) {
+    this.textarea = textarea;
   }
-};
+
+  AceEditorBuilder.prototype = {
+    setupEditor: function () {
+      this.form = this.textarea.form;
+      this.editor = ace.edit(this.textarea);
+      this.editor.container.id = "editor-container";//FIXME
+
+    },
+    setupLanguage: function () {
+      var language = $(this.textarea).data('editor-language');
+      if (language === 'dynamic') {
+        mumuki.page.dynamicEditors.push(this.editor);
+      } else {
+        mumuki.editor.setEditorLanguage(this.editor, language);
+      }
+    },
+    setupOptions: function () {
+      this.editor.setOptions({
+        minLines: 15,
+        maxLines: Infinity,
+        wrap: true
+      });
+      this.editor.setFontSize(13);
+    },
+    setupSubmit: function () {
+      var textarea = this.textarea;
+      var form = this.form;
+      var editor = this.editor;
+      this.form.addEventListener("submit", function () {
+        textarea.style.visibility = "hidden";//FIXME
+        textarea.value = editor.getValue();
+        form.appendChild(textarea)
+      });
+    },
+    build: function () {
+      return this.editor;
+    }
+  };
+
+  mumuki.editor = mumuki.editor || {};
+  mumuki.editor.AceEditorBuilder = AceEditorBuilder;
+})(mumuki);
--- app/assets/javascripts/application/ace_editor.js
+++ app/assets/javascripts/application/ace_editor.js
@@ -1,44 +1,49 @@
 var mumuki = mumuki || {};
-mumuki.page = mumuki.page || {};
 
-mumuki.page.dynamicEditors = [];
-mumuki.page.editors = [];
+(function (mumuki) {
+  function createAceEditors() {
+    var editors = $(".editor").map(function (index, textarea) {
+      var builder = new mumuki.editor.AceEditorBuilder(textarea);
+      builder.setupEditor();
+      builder.setupOptions();
+      builder.setupSubmit();
+      builder.setupLanguage();
+      return builder.build();
+    });
 
-mumuki.editor = mumuki.editor || {};
-mumuki.editor.setupAceEditors = function () {
-  var editors = $(".editor").map(function (index, textarea) {
-    var builder = new mumuki.editor.AceEditorBuilder(textarea);
-    builder.setupEditor();
-    builder.setupOptions();
-    builder.setupSubmit();
-    builder.setupLanguage();
-    return builder.build();
+    if (editors[0]) {
+      editors[0].focus();
+    }
+    return editors;
+  }
+
+  function onSelectUpdateAceEditor() {
+    $("#exercise_language_id").change(updateAceEditorLanguage);
+  }
+
+  function updateAceEditorLanguage() {
+    var language = $("#exercise_language_id").find(":selected").html() || $('#exercise_language').val();
+    if (language !== undefined) {
+      mumuki.page.dynamicEditors.forEach(function (e) {
+        setEditorLanguage(e, language);
+      })
+    }
+  }
+
+  function setEditorLanguage(editor, language) {
+    editor.getSession().setMode("ace/mode/" + language.toLowerCase())
+  }
+
+  mumuki.editor = mumuki.editor || {};
+  mumuki.editor.setupAceEditors = setEditorLanguage;
+
+  mumuki.page = mumuki.page || {};
+  mumuki.page.dynamicEditors = [];
+  mumuki.page.editors = [];
+
+  $(document).on('ready page:load', function () {
+    mumuki.page.editors = createAceEditors();
+    updateAceEditorLanguage();
+    onSelectUpdateAceEditor();
   });
-
-  if (editors[0]) {
-    editors[0].focus();
-  }
-
-  mumuki.page.editors = editors;
-};
-mumuki.editor.onSelectUpdateAceEditor = function () {
-  $("#exercise_language_id").change(mumuki.editor.updateAceEditorLanguage);
-};
-mumuki.editor.updateAceEditorLanguage = function () {
-  var language = $("#exercise_language_id").find(":selected").html() || $('#exercise_language').val();
-  if (language !== undefined) {
-    mumuki.page.dynamicEditors.forEach(function (e) {
-      mumuki.editor.setEditorLanguage(e, language);
-    })
-  }
-};
-mumuki.editor.setEditorLanguage = function(editor, language) {
-  editor.getSession().setMode("ace/mode/" + language.toLowerCase())
-};
-
-$(document).on('ready page:load', function () {
-  mumuki.editor.setupAceEditors();
-  mumuki.editor.updateAceEditorLanguage();
-  mumuki.editor.onSelectUpdateAceEditor();
-});
-
+})(mumuki);
