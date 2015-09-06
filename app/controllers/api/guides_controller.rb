ffd962e0efc20ad534e251382b00618dd6e2c319
aguspina <aguspina87@gmail.com>
2015-09-06 12:36:57 -0300

    Fix typo

--- app/controllers/api/guides_controller.rb
+++ app/controllers/api/guides_controller.rb
@@ -1,6 +1,6 @@
 class Api::GuidesController < Api::BaseController
   def index
-    @guides = Guides.all
+    @guides = Guide.all
     render json: {users: @guides.as_json(only: [:id, :name, :language_id, :path_id, :github_repository, :position])}
   end
 end
