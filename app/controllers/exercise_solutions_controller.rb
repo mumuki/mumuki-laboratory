b91fd5f249893154304b43293f54ba2e1d9f0232
gcrespi <crespi.gustavo@gmail.com>
2015-09-16 00:29:03 -0300

    Rename "guide_done?"

--- app/controllers/exercise_solutions_controller.rb
+++ app/controllers/exercise_solutions_controller.rb
@@ -4,14 +4,14 @@ class ExerciseSolutionsController < ApplicationController
   before_action :authenticate!
 
   def create
-    guide_previously_done = is_guide_done?
+    guide_previously_done = guide_done?
     @solution = @exercise.submit_solution(current_user, solution_params)
     @solution.run_tests!
-    guide_finished_by_solution = !guide_previously_done && is_guide_done?
+    guide_finished_by_solution = !guide_previously_done && guide_done?
     render partial: 'exercise_solutions/results', locals: {solution: @solution, guide_finished_by_solution: guide_finished_by_solution}
   end
 
-  def is_guide_done?
+  def guide_done?
     guide = @exercise.guide
     guide && guide.stats_for(current_user).done?
   end
