2982133e1f6aa031ff89c52db0f4a942b73194bf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-10-02 10:16:32 -0300

    Rename method

--- app/controllers/exercise_solutions_controller.rb
+++ app/controllers/exercise_solutions_controller.rb
@@ -5,7 +5,7 @@ class ExerciseSolutionsController < ApplicationController
 
   def create
     guide_previously_done = @exercise.guide_done_for?(current_user)
-    @assignment = @exercise.submit_solution(current_user, assignment_params)
+    @assignment = @exercise.submit_solution(current_user, solution_params)
     @assignment.run_tests!
     guide_finished_by_solution = !guide_previously_done && @exercise.guide_done_for?(current_user)
     render partial: 'exercise_solutions/results', locals: {assignment: @assignment, guide_finished_by_solution: guide_finished_by_solution}
@@ -13,7 +13,7 @@ class ExerciseSolutionsController < ApplicationController
 
   private
 
-  def assignment_params
+  def solution_params
     params.require(:solution).permit(:content)
   end
 end
