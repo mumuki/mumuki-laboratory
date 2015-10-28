f092bd965c2591e2039fded713b1a644e40c4928
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-10-28 12:52:36 -0300

    Not requiring GH write permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['MUMUKI_GITHUB_CLIENT_ID'], ENV['MUMUKI_GITHUB_CLIENT_SECRET'], scope: 'user:email,write:repo_hook,repo'
+  provider :github, ENV['MUMUKI_GITHUB_CLIENT_ID'], ENV['MUMUKI_GITHUB_CLIENT_SECRET'], scope: 'user:email'
   provider :facebook, ENV['MUMUKI_FACEBOOK_KEY'], ENV['MUMUKI_FACEBOOK_SECRET'], :image_size => 'large'
 end
