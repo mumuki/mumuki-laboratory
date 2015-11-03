d155ccf6e8aab134afb7654b63e34153338816b7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-03 12:32:03 -0300

    Quickfix: submit button was not displayed

--- app/helpers/concerns/with_exercise_form_rendering.rb
+++ app/helpers/concerns/with_exercise_form_rendering.rb
@@ -1,5 +1,5 @@
 module WithExerciseFormRendering
-  def exercise_form(exercise)
-    render "layouts/submissions/editor/#{exercise.class.name.underscore}_form"
+  def exercise_form(exercise, editor_visible=true)
+    render "layouts/submissions/editor/#{exercise.class.name.underscore}_form", editor_visible: editor_visible
   end
 end
