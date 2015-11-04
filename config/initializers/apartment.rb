0cac5a3d7bad1b901bbbcae88a96ae7ac4b0a26b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-04 13:32:43 -0300

    Adding apartment config

---
+++ config/initializers/apartment.rb
@@ -0,0 +1,27 @@
+require 'apartment/elevators/first_subdomain'
+
+Apartment.configure do |config|
+
+  config.excluded_models = %w{ User Assignment Exercise Export Guide Import Language  }
+
+  config.tenant_names = ['haskell', 'gobstones']
+  #config.tenant_names = lambda { ToDo_Tenant_Or_User_Model.pluck :database }
+
+
+  # Apartment can be forced to use raw SQL dumps instead of schema.rb for creating new schemas.
+  # Use this when you are using some extra features in PostgreSQL that can't be respresented in
+  # schema.rb, like materialized views etc. (only applies with use_schemas set to true).
+  # (Note: this option doesn't use db/structure.sql, it creates SQL dump by executing pg_dump)
+  #
+  # config.use_sql = false
+
+  # There are cases where you might want some schemas to always be in your search_path
+  # e.g when using a PostgreSQL extension like hstore.
+  # Any schemas added here will be available along with your selected Tenant.
+  #
+  # config.persistent_schemas = %w{ hstore }
+
+end
+
+Rails.application.config.middleware.use 'Apartment::Elevators::FirstSubdomain'
+
