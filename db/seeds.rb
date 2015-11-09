d555286a3a1dd23cf6ffbca4c582f72989d9270b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-08 21:08:56 -0300

    Adding guides from content.editor as part of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,16 @@
-haskell = Language.create!(name: 'Haskell',
+haskell = Language.create!(name: 'haskell',
                            test_runner_url: 'http://mumuki-hspec-server.herokuapp.com',
                            extension: 'hs',
                            image_url: 'https://www.haskell.org/wikistatic/haskellwiki_logo.png')
 
-Language.create!(name: 'Prolog',
+Language.create!(name: 'prolog',
                  test_runner_url: 'http://mumuki-plunit-server.herokuapp.com',
                  extension: 'pl',
                  image_url: 'http://cdn.portableapps.com/SWI-PrologPortable_128.png')
 
-Language.create!(name: 'Ruby',
+Language.create!(name: 'ruby',
                  test_runner_url: 'http://mumuki-rspec-server.herokuapp.com/',
                  extension: 'rb',
-                 image_url: 'https://www.ruby-lang.org/images/header-ruby-logo.png')
-
-Language.create!(name: 'ruby-stones',
-                 test_runner_url: 'http://mumuki-ruby-stones-server.herokuapp.com/',
-                 extension: 'yml',
                  image_url: 'https://www.ruby-lang.org/images/header-ruby-logo.png')
 
 Language.create!(name: 'gobstones',
@@ -24,8 +19,23 @@ Language.create!(name: 'gobstones',
                  image_url: 'https://www.ruby-lang.org/images/header-ruby-logo.png')
 
 
-functional = Category.create!(name: 'Programación Funcional', locale: :es, description: 'Programación Funcional', image_url: 'http://mumuki.io/favicon')
+[ "df52800791c4053a",
+  "1051a6103501cb75",
+  "32f86641962911d1",
+  "31322fa7d9003fa3",
+  "bafc328faba8a103",
+  "eb5f5e4ad07b02d4",
+  "c9c71d96902587b6",
+  "947312032126df6b",
+  "5885fefcda5b9a3c"].each do |id|
+  Guide.create!(url: "http://content.mumuki.io/guides/#{id}").import!
+end
 
+
+functional = Category.create!(name: 'Programación Funcional',
+                              locale: :es,
+                              description: 'Programación Funcional',
+                              image_url: 'http://mumuki.io/favicon')
 
 Path.create(category: functional, language: haskell)
 
