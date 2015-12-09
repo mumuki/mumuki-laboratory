c4dd98a23b61efc1d3c81d641e84096a66191355
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-08 22:44:03 -0300

    Removing nonsense spec

--- spec/models/chapter_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Chapter do
-  let!(:functional) { build(:chapter, name: 'Functional Programming') }
-  let!(:haskell) { build(:language, name: 'haskell') }
-  let(:guide) { build(:guide, github_repository: 'flbulgarelli/mumuki-sample-exercises', author: 'author', chapter: functional) }
-
-  it { expect(functional.name).to eq 'Functional Programming' }
-end
