f8acda244b2e656d8eae0e5d371981182d9e3caa
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-14 21:09:09 -0300

    Refactor book interface

--- db/seeds/central.rb
+++ db/seeds/central.rb
@@ -1,44 +1,44 @@
-PathBuilder.build do |p|
-  b.chapter 'Fundamentos' do |p|
-    p.organization 'sagrado-corazon-alcal'
-    p.guide 'fundamentos-gobstones-guia-1-primeros-programas'
-    p.guide 'fundamentos-gobstones-guia-2-procedimientos'
-    p.guide 'fundamentos-gobstones-guia-3-repeticion-simple'
+Book.build! do |b|
+  b.chapter 'Fundamentos' do |c|
+    c.organization 'sagrado-corazon-alcal'
+    c.guide 'primeros-programas'
+    c.guide 'procedimientos'
+    c.guide 'repeticion-simple'
   end
-  b.chapter 'Programacion Funcional' do |p|
-    p.organization 'pdep-utn'
-    p.prefix 'funcional'
+  b.chapter 'Programacion Funcional' do |c|
+    c.organization 'pdep-utn'
+    c.prefix 'funcional'
 
-    p.guide 'valores-y-funciones'
-    p.guide 'practica-funcional-valores-y-funciones'
+    c.guide 'valores-y-funciones'
+    c.guide 'practica-funcional-valores-y-funciones'
     #p.guide A Tipos Simples
     #P Práctica Tipos Simples
     #A Aplicación Parcial y Funciones de Orden Superior [Currificacion]
-    p.guide 'practica-aplicacion-parcial-y-orden-superior' #+ alguno de orden superior simple
+    c.guide 'practica-aplicacion-parcial-y-orden-superior' #+ alguno de orden superior simple
     #A Funciones Partidas: Guardas y Pattern Matching [Tuplas]
-    p.guide 'practica-funciones-partidas' #y pdep-utn/mumuki-funcional-guia-2 (1-4)
+    c.guide 'practica-funciones-partidas' #y pdep-utn/mumuki-funcional-guia-2 (1-4)
     #A Listas, Parte 1: Introducción, length, head, tail, zip
     #A Listas, Parte 2: Orden superior, map, filter, any, all, zipWith
     #A Tipos Paramétricos
-    p.guide 'practica-listas'
+    c.guide 'practica-listas'
     #A Listas, Parte 3: Compresión
-    p.guide 'practica-listas-por-compresion'
+    c.guide 'practica-listas-por-compresion'
     #A Funciones Anónimas
-    p.guide 'practica-expresiones-lambda'
+    c.guide 'practica-expresiones-lambda'
 
     #p.guide 'modelado'
     #p.guide alguna practica integradora
 
     #p.guide 'recursividad'
-    p.guide 'practica-recursividad'
+    c.guide 'practica-recursividad'
 
     #p.guide 'evaluacion-diferida-y-listas-infinitas'
     #A typeclasses: Restricciones de Tipos
-    p.guide 'inferencia-de-tipos'
+    c.guide 'inferencia-de-tipos'
 
     #A Declaraciones Locales
 
-    p.guide 'practica-chocobos'
+    c.guide 'practica-chocobos'
   end
   b.chapter 'Programación Lógica' do |p|
 
@@ -66,7 +66,6 @@ PathBuilder.build do |p|
 
   end
   b.chapter 'Programación con Objetos' do |p|
-
   end
 end
 
