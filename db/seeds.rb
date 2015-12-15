0918930e54909635e8ff9b71d6049de8f7afcec4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-14 21:15:29 -0300

    Adding book dsl for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
+require_relative './seeds/book'
+
 if Tenant.on_public?
   puts 'Seeding global'
   require_relative './seeds/languages'
   require_relative './seeds/guides'
 
   Tenant.create!(name: 'central', locale: 'es').switch!
-else
-  Chapter.create!(name: 'Programación Funcional',
-                  locale: :es,
-                  description: 'Programación Funcional',
-                  image_url: 'http://mumuki.io/favicon').rebuild!(Guide.all)
+elsif Tenant.on_central?
+  require_relative '../seeds/central'
 end
 
 
---
+++ db/seeds/book.rb
@@ -0,0 +1,53 @@
+class Book
+  def initialize
+    @chapters=[]
+  end
+
+  def chapter(name)
+    builder = ChapterBuilder.new(name)
+    yield builder
+    @chapters << builder.build
+  end
+
+  def build!
+    Chapter.rebuild!(@chapters)
+  end
+
+  def self.build!
+    book = Book.new
+    yield book
+    book.build!
+  end
+end
+
+class ChapterBuilder
+  def initialize(name)
+    @chapter = Chapter.new(name: name)
+    @slugs = []
+  end
+
+  def organization(name)
+    @organization = name
+  end
+
+  def prefix(prefix)
+    @prefix = prefix
+  end
+
+  def guide(slug_part)
+    @slugs << create_slug(slug_part)
+  end
+
+  def build
+    @chapter.guides = @slugs.map { |it| Guide.find_by(slug: it) }
+    @chapter
+  end
+
+  private
+
+  def create_slug(slug_part)
+    "#{@organization}/mumuki-#{I18n.t :guide}-#{@prefix}-#{slug_part}"
+  end
+end
+
+
