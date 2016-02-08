3ff674cc164bba8c5fde482821dca319ff60a71f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-02-07 21:36:24 -0300

    Bye bye openshift...

--- escualo_install.sh
+++ escualo_install.sh
@@ -3,6 +3,9 @@ REV=$1
 
 echo $REV > public/version
 
+echo "[Escualo::Atheneum] Copying robots.txt"
+mv public/robots.production.txt public/robots.txt
+
 echo "[Escualo::Atheneum] Running Migrations..."
 rake db:migrate
 
