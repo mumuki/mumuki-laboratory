c6bec2fc73b4f2e24e580d9cad72d17f546ef5f4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-04 15:37:31 -0300

    Updating progress bar
    Fixes #446

--- app/helpers/concerns/with_status_rendering.rb
+++ app/helpers/concerns/with_status_rendering.rb
@@ -6,4 +6,8 @@ module WithStatusRendering
   def icon_type_for_status(s)
     Status.coerce(s).iconize[:type].to_s
   end
+
+  def class_for_exercise(exercise)
+    class_for_status(exercise.status_for(current_user))
+  end
 end
