b26e4c484b9d65953803636c3f27f866fe3cd506
Agustin Pina <aguspina87@gmail.com>
2016-04-26 11:32:35 -0300

    Add some more tests

--- spec/models/event_subscriber_spec.rb
+++ spec/models/event_subscriber_spec.rb
@@ -5,6 +5,8 @@ require 'rspec/mocks'
 describe EventSubscriber do
   let!(:subscriber) { EventSubscriber.create!(enabled: true, url: 'http://localhost:80') }
   let!(:assignment) { create(:assignment) }
+  let(:user) { create(:user) }
+  let(:exercise) { create(:x_equal_5_exercise) }
 
   describe '#notify!' do
     before { expect_any_instance_of(EventSubscriber).to receive(:do_request).and_return({'status' => 'ok'}.to_json) }
@@ -24,10 +26,21 @@ describe EventSubscriber do
 
   describe 'test event subscriber api' do
     before { expect_any_instance_of(EventSubscriber).to receive(:do_request).and_return({'status' => 'ok'}.to_json) }
-    let(:user) { create(:user) }
-    let(:exercise) { create(:x_equal_5_exercise) }
 
     it 'run notifier when submit a solution' do
+      expect { exercise.submit_solution! user }.to_not raise_error
+    end
+  end
+
+  describe 'test event queue_name' do
+    it 'submission event queue_name is submissions' do
+      expect(Mumukit::Nuntius::Publisher).to receive(:publish_submissions)
+      expect { exercise.submit_solution! user }.to_not raise_error
+    end
+
+    it 'submissions mock queue_name is comments' do
+      expect_any_instance_of(Event::Submission).to receive(:queue_name).and_return('comments')
+      expect(Mumukit::Nuntius::Publisher).to receive(:publish_comments)
       expect { exercise.submit_solution! user }.to_not raise_error
     end
   end
