b8212f9d5671ddefbe701c5e727e6c3587fad115
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-27 21:05:13 -0300

    Adding locale to topic

--- db/migrate/20160427235612_create_topic_objects.rb
+++ db/migrate/20160427235612_create_topic_objects.rb
@@ -1,7 +1,7 @@
 class CreateTopicObjects < ActiveRecord::Migration
   def change
     Chapter.all.each do |it|
-      topic = Topic.create! name: it.name, description: it.description
+      topic = Topic.create! name: it.name, description: it.description, locale: it.locale
       it.topic = topic
     end
   end
