4b6a55b33f2b248a29c609cc34c4d75a988babf4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-08 17:46:37 -0300

    Ignoring dead ends in navigation

--- app/helpers/concerns/with_navigation.rb
+++ app/helpers/concerns/with_navigation.rb
@@ -1,5 +1,6 @@
 module WithNavigation
   def next_button(navigable)
+    return unless navigable
     ContinueNavigation.new(self).button(navigable) || RevisitNavigation.new(self).button(navigable)
   end
 end
