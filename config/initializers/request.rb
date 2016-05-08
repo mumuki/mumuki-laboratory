10381766767cc02783b8bd9a0b539ee53077f5ac
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-08 05:41:45 -0300

    Quickfix: subdomain parsing

--- config/initializers/request.rb
+++ config/initializers/request.rb
@@ -15,7 +15,7 @@ module Rack
     def extract_subdomains
       raise 'no host set' unless host
       raise 'set hostname first!' if /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(host)
-      host.split('.')[0]
+      (host.split('.') - ['mumuki', 'io'])[0]
     end
   end
 end
