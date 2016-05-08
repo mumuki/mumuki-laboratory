5f500a5483455048ce98b28df880655883f04952
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-07 22:47:57 -0300

    Introducing complement factory

---
+++ spec/factories/complement_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+
+  factory :complement, traits: [:guide_container] do
+    book { Organization.current.book rescue nil }
+  end
+end
--- spec/factories/lesson_factory.rb
+++ spec/factories/lesson_factory.rb
@@ -1,24 +1,7 @@
 FactoryGirl.define do
 
-  factory :lesson do
+  factory :lesson, traits: [:guide_container] do
     sequence(:number)
     topic
-
-    transient do
-      exercises []
-      name { Faker::Lorem.sentence(3) }
-      description { Faker::Lorem.sentence(10) }
-      language { create(:language) }
-      slug { "mumuki/mumuki-test-lesson-#{SecureRandom.uuid}" }
-    end
-
-    after(:build) do |lesson, evaluator|
-      lesson.guide = build(:guide,
-                           name: evaluator.name,
-                           slug: evaluator.slug,
-                           exercises: evaluator.exercises,
-                           description: evaluator.description,
-                           language: evaluator.language) unless evaluator.guide
-    end
   end
 end
