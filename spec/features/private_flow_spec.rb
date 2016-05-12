e63e9232669a6d25f48cf5f8c3a818485e5915ee
Agustin Pina <aguspina87@gmail.com>
2016-05-12 14:02:38 -0300

    Add tests

---
+++ spec/features/private_flow_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature 'When public org' do
+  scenario 'should access normally' do
+    visit '/exercises'
+
+    expect(page).to have_text('Nobody')
+  end
+end
+
+feature 'When private org' do
+  before {
+    create(:organization,
+         name: 'private',
+         private: true,
+         book: create(:book, name: 'private', slug: 'mumuki/mumuki-the-private-book'))
+    set_subdomain_host 'private'
+  }
+
+  scenario 'should not access' do
+    visit '/guides'
+
+
+    expect(page).not_to have_text('Nobody')
+  end
+
+  scenario 'should raise routing error' do
+    expect_any_instance_of(ApplicationController).to receive(:must_login).and_return(false)
+    expect_any_instance_of(ApplicationController).to receive(:from_auth0?).and_return(false)
+    expect_any_instance_of(ApplicationController).to receive(:can_visit?).and_return(false)
+
+    expect { visit '/guides' }.to raise_error(ActionController::RoutingError)
+  end
+end
