473cd1d283bfe96170c31d4409660578b2e545d8
Rodrigo Alfonso <rodri042@gmail.com>
2016-05-13 02:37:26 -0300

    start_offline.sh will be in /mumuki-offline side

--- start_offline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export OFFLINE_MODE=true
-export SECRET_KEY_BASE=56059cc4131dec83e36833d9afd0201eb4e3fa4f0a49ee75de9788ff0e4f66e17a08c49aaa1767169ad0757163397a5d4868419575ef133d9c43208bd24cf20a
-export PGDATABASE=mumuki
-export POSTGRESQL_DB_USERNAME=mumuki
-export POSTGRESQL_DB_PASSWORD=mumuki
-export POSTGRESQL_DB_HOST=localhost
-RAILS_ENV=production rails s
