d1f1d65f62c9d46f52a316d9ee9593412faa3a56
Agustin Pina <aguspina87@gmail.com>
2016-05-17 19:18:02 -0300

    Add import_from_json logic in exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -34,4 +34,21 @@ class Exam < ActiveRecord::Base
   def enabled_range
     start_time..end_time
   end
+
+  def self.import_from_json!(json)
+    Organization.find_by!(name: json.delete('tenant')).switch!
+    exam_data = Exam.parse_json json
+    users = exam_data.delete('users')
+    e = Exam.where(classroom_id: exam_data.delete('classroom_id')).first_or_create exam_data
+    users.map { |user| e.authorize! user}
+  end
+
+  def self.parse_json(exam_json)
+    exam = exam_json.except('name', 'language')
+    exam['guide_id'] = Guide.find_by(slug: exam.delete('slug')).id
+    exam['organization_id'] = Organization.current.id
+    exam['users'] = exam.delete('social_ids').map { |sid| User.find_by(uid: sid) }
+    ['start_time', 'end_time'].each { |param| exam[param] = exam[param].to_time }
+    exam
+  end
 end
