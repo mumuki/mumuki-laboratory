a58270ed408dfe0a1db19015bef22696138b75b2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-23 07:00:48 -0300

    Adding more tests

--- spec/features/links_flow_spec.rb
+++ spec/features/links_flow_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
 feature 'Lessons Flow' do
-  let!(:lesson) { create(:lesson, name: 'L1') }
+  let(:exercise) { create(:exercise, name:  'E1') }
+  let!(:complement) { create(:complement) }
+  let!(:lesson) { create(:lesson, name: 'L1', exercises: [exercise]) }
   let!(:chapter) { create(:chapter, name: 'C1', lessons: [lesson]) }
+
 
   let(:other_organization) { create(:organization, name: 'other') }
   let(:other_book) { create(:book) }
-  let!(:other_lesson) { create(:lesson, name: 'L2') }
-  let!(:other_chapter) { create(:chapter, name: 'C2', lessons: [other_lesson], book: other_book) }
+  let(:other_exercise) { create(:exercise) }
+  let!(:other_complement) { create(:complement, book: other_book) }
+  let!(:other_lesson) { create(:lesson, exercises: [other_exercise]) }
+  let!(:other_chapter) { create(:chapter, lessons: [other_lesson], book: other_book) }
 
   before { reindex_current_book! }
   before { reindex_book! other_organization }
@@ -30,5 +35,25 @@ feature 'Lessons Flow' do
 
   scenario 'visit chapter not in path' do
     expect { visit "/chapters/#{other_chapter.id}" }.to raise_error(ActionController::RoutingError)
+  end
+
+  scenario 'visit exercise in path' do
+    visit "/exercises/#{exercise.id}"
+
+    expect(page).to have_text('E1')
+  end
+
+  scenario 'visit exercise not in path' do
+    expect { visit "/exercises/#{other_exercise.id}" }.to raise_error(ActionController::RoutingError)
+  end
+
+  scenario 'visit exercise in path' do
+    visit "/complements/#{complement.id}"
+
+    expect(page).to have_text(complement.name)
+  end
+
+  scenario 'visit exercise not in path' do
+    expect { visit "/complements/#{other_complement.id}" }.to raise_error(ActionController::RoutingError)
   end
 end
