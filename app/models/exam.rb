4119f76d1549c180b0cd2760756cae8db9fa5e1d
Agustin Pina <aguspina87@gmail.com>
2016-05-31 17:30:03 -0300

    Teachers do not start exams

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -60,7 +60,7 @@ class Exam < ActiveRecord::Base
   end
 
   def start!(user)
-    authorization_for(user).start!
+    authorization_for(user).start! unless user.teacher?(guide.slug)
   end
 
   def started?(user)
