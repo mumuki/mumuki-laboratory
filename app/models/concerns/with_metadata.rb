c837eba44cf1fbb7ecd587d8e2eaf48ee7e0a066
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-06-13 16:05:26 -0300

    Revert "Quick teacher exam fix"
    This reverts commit 88a0dd8842fc4046a3b572432468737773d97b90.

--- app/models/concerns/with_metadata.rb
+++ app/models/concerns/with_metadata.rb
@@ -6,11 +6,7 @@ module WithMetadata
 
     validates_presence_of :metadata
 
-    delegate :student?, :admin?, to: :metadata
-
-    def teacher?(slug) ## BIG FIX ME
-      metadata.permissions('classroom').present?
-    end
+    delegate :student?, :teacher?, :admin?, to: :metadata
   end
 
 end
