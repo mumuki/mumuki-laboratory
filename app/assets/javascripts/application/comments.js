ec9b3490031e47a838a12b48946d6648ee6fc99a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-06-15 23:03:40 -0300

    Not requesting comments for annonymous users

--- app/assets/javascripts/application/comments.js
+++ app/assets/javascripts/application/comments.js
@@ -4,6 +4,9 @@ var mumuki = mumuki || {};
   $(function () {
     var token = new mumuki.CsrfToken();
     setInterval(function () {
+      if ($('.badge-comments').length == 0) {
+        return;
+      }
       $.ajax(token.newRequest({
           url: '/comments',
           type: 'GET'
@@ -12,7 +15,7 @@ var mumuki = mumuki || {};
           $('.badge-comments').html(res.comments_count);
           $('.comments-box').toggleClass('comments-box-empty', !res.has_comments);
         }).
-        fail(function(_error) {
+        fail(function (_error) {
           //ignoring error, not important
         });
     }, 60000);
