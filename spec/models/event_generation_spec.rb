6fe3c1219f7f92481560452aa3d2b3f001a8acc4
Agustin Pina <aguspina87@gmail.com>
2016-06-27 11:41:24 -0300

    Add exam submission spec

--- spec/models/event_generation_spec.rb
+++ spec/models/event_generation_spec.rb
@@ -5,43 +5,42 @@ describe Event do
   describe '#to_json' do
     describe Event::Submission do
       let(:user) { create(:user, id: 2, name: 'foo', provider: 'auth0', uid: 'github|gh1234') }
+      describe 'lesson type' do
+        let(:lesson) { chapter.lessons.fourth }
+        let(:guide) { lesson.guide }
+        let(:exercise) { lesson.exercises.first }
+        let!(:chapter) {
+          create(:chapter, lessons: [
+            create(:lesson),
+            create(:lesson),
+            create(:lesson),
+            create(:lesson, exercises: [create(:exercise)])]) }
 
+        before { reindex_current_organization! }
 
-      let(:lesson) { chapter.lessons.fourth }
-      let(:guide) { lesson.guide }
-      let(:exercise) { lesson.exercises.first }
-      let!(:chapter) {
-        create(:chapter, lessons: [
-        create(:lesson),
-        create(:lesson),
-        create(:lesson),
-        create(:lesson, exercises: [create(:exercise)])]) }
+        let(:assignment) { create(:assignment,
+                                  solution: 'x = 2',
+                                  status: Status::Passed,
+                                  submissions_count: 2,
+                                  submitter: user,
+                                  submission_id: 'abcd1234',
+                                  exercise: exercise) }
+        let(:event) { Event::Submission.new(assignment) }
+        let(:json) { event.as_json.deep_symbolize_keys }
 
-      before { reindex_current_organization! }
-
-      let(:assignment) { create(:assignment,
-                                solution: 'x = 2',
-                                status: Status::Passed,
-                                submissions_count: 2,
-                                submitter: user,
-                                submission_id: 'abcd1234',
-                                exercise: exercise) }
-      let(:event) { Event::Submission.new(assignment) }
-      let(:json) { event.as_json.deep_symbolize_keys }
-
-      it { expect(lesson.number).to eq 4 }
-      it do
-        expect(json).to eq(status: Status::Passed,
-                           result: nil,
-                           expectation_results: nil,
-                           feedback: nil,
-                           test_results: nil,
-                           submissions_count: 2,
-                           exercise: {
+        it { expect(lesson.number).to eq 4 }
+        it do
+          expect(json).to eq(status: Status::Passed,
+                             result: nil,
+                             expectation_results: nil,
+                             feedback: nil,
+                             test_results: nil,
+                             submissions_count: 2,
+                             exercise: {
                                id: exercise.id,
                                name: exercise.name,
                                number: exercise.number},
-                           guide: {
+                             guide: {
                                name: guide.name,
                                slug: guide.slug,
                                lesson: {
@@ -59,19 +58,70 @@ describe Event do
                                    name: guide.chapter.name
                                  }
                                }
-                           },
-                           submitter: {
+                             },
+                             submitter: {
                                social_id: 'github|gh1234',
                                name: 'foo',
                                email: nil,
                                image_url: 'user_shape.png'},
-                           id: 'abcd1234',
-                           created_at: assignment.updated_at,
-                           content: 'x = 2',
-                           tenant: 'test')
+                             id: 'abcd1234',
+                             created_at: assignment.updated_at,
+                             content: 'x = 2',
+                             tenant: 'test')
+        end
+      end
+      describe 'exam type' do
+        let(:exam) { create(:exam, guide: create(:guide, exercises: [create(:exercise)])) }
+        let(:guide) { exam.guide }
+        let(:exercise) { guide.exercises.first }
+        before { reindex_current_organization! }
+        let(:assignment) { create(:assignment,
+                                  solution: 'x = 2',
+                                  status: Status::Passed,
+                                  submissions_count: 2,
+                                  submitter: user,
+                                  submission_id: 'abcd1234',
+                                  exercise: exercise) }
+        let(:event) { Event::Submission.new(assignment) }
+        let(:json) { event.as_json.deep_symbolize_keys }
+        before { puts exercise.navigable_parent }
+
+        it do
+          expect(json).to eq(status: Status::Passed,
+                             result: nil,
+                             expectation_results: nil,
+                             feedback: nil,
+                             test_results: nil,
+                             submissions_count: 2,
+                             exercise: {
+                               id: exercise.id,
+                               name: exercise.name,
+                               number: exercise.number},
+                             guide: {
+                               name: guide.name,
+                               slug: guide.slug,
+                               language: {
+                                 name: guide.language.name
+                               },
+                               parent: {
+                                 type: 'Exam',
+                                 name:  guide.name,
+                                 position: nil,
+                                 chapter: nil
+                               }
+                             },
+                             submitter: {
+                               name: 'foo',
+                               email: nil,
+                               image_url: 'user_shape.png',
+                               social_id: 'github|gh1234'},
+                             id: 'abcd1234',
+                             created_at: assignment.updated_at,
+                             content: 'x = 2',
+                             tenant: 'test')
+
+        end
       end
     end
   end
-
-
 end
