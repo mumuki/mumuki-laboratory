4e90c9397f5d0054c7d2ab332410318848b361c8
Federico Scarpa <fedescarpa@gmail.com>
2016-08-12 19:27:54 -0300

    added failing tests

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,21 +1,45 @@
 require 'spec_helper'
 
 describe Problem do
-  context 'when no test nor expectations' do
-    let(:problem) { build(:problem, test: nil, expectations: []) }
-    it { expect(problem.evaluation_criteria?).to be false }
+
+  context 'when no manual evaluation and' do
+    before { problem.manual_evaluation = false }
+
+    context 'when no test nor expectations' do
+      let(:problem) { build(:problem, test: nil, expectations: []) }
+      it { expect(problem.evaluation_criteria?).to be false }
+    end
+
+    context 'when no test but expectations' do
+      let(:problem) { build(:problem, test: nil, expectations: [{inspection: 'HasBinding', binding: 'foo'}]) }
+      it { expect(problem.evaluation_criteria?).to be true }
+    end
+
+    context 'when no expectations but test' do
+      let(:problem) { build(:problem, test: 'describe ...', expectations: []) }
+      it { expect(problem.evaluation_criteria?).to be true }
+    end
   end
 
-  context 'when no test but expectations' do
-    let(:problem) { build(:problem, test: nil, expectations: [{inspection: 'HasBinding', binding: 'foo'}]) }
-    it { expect(problem.evaluation_criteria?).to be true }
-  end
+  context 'when is manual evaluation and' do
+    before { problem.manual_evaluation = true }
 
-  context 'when no test nor expectations' do
-    let(:problem) { build(:problem, test: 'describe ...', expectations: []) }
-    it { expect(problem.evaluation_criteria?).to be true }
-  end
+    context 'when no test nor expectations' do
+      let(:problem) { build(:problem, test: nil, expectations: []) }
+      it { expect(problem.evaluation_criteria?).to be true }
+    end
 
+    context 'when no test but expectations' do
+      let(:problem) { build(:problem, test: nil, expectations: [{inspection: 'HasBinding', binding: 'foo'}]) }
+      it { expect(problem.evaluation_criteria?).to be false }
+    end
+
+    context 'when no expectations but test' do
+      let(:problem) { build(:problem, test: 'describe ...', expectations: []) }
+      it { expect(problem.evaluation_criteria?).to be false }
+    end
+
+  end
 
   context 'when solving then failing' do
     let!(:problem) { create(:problem) }
