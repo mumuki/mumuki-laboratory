a097e359782cf20bbf837e45e792fbd67a04ca19
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-05 19:11:07 -0300

    Adding tests for authoring rendering

--- spec/features/guides_flow_spec.rb
+++ spec/features/guides_flow_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 feature 'Guides Flow' do
   let(:haskell) { create(:haskell) }
-  let!(:exercises) {[
-    create(:exercise, name: 'Foo',        guide: guide, number: 1, description: 'Description of foo'),
-    create(:exercise, name: 'Bar',        guide: guide, number: 2),
-    create(:exercise, name: 'Baz',        guide: guide, number: 4)
-  ]}
-  let(:guide) { create(:guide, name: 'awesomeGuide', description: 'An awesome guide', language: haskell, slug: 'foo/bar') }
+  let!(:exercises) { [
+    create(:exercise, name: 'Foo', guide: guide, number: 1, description: 'Description of foo'),
+    create(:exercise, name: 'Bar', guide: guide, number: 2),
+    create(:exercise, name: 'Baz', guide: guide, number: 4)
+  ] }
+  let(:guide) { create(:guide, name: 'awesomeGuide', description: 'An awesome guide', language: haskell, slug: 'foo/bar', authors: authors) }
+  let(:authors) { nil }
   let(:guide_not_in_path) { create(:guide) }
 
   let!(:lesson) { create(:lesson, guide: guide) }
@@ -15,68 +16,101 @@ feature 'Guides Flow' do
 
 
   let!(:complement) { create(:complement, name: 'a complement', exercises: [
-      create(:exercise, name: 'complementary exercise 1'),
-      create(:exercise, name: 'complementary exercise 2')
+    create(:exercise, name: 'complementary exercise 1'),
+    create(:exercise, name: 'complementary exercise 2')
   ]) }
 
   before { reindex_current_organization! }
 
-  let(:user) { User.find_by(name:'testuser') }
+  let(:user) { User.find_by(name: 'testuser') }
 
-  scenario 'visit complement by guide by id' do
-    visit "/guides/#{complement.guide.id}"
 
-    expect(page).to have_text('a complement')
-    expect(page).to have_text('Content')
+  context 'inexistent guide' do
+    scenario 'visit guide by slug, not in path' do
+      visit "/guides/#{guide_not_in_path.slug}"
+      expect(page).to have_text('You may have mistyped the address or the page may have moved')
+    end
+
+    scenario 'visit guide by slug, unknown guide' do
+      visit '/guides/goo/baz'
+      expect(page).to have_text('You may have mistyped the address or the page may have moved')
+    end
+
+    scenario 'visit guide by id, not in path' do
+      visit "/guides/#{guide_not_in_path.id}"
+      expect(page).to have_text('You may have mistyped the address or the page may have moved')
+    end
+
+    scenario 'visit guide by id, unknown guide' do
+      visit '/guides/900000'
+      expect(page).to have_text('You may have mistyped the address or the page may have moved')
+    end
   end
 
-  scenario 'visit lesson by guide by id' do
-    visit "/guides/#{lesson.guide.id}"
+  context 'existent guide' do
+    context 'no authors' do
+      let(:authors) { '' }
 
-    expect(page).to have_text('awesomeGuide')
-    expect(page).to have_text('Content')
-  end
+      scenario 'visit complement by guide by id' do
+        visit "/guides/#{complement.guide.id}"
+
+        expect(page).to have_text('a complement')
+        expect(page).to have_text('Content')
+      end
+
+      scenario 'visit lesson by id' do
+        visit "/guides/#{lesson.guide.id}"
+
+        expect(page).to have_text('awesomeGuide')
+        expect(page).to have_text('Content')
+        expect(page).to_not have_text('Creative Commons')
+      end
 
 
-  scenario 'visit guide by slug' do
-    visit '/guides/foo/bar'
+      scenario 'visit guide by slug' do
+        visit '/guides/foo/bar'
 
-    expect(page).to have_text('awesomeGuide')
-    expect(page).to have_text('An awesome guide')
-    expect(page).to have_text('Content')
-  end
+        expect(page).to have_text('awesomeGuide')
+        expect(page).to have_text('An awesome guide')
+        expect(page).to have_text('Content')
+      end
 
-  scenario 'visit guide by slug, not in path' do
-    visit "/guides/#{guide_not_in_path.slug}"
-    expect(page).to have_text('You may have mistyped the address or the page may have moved')
-  end
+      scenario 'visit guides from search page, and starts practicing' do
+        visit '/guides'
 
-  scenario 'visit guide by slug, unknown guide' do
-    visit '/guides/goo/baz'
-    expect(page).to have_text('You may have mistyped the address or the page may have moved')
-  end
+        click_on 'awesomeGuide'
 
-  scenario 'visit guide by id, not in path' do
-    visit "/guides/#{guide_not_in_path.id}"
-    expect(page).to have_text('You may have mistyped the address or the page may have moved')
-  end
+        expect(page).to have_text('awesomeGuide')
+        expect(page).to have_text('An awesome guide')
+        expect(page).to have_text('Content')
 
-  scenario 'visit guide by id, unknown guide' do
-    visit '/guides/900000'
-    expect(page).to have_text('You may have mistyped the address or the page may have moved')
-  end
+        click_on 'Start this lesson'
 
-  scenario 'visit guides from search page, and starts practicing' do
-    visit '/guides'
+        expect(page).to have_text('Description of foo')
+      end
+    end
 
-    click_on 'awesomeGuide'
+    context 'with authors' do
+      let(:authors) { 'Jon Doe' }
 
-    expect(page).to have_text('awesomeGuide')
-    expect(page).to have_text('An awesome guide')
-    expect(page).to have_text('Content')
+      scenario 'visit lesson by id' do
+        visit "/guides/#{lesson.guide.id}"
 
-    click_on 'Start this lesson'
+        expect(page).to have_text('awesomeGuide')
+        expect(page).to have_text('Content')
+        expect(page).to have_text('Jon Doe')
+        expect(page).to have_text('Creative Commons')
+      end
 
-    expect(page).to have_text('Description of foo')
+
+      scenario 'visit lesson, then exercise' do
+        visit "/guides/#{lesson.guide.id}"
+        click_on 'Foo'
+
+        expect(page).to have_text('Description of foo')
+        expect(page).to have_text('Jon Doe')
+        expect(page).to have_text('Creative Commons')
+      end
+    end
   end
 end
