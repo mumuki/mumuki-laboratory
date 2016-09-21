54c992a87f0771ec5aeada3b1e4ef790ee1baa57
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-20 22:24:38 -0300

    Prevent tabs focus change
    Fixes #667

--- app/helpers/concerns/with_editor_tabs.rb
+++ app/helpers/concerns/with_editor_tabs.rb
@@ -1,10 +1,10 @@
 module WithEditorTabs
   def extra_code_tab
 
-"<li role='presentation'> <a href='#visible-extra' aria-controls='visible-extra' role='tab' data-toggle='tab' class='editor-tab'>#{fa_icon 'code'} #{t 'activerecord.attributes.exercise.extra'}</a> </li>".html_safe
+"<li role='presentation'> <a data-target='#visible-extra' aria-controls='visible-extra' role='tab' data-toggle='tab' class='editor-tab'>#{fa_icon 'code'} #{t 'activerecord.attributes.exercise.extra'}</a> </li>".html_safe
   end
 
   def console_tab
-"<li role='presentation'> <a href='#console' aria-controls='console' role='tab' data-toggle='tab' class='editor-tab'>#{fa_icon 'terminal'} #{t :console }</a> </li>" .html_safe
+"<li role='presentation'> <a data-target='#console' aria-controls='console' role='tab' data-toggle='tab' class='editor-tab'>#{fa_icon 'terminal'} #{t :console }</a> </li>" .html_safe
   end
 end
