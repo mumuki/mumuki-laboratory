ea80ce97de773722c55b03e9c4933d1a5a2cb726
Federico Scarpa <fedescarpa@users.noreply.github.com>
2016-10-04 15:17:43 -0300

    Revert "Queue commands"

--- app/models/atheneum/command/update_user_metadata.rb
+++
@@ -1,5 +0,0 @@
-class Atheneum::Command::UpdateUserMetadata
-  def self.execute!(body)
-    User.find_by(uid: body['social_id']).revoke!
-  end
-end
--- app/models/notification_mode/nuntius.rb
+++ app/models/notification_mode/nuntius.rb
@@ -2,8 +2,4 @@ class NotificationMode::Nuntius
   def notify!(queue_name, event)
     Mumukit::Nuntius::Publisher.publish queue_name, event
   end
-
-  def notify_command!(queue_name, data, type)
-    Mumukit::Nuntius::CommandPublisher.publish queue_name, data, type
-  end
 end
