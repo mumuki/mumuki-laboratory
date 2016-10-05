93aff799801463387c274c9d8bcab672393c8fdd
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-10-05 01:03:47 -0300

    Refactoring editor types

--- app/helpers/concerns/with_exercise_form_rendering.rb
+++
@@ -1,9 +0,0 @@
-module WithExerciseFormRendering
-  def exercise_form(exercise, options)
-    render "layouts/inputs/exercises/#{exercise.class.name.underscore}_form", options
-  end
-
-  def editor_form(exercise)
-    render "layouts/inputs/exercises/editors/#{exercise.editor}_form"
-  end
-end
