bfba47f88a3d6a11b4824944b9a0c6bad53cd49e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-10-18 13:23:48 -0300

    Extracting download component

---
+++ app/helpers/concerns/with_solution_download.rb
@@ -0,0 +1,15 @@
+module WithSolutionDownload
+  def solution_download_link(assignment)
+    link_to fa_icon(:download, text: t(:download)),
+            solution_octet_data(assignment),
+            download: solution_filename(assignment)
+  end
+
+  def solution_octet_data(assignment)
+    "data:application/octet-stream,#{URI.encode assignment.solution}"
+  end
+
+  def solution_filename(assignment)
+    "solution.#{assignment.extension}"
+  end
+end
