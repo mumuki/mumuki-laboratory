dd447f7b366485e9aa558dbfa4a4fdec6140f2ce
Agustin Pina <aguspina87@gmail.com>
2016-10-20 14:22:37 -0300

    Do not show exercises from another book

--- app/controllers/concerns/with_exercise_index.rb
+++ app/controllers/concerns/with_exercise_index.rb
@@ -2,7 +2,7 @@ module WithExerciseIndex
 
   def set_exercises
     @q = params[:q]
-    @exercises = paginated exercises.by_full_text(@q).reorder(submissions_count: :desc)
+    @exercises = paginated exercises.by_full_text(@q).select {|e| e.used_in?(Organization.current).present? }.sort! {|e1, e2| e2.submissions_count <=> e1.submissions_count}
   end
 
 end
