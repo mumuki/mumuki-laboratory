21bc93ca769d94cb41d8b66a025138030dd540d5
Federico Scarpa <fedescarpa@gmail.com>
2016-11-07 14:59:57 -0300

    removed ahahamojimoji :cry:

--- app/helpers/concerns/with_breadcrumbs.rb
+++ app/helpers/concerns/with_breadcrumbs.rb
@@ -11,6 +11,6 @@ module WithBreadcrumbs
   end
 
   def home_breadcrumb
-    "<li><span class=\"ahahamojimoji\">#{link_to 'ム', root_path }</span></li>".html_safe
+    "<li>#{link_to '<i class="brand da da-mumuki"></i>'.html_safe, root_path }</li>".html_safe
   end
 end
