51f22801ef98e3a419ab80fcadcdc1970ba554fc
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-11 15:31:25 -0300

    Supporting www hosts

---
+++ config/initializers/uri.rb
@@ -0,0 +1,13 @@
+class URI::HTTP
+  def add_subdomain(subdomain)
+    if host.start_with? 'www.'
+      new_host = host.gsub('www.', "www.#{subdomain}.")
+    else
+      new_host = "#{subdomain}.#{host}"
+    end
+    URI::HTTP.build(scheme: scheme,
+                    host: new_host,
+                    path: path,
+                    query: query)
+  end
+end
--- spec/models/uri_spec.rb
+++ spec/models/uri_spec.rb
@@ -1,23 +1,12 @@
 require 'spec_helper'
 
-
-class URI::HTTP
-  def add_subdomain(subdomain)
-    URI::HTTP.build(scheme: scheme,
-                    host: "#{subdomain}.#{host}",
-                    path: path,
-                    query: query)
-  end
-end
-
 def add_extra_subdomain(original_url)
   URI(original_url).add_subdomain('extra').to_s
 end
 
-
 describe URI do
   it { expect(add_extra_subdomain('http://foo.bar')).to eq 'http://extra.foo.bar' }
   it { expect(add_extra_subdomain('http://foo.bar/zaraza')).to eq 'http://extra.foo.bar/zaraza' }
-  skip { expect(add_extra_subdomain('http://www.foo.bar.com/')).to eq 'http://www.extra.foo.bar.com/' }
+  it { expect(add_extra_subdomain('http://www.foo.bar.com/')).to eq 'http://www.extra.foo.bar.com/' }
   it { expect(add_extra_subdomain('http://foo.bar.com/foo?z=3')).to eq 'http://extra.foo.bar.com/foo?z=3' }
 end
