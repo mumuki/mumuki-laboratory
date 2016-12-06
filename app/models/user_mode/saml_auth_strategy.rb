f28edc1c4d3c9da699e146d3c20b1ddc4fc5ff42
Gonza Gluzman <dggluz@gmail.com>
2016-12-06 03:27:43 -0300

    Fixing bugs after conflicting rebase

--- app/models/user_mode/saml_auth_strategy.rb
+++ app/models/user_mode/saml_auth_strategy.rb
@@ -4,7 +4,7 @@ class UserMode::SamlAuthStrategy < UserMode::MultiUser
       # TODO: change the :assertion_consumer_service_url:
       # =>  1. we can not call any Organization method since there is none instantiated yet and
       # =>  2. we must use the absolut path to generate the right SAML metadata to set up the federation with the IdP
-      :assertion_consumer_service_url     => "http://central.#{Rails.configuration.domain_url}/auth/saml/callback",
+      :assertion_consumer_service_url     => "http://central.#{Rails.configuration.domain}:3000/auth/saml/callback",
       :issuer                             => "Mumuki",
       :idp_sso_target_url                 => Rails.configuration.saml_idp_sso_target_url,
       :idp_cert                           => File.read('./saml.crt')
@@ -15,7 +15,8 @@ class UserMode::SamlAuthStrategy < UserMode::MultiUser
   end
 
   def auth_link
-    'href="' + Organization.absolute_link('/auth/saml/') + '"'
+    # Was using Organization.url_for('/auth/saml/') but the port got lost (is it a bug?)
+    'href="http://central.' + Rails.configuration.domain + ':3000/auth/saml/"'
   end
 
 end
