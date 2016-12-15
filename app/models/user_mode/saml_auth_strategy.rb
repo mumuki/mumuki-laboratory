6af427ee9fa69d50f75085ed573f8ae188452a2a
Gonza Gluzman <dggluz@gmail.com>
2016-12-15 15:28:54 -0300

    Specifying the needed version of omniauth-saml and using the right parameters to configure it

--- app/models/user_mode/saml_auth_strategy.rb
+++ app/models/user_mode/saml_auth_strategy.rb
@@ -5,9 +5,20 @@ class UserMode::SamlAuthStrategy < UserMode::MultiUser
       # =>  1. we can not call any Organization method since there is none instantiated yet and
       # =>  2. we must use the absolut path to generate the right SAML metadata to set up the federation with the IdP
       :assertion_consumer_service_url     => "http://central.#{Rails.configuration.domain}:3000/auth/saml/callback",
-      :issuer                             => "Mumuki",
+      :issuer                             => "http://central.#{Rails.configuration.domain}:3000/auth/saml",
       :idp_sso_target_url                 => Rails.configuration.saml_idp_sso_target_url,
-      :idp_cert                           => File.read('./saml.crt')
+      :idp_cert                           => File.read('./saml.crt'),
+      :attribute_service_name             => 'Mumuki',
+      :request_attributes                 => [
+        { :name => 'email', :name_format => 'urn:oasis:names:tc:SAML:2.0:attrname-format:basic', :friendly_name => 'Email address' },
+        { :name => 'name', :name_format => 'urn:oasis:names:tc:SAML:2.0:attrname-format:basic', :friendly_name => 'Full name' },
+        { :name => 'image', :name_format => 'urn:oasis:names:tc:SAML:2.0:attrname-format:basic', :friendly_name => 'Avatar image' }
+      ],
+      :attribute_statements               => {
+        name: ["name"],
+        email: ["email"],
+        image: ["image"]
+      }
   end
 
   def protect_from_forgery(controller)
