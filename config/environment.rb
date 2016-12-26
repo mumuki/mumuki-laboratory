f401eb127b6fced723ac60f8ffe6700d15288817
Agustin Pina <aguspina87@gmail.com>
2016-12-26 16:02:14 -0300

    Upgrade nuntius

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,7 @@ Mumukit::Auth.configure do |c|
   c.client_secret = Rails.configuration.auth_client_secret
   c.daybreak_name = Rails.configuration.daybreak_name
 end
+
+Mumukit::Nuntius.configure do |c|
+  c.app_name = 'atheneum'
+end
