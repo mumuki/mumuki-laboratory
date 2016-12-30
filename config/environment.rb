0c26ee031625677dc89f077d2ac99008749f18a7
Agustin Pina <aguspina87@gmail.com>
2016-12-30 02:50:30 -0300

    Fix env

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,8 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 Mumukit::Auth.configure do |c|
-  c.client_id = Rails.configuration.auth_client_id
-  c.client_secret = Rails.configuration.auth_client_secret
+  c.client_id = Rails.configuration.auth0_client_id
+  c.client_secret = Rails.configuration.auth0_client_secret
   c.daybreak_name = Rails.configuration.daybreak_name
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.auth_client_id = ENV['MUMUKI_AUTH0_CLIENT_ID']
-  config.auth_client_secret = ENV['MUMUKI_AUTH0_CLIENT_SECRET']
-  config.auth_domain = ENV['MUMUKI_AUTH0_DOMAIN']
+  config.auth0_client_id = ENV['MUMUKI_AUTH0_CLIENT_ID']
+  config.auth0_client_secret = ENV['MUMUKI_AUTH0_CLIENT_SECRET']
+  config.auth0_domain = ENV['MUMUKI_AUTH0_DOMAIN']
 
   config.thesaurus_url = ENV['MUMUKI_THESAURUS_URL'] || 'http://thesaurus.mumuki.io'
   config.bibliotheca_url = ENV['MUMUKI_BIBLIOTHECA_URL'] || 'http://bibliotheca-api.mumuki.io'
