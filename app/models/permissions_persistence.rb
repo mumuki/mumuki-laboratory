037ef596b12136a535691436e7576c1b255c4970
Agustin Pina <aguspina87@gmail.com>
2016-12-31 20:10:34 -0300

    Add polimorfism methods

--- app/models/permissions_persistence.rb
+++ app/models/permissions_persistence.rb
@@ -1,10 +1,20 @@
 class PermissionsPersistence
 
+  def self.from_env
+    new
+  end
+
   def set!(uid, permissions)
     User.find_by(uid).update!(permissions: permissions)
   end
 
   def get(uid)
     User.find_by(uid: uid).permissions
+  end
+
+  def close
+  end
+
+  def clean_env!
   end
 end
