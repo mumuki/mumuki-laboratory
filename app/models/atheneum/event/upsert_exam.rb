67a3c29dd9e06c9af5474ee254ff63031807cad7
Agustin Pina <aguspina87@gmail.com>
2017-01-04 16:35:46 -0300

    Fix upsert exam

--- app/models/atheneum/event/upsert_exam.rb
+++ app/models/atheneum/event/upsert_exam.rb
@@ -6,23 +6,23 @@ module Atheneum
       end
 
       def self.import_from_json!(json)
-        organization = Organization.find_by!(name: json.delete('tenant'))
+        organization = Organization.find_by!(name: json.delete(:tenant))
         organization.switch!
         exam_data = parse_json json
-        remove_previous_version exam_data['id'], exam_data['guide_id']
-        users = exam_data.delete('users')
-        exam = Exam.where(classroom_id: exam_data.delete('id')).update_or_create! exam_data
+        remove_previous_version exam_data[:id], exam_data[:guide_id]
+        users = exam_data.delete(:users)
+        exam = Exam.where(classroom_id: exam_data.delete(:id)).update_or_create! exam_data
         exam.process_users users
         exam.index_usage! organization
         exam
       end
 
       def self.parse_json(exam_json)
-        exam = exam_json.except('name', 'language')
-        exam['guide_id'] = Guide.find_by(slug: exam.delete('slug')).id
-        exam['organization_id'] = Organization.id
-        exam['users'] = exam.delete('uids').map { |uid| User.find_by(uid: uid) }.compact
-        ['start_time', 'end_time'].each { |param| exam[param] = exam[param].to_time }
+        exam = exam_json.except(:name, :language)
+        exam[:guide_id] = Guide.find_by(slug: exam.delete(:slug)).id
+        exam[:organization_id] = Organization.id
+        exam[:users] = exam.delete(:uids).map { |uid| User.find_by(uid: uid) }.compact
+        [:start_time, :end_time].each { |param| exam[param] = exam[param].to_time }
         exam
       end
 
