c2908e89e3944baebfd4b6eabb7bfd6e89aadff8
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-01-20 16:38:21 -0300

    Adding FIXME

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -3,6 +3,7 @@ module Authorization
     return if Organization.public?
     return if from_login_callback?
 
+    # FIXME use login provider here
     render file: 'layouts/login' and return unless current_user?
     raise Exceptions::OrganizationPrivateError if !current_user.student? && !from_logout?
   end
