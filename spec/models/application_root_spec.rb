90d9bd8436c06ccdde088be98e9f155ec7ce7fde
Agustin Pina <aguspina87@gmail.com>
2017-01-26 17:05:47 -0300

    Fix specs

--- spec/models/application_root_spec.rb
+++ spec/models/application_root_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe ApplicationRoot do
-  it { expect(ApplicationRoot.url_for 'foo', '/foo/baz').to eq 'http://foo.localmumuki.io:3000/foo/baz' }
+  it { expect(ApplicationRoot.laboratory.url_for 'foo', '/foo/baz').to eq 'http://foo.localmumuki.io:3000/foo/baz' }
+  it { expect(ApplicationRoot.classroom.url_for 'foo', '/foo/baz').to eq 'http://foo.classroom.mumuki.io/foo/baz' }
+  it { expect(ApplicationRoot.office.base_url).to eq 'http://office.mumuki.io' }
 end
