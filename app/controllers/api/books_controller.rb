3102e16111769b5ed3ed808f463703efc0b20afd
Franco Bulgarelli <franco@mumuki.org>
2017-01-30 19:00:13 -0300

    Removing controllers

--- app/controllers/api/books_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::BooksController < Api::BaseController
-  def create
-    book = Book.import!(params[:slug])
-    render json: { topic: book.as_json }
-  end
-end
--- app/controllers/api/topics_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::TopicsController < Api::BaseController
-
-  def create
-    topic = Topic.import!(params[:slug])
-    render json: { topic: topic.as_json }
-  end
-
-end
