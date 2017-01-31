d052a94b3d90a5755914d56b2725dc8cac505674
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-01-31 17:50:24 -0300

    Moving methods to NavigationHelper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,4 +1,12 @@
 module NavigationHelper
+  def next_lesson_button(guide)
+    next_button(guide.lesson) || chapter_finished(guide.chapter)
+  end
+
+  def next_exercise_button(exercise)
+    next_button(exercise) || next_button(exercise.guide.lesson)
+  end
+
   def next_button(navigable)
     return unless navigable
     ContinueNavigation.new(self).button(navigable) || RevisitNavigation.new(self).button(navigable)
