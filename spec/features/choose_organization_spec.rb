b5ff55d3b0e6d43dd5766701b987d771d2c3814e
Agustin Pina <aguspina87@gmail.com>
2017-02-09 12:13:59 -0300

    Fix specs

--- spec/features/choose_organization_spec.rb
+++ spec/features/choose_organization_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 feature 'Choose organization Flow' do
 
+  let(:chapter) { create(:chapter, lessons: [create(:lesson)]) }
+  let(:book) { create(:book, chapters: [chapter], name: 'foo', slug: 'mumuki/mumuki-the-foo-book') }
+  let(:foo_organization) { create(:organization, name: 'foo', book: book) }
+
   let(:user) { create(:user, permissions: {student: 'pdep/*'}) }
   let(:user2) { create(:user, permissions: {student: ''}) }
 
@@ -29,11 +33,12 @@ feature 'Choose organization Flow' do
     expect(page).not_to have_text('pdep')
   end
 
-  scenario 'when visit test subdomain' do
+  scenario 'when visit foo subdomain' do
+    set_subdomain_host!('foo')
     visit '/'
 
     expect(page).not_to have_text('Do you want to go there?')
-    expect(page).not_to have_text('pdep')
+    expect(page).not_to have_text('foo')
   end
 
   scenario 'when user does not have any permission' do
