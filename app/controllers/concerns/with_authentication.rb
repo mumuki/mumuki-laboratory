a8d6ceb39043473bfce647fa47266a9aedf134da
Agustin Pina <aguspina87@gmail.com>
2017-02-09 14:40:25 -0300

    Uncomment current user code

--- app/controllers/concerns/with_authentication.rb
+++ app/controllers/concerns/with_authentication.rb
@@ -1,11 +1,11 @@
 module WithAuthentication
   include Mumukit::Login::AuthenticationHelpers
 
-  #def current_user_uid
-  #  @current_user_uid ||= remember_me_token.value.try do |token|
-  #    User.where(remember_me_token: token).first.try(:uid)
-  #  end
-  #end
+  def current_user_uid
+    @current_user_uid ||= remember_me_token.value.try do |token|
+      User.where(remember_me_token: token).first.try(:uid)
+    end
+  end
 
   def login_button(options={})
     login_form.button_html I18n.t(:sign_in), options[:class]
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,12 +7,12 @@ class LoginController < ApplicationController
     @error_msg = request.params['message']
   end
 
-  #
-  # def destroy_current_user_session!
-  #   remember_me_token.clear!
-  # end
-  #
-  # def save_current_user_session!(user)
-  #   remember_me_token.value = user.remember_me_token
-  # end
+
+  def destroy_current_user_session!
+    remember_me_token.clear!
+  end
+
+  def save_current_user_session!(user)
+    remember_me_token.value = user.remember_me_token
+  end
 end
