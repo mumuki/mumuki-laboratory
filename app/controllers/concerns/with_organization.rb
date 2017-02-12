2c1596940196f8724d9058f5cc3d1280129a0781
Franco Bulgarelli <franco@mumuki.org>
2017-02-11 21:58:06 -0300

    Decoupling the subdomain logic from mumukit-core

--- app/controllers/concerns/with_organization.rb
+++ app/controllers/concerns/with_organization.rb
@@ -1,10 +1,13 @@
 module WithOrganization
   def set_organization!
-    Organization.find_by!(name: request.organization_name).switch!
+    Organization.find_by!(name: first_subdomain).switch!
+  end
+
+  def first_subdomain
+    request.first_subdomain_after(Rails.configuration.domain) || 'central'
   end
 
   def visit_organization!
     current_user.visit!(Organization.current)
   end
-
 end
