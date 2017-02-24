62188876090578f55c8efbedc4dcafed9438dd1f
Rodrigo Alfonso <rodri042@gmail.com>
2017-02-24 03:14:51 -0300

    Designing invitation view

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,20 @@
 class InvitationsController < ApplicationController
   before_action :authenticate!
+  before_action :set_user!
+  before_action :set_invitation!
 
   def show
+  end
+
+  def join
+  end
+
+  def set_user!
+    @user = current_user
+  end
+
+  def set_invitation!
+    @invitation = Invitation.find_by_slug params[:invitation]
+    @organization = @invitation.organization
   end
 end
