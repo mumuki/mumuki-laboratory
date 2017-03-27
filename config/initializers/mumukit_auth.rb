99e15cd8ceb761dd3effa9f8c5e0175d4b0113e3
Agustin Pina <aguspina87@gmail.com>
2017-03-27 16:14:10 -0300

    Make accessible organizations more expressive

--- config/initializers/mumukit_auth.rb
+++ config/initializers/mumukit_auth.rb
@@ -1,6 +1,6 @@
 class Mumukit::Auth::Permissions
   def accessible_organizations
-    scope_for(:student)&.grants&.map { |grant| grant.instance_variable_get(:@first) }.to_set
+    scope_for(:student)&.grants&.map { |grant| grant.to_mumukit_slug.organization }.to_set
   end
 end
 
