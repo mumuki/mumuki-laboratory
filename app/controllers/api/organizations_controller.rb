ca6627dfded9bb500839eaa4b71c1d080208e253
Agustin Pina <aguspina87@gmail.com>
2017-04-12 12:01:48 -0300

    Remove organizations api controller

--- app/controllers/api/organizations_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::OrganizationsController < Api::BaseController
-  include WithOrganization
-  before_action :set_organization!
-
-  def index
-    organization = Organization.current
-    render json: { organization: organization.as_json(except: [:login_methods]).merge(locale: organization.locale, lock_json: organization.login_settings.lock_json) }
-  end
-end
