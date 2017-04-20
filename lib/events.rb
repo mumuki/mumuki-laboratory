1e502e46c03b69f9eb9791a88b39aaeb144786dc
Agustin Pina <aguspina87@gmail.com>
2017-04-20 05:20:27 -0300

    Fix nuntius events

--- lib/events.rb
+++ lib/events.rb
@@ -1,17 +1,17 @@
 Mumukit::Nuntius::EventConsumer.handle do
-  event :UserChanged do |payload|
+  event 'UserChanged' do |payload|
     User.import_from_json! payload.deep_symbolize_keys[:user]
   end
 
-  event :OrganizationCreated do |payload|
+  event 'OrganizationCreated' do |payload|
     Organization.create_from_json! payload.deep_symbolize_keys[:organization]
   end
 
-  event :OrganizationChanged do |payload|
+  event 'OrganizationChanged' do |payload|
     Organization.update_from_json! payload.deep_symbolize_keys[:organization]
   end
 
-  event :UpsertExam do |body|
+  event 'UpsertExam' do |body|
     Exam.import_from_json! body
   end
 
