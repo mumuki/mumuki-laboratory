c8d48e9a76b8f3fb1d177b0b598ea3b659bd8257
Franco Bulgarelli <franco@mumuki.org>
2017-04-30 11:59:52 -0300

    Using platform's organization mappers

--- config/initializers/mumukit_platform.rb
+++ config/initializers/mumukit_platform.rb
@@ -1,3 +1,4 @@
 Mumukit::Platform.configure do |config|
   config.application = Mumukit::Platform.laboratory
+  config.web_framework = Mumukit::Platform::WebFramework::Rails
 end
--- config/initializers/organization_mapper.rb
+++
@@ -1,9 +0,0 @@
-module OrganizationMapper
-  def self.configure_application_routes(mapper, &block)
-    mapper.instance_eval(&block)
-  end
-
-  def self.extract_organization_name(request)
-    request.first_subdomain_after(Rails.configuration.domain) || 'central'
-  end
-end
