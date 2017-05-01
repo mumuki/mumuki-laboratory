275cfe0fc0caf9c74c88826d4469b513ec49a4c3
Franco Bulgarelli <franco@mumuki.org>
2017-05-01 15:57:06 -0300

    Ensuring a proper 404 error on org's path mappings

--- spec/features/not_found_flow_spec.rb
+++ spec/features/not_found_flow_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
 
 feature 'Choose organization Flow' do
+  let!(:central) { create(:organization, name: 'central') }
 
-  scenario 'when routes does not exist' do
+  scenario 'when routes does not exist in implicit central' do
     visit '/foo'
+
+    expect(page).to have_text('You may have mistyped the address')
+  end
+
+  scenario 'when route does not exist in explicit central' do
+    set_subdomain_host! 'test'
+
+    visit '/foo'
+
+    expect(page).to have_text('You may have mistyped the address')
+  end
+
+  scenario 'when organization does not exist' do
+    set_subdomain_host! 'foo'
+
+    visit '/'
 
     expect(page).to have_text('You may have mistyped the address')
   end
