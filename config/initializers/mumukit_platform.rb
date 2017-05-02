02b4f9d9773896d24b9325262aae671e40f68c56
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-05-02 17:38:50 -0300

    Fix: login in path mapping

--- config/initializers/mumukit_platform.rb
+++ config/initializers/mumukit_platform.rb
@@ -2,3 +2,18 @@ Mumukit::Platform.configure do |config|
   config.application = Mumukit::Platform.laboratory
   config.web_framework = Mumukit::Platform::WebFramework::Rails
 end
+
+module Mumukit::Platform::OrganizationMapping::Path
+  class << self
+    alias_method :__organization_name__, :organization_name
+
+    def organization_name(request, domain)
+      name = __organization_name__(request, domain)
+      if %w(auth login logout).include? name
+        'central'
+      else
+        name
+      end
+    end
+  end
+end
