e7ed9e62366ea47c818a0d9714136f1e5cc1a12d
Agustin Pina <aguspina87@gmail.com>
2017-05-29 15:41:41 -0300

    Remove unused code

--- app/helpers/markdown_helper.rb
+++
@@ -1,9 +0,0 @@
-module MarkdownHelper
-  def code_to_markdown(language, code)
-    Mumukit::ContentType::Markdown.highlighted_code language, code
-  end
-
-  def markdown_to_html(language, code)
-    Mumukit::ContentType::Markdown.to_html code_to_markdown(language, code)
-  end
-end
