842e3a0e36d38f02df1e7876cf02f97018220d0b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-06-08 11:50:11 -0300

    Only sending questions when necessary

--- app/models/concerns/submittable/questionable.rb
+++ app/models/concerns/submittable/questionable.rb
@@ -1,6 +1,6 @@
 module Questionable
   def submit_question!(user, question)
-    submit! user, Question.new 
+    submit! user, Question.new unless assigned_to? user
     assignment_for(user).send_question! question
   end
 end
