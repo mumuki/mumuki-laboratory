e5765b67094fdb1a4ee6644b85cfd2f108a72844
Agustin Pina <aguspina87@gmail.com>
2017-08-11 13:12:05 -0300

    Migrate data inside migrations

--- db/migrate/20170621222902_introduce_settings_and_themes.rb
+++ db/migrate/20170621222902_introduce_settings_and_themes.rb
@@ -1,8 +1,29 @@
 class IntroduceSettingsAndThemes < ActiveRecord::Migration
   def change
-    add_column :organizations, :settings,  :text, default: "{}", null: false
-    add_column :organizations, :theme,     :text, default: "{}", null: false
-    add_column :organizations, :profile,   :text, default: "{}", null: false
+    add_column :organizations, :settings, :text, default: "{}", null: false
+    add_column :organizations, :theme, :text, default: "{}", null: false
+    add_column :organizations, :profile, :text, default: "{}", null: false
+
+    Organization.all.each do |organization|
+      organization.profile = Mumukit::Platform::Organization::Profile.new(
+        logo_url: organization[:logo_url],
+        locale: organization[:locale],
+        description: organization[:description],
+        contact_email: organization[:contact_email],
+        terms_of_service: organization[:terms_of_service],
+        community_link: organization[:community_link]
+      )
+      organization.settings = Mumukit::Platform::Organization::Settings.new(
+        login_methods: organization[:login_methods],
+        raise_hand_enabled: organization[:raise_hand_enabled],
+        public: organization[:public]
+      )
+      organization.theme = Mumukit::Platform::Organization::Theme.new(
+        theme_stylesheet_url: organization[:theme_stylesheet_url],
+        extension_javascript_url: organization[:extension_javascript_url]
+      )
+      organization.save!
+    end
 
     remove_column :organizations, :login_methods
     remove_column :organizations, :raise_hand_enabled
