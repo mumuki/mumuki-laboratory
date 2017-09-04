b10a825eab1e0f28facdc023e11bff74ac6ae998
Federico Scarpa <fedescarpa@gmail.com>
2017-09-04 20:18:46 -0300

    addeding haskell keywords autocompletion

---
+++ app/assets/javascripts/application/codemirror-modes/haskell.js
@@ -0,0 +1,5 @@
+registerAutocomplete('haskell', (
+    'as case of class data data family data instance default deriving deriving ' +
+    'instance do forall foreign hiding if then else import infix infixl infixr instance ' +
+    'let in mdo module newtype proc qualified rec type type family type instance where'
+).split(' '));
