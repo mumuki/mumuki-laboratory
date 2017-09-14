f5720eec483c1857c2a56a38f30d21aafb1d9e38
Julian Berbel Alt <julianberbel@hotmail.com>
2017-09-14 16:04:21 -0300

    Few more improvements
    Set language to current organization langugage
    Make editor reset button focusable and describe it
    Add home locale entry and parametrize link description in breadcrumb

--- app/helpers/reset_button_helper.rb
+++ app/helpers/reset_button_helper.rb
@@ -1,6 +1,6 @@
 module ResetButtonHelper
   def reset_button
-    fa_icon('trash-o', class: 'fa-fw')
+    fa_icon('trash-o', {class: 'fa-fw', role: 'button', tabindex: '0', 'aria-label': 'Reiniciar'})
   end
 
 end
