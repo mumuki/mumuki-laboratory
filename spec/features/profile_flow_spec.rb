15e14907b4983afa32380657ebd22377a4d830bd
Julian Berbel Alt <julianberbel@hotmail.com>
2017-11-02 15:12:47 -0300

    Test placeholders show up when appropriate

--- spec/features/profile_flow_spec.rb
+++ spec/features/profile_flow_spec.rb
@@ -10,7 +10,21 @@ feature 'Standard Flow' do
             create(:exercise, name: 'The Basic Values', description: "Let's say we want to declare a variable...")
         ])
     ]) }
-  before { reindex_current_organization! }
+  let(:problem) { create :problem}
+  let(:message) {
+    {'exercise_id' => problem.id,
+     'submission_id' => problem.assignments.last.submission_id,
+     'organization' => 'test-organization',
+     'message' => {
+       'sender' => 'test-email@gmail.com',
+       'content' => 'a',
+       'created_at' => '1/1/1'}} }
+  let(:organization) { create(:organization, name: 'test-organization') }
+
+  before do
+    reindex_organization! organization
+    reindex_current_organization!
+  end
 
   before do
     visit '/'
@@ -37,6 +51,44 @@ feature 'Standard Flow' do
   scenario 'do not redirect to /user if user do not have organizations' do
     click_on 'Sign in'
     expect(page).not_to have_text('Please complete your profile data to continue!')
+  end
+
+  context 'logged in user' do
+    before { set_current_user! user }
+
+    context 'with no organizations nor messages' do
+      scenario 'visit organizations tab' do
+        visit "/user#organizations"
+
+        expect(page).to have_text('It seems you aren\'t in any organizations yet!')
+      end
+
+      scenario 'visit messages tab' do
+        visit "/user#messages"
+
+        expect(page).to have_text('It seems you don\'t have any messages yet!')
+      end
+    end
+
+    context 'with organizations and messages' do
+      scenario 'visit organizations tab' do
+        user.make_student_of! organization.slug
+        user.save!
+        visit "/user#organizations"
+
+        expect(page).to_not have_text('It seems you aren\'t in any organizations yet!')
+        expect(page).to have_text('Go to test-organization')
+      end
+
+      scenario 'visit messages tab' do
+        problem.submit_solution! user, {content: 'something'}
+        Message.import_from_json! message
+        visit "/user#messages"
+
+        expect(page).to_not have_text('It seems you don\'t have any messages yet!')
+        expect(page).to have_text(problem.name)
+      end
+    end
   end
 
 end
