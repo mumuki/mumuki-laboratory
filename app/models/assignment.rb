bb37627c41b06bbb201557fa605e369e2dd16b15
Federico Scarpa <fedescarpa@gmail.com>
2017-11-10 12:45:30 -0300

    added manual evaluation event test

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -33,7 +33,7 @@ class Assignment < ActiveRecord::Base
   end
 
   def evaluate_manually!(teacher_evaluation)
-    update! status: teacher_evaluation[:status], manual_evaluation_comment: teacher_evaluation[:manual_evaluation]
+    update! status: Status.from_sym(teacher_evaluation[:status]), manual_evaluation_comment: teacher_evaluation[:manual_evaluation]
   end
 
   def single_visual_result?
