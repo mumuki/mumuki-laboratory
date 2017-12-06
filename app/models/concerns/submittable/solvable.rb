37d2e5b8fcfadbb280130a9d6367e4fba864421f
Julian Berbel Alt <julianberbel@hotmail.com>
2017-12-06 17:34:20 -0300

    Refactor solution Hash to string conversion

--- app/models/concerns/submittable/solvable.rb
+++ app/models/concerns/submittable/solvable.rb
@@ -1,7 +1,7 @@
 module Solvable
   def submit_solution!(user, attributes={})
     content = attributes[:content]
-    attributes[:content] = serialize(content) if content.is_a? Hash
+    attributes[:content] = join_files(content) if content.is_a? Hash
     assignment, _ = find_assignment_and_submit! user, Solution.new(content: attributes[:content]&.normalize_whitespaces)
     assignment
   end
@@ -17,17 +17,17 @@ module Solvable
 
   private
 
-  def serialize(content)
+  def join_files(content)
     comment_type = Mumukit::Directives::CommentType.parse(language.comment_type)
-    file_declarations, file_references = content.map{|filename, content| method_name(comment_type, content, filename)}.transpose
-    "#{file_declarations.join "\n" }\n#{wrapWith 'content', file_references.join("\n"), comment_type}"
+    file_declarations, file_references = content.map{|filename, content| declare_and_reference(comment_type, content, filename)}.transpose
+    "#{file_declarations.join "\n" }\n#{wrap_with 'content', file_references.join("\n"), comment_type}"
   end
 
-  def method_name(comment_type, content, filename)
-    [wrapWith(filename, content, comment_type), (comment_type.comment "...#{filename}...")]
+  def declare_and_reference(comment_type, content, filename)
+    [wrap_with(filename, content, comment_type), (comment_type.comment "...#{filename}...")]
   end
 
-  def wrapWith(tag, text, comment_type)
+  def wrap_with(tag, text, comment_type)
     "#{comment_type.comment "<#{tag}#"}#{text}#{comment_type.comment "##{tag}>"}"
   end
 end
