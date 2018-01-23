acd8a08fcc4829764990a77d2ad20fd840dd1d37
Julián Berbel Alt <julianberbel@hotmail.com>
2018-01-23 11:12:01 -0300

    add test cases

--- spec/features/choose_organization_spec.rb
+++ spec/features/choose_organization_spec.rb
@@ -2,50 +2,75 @@ require 'spec_helper'
 
 feature 'Choose organization Flow' do
 
-  let(:chapter) { create(:chapter, lessons: [create(:lesson)]) }
-  let(:book) { create(:book, chapters: [chapter], name: 'foo', slug: 'mumuki/mumuki-the-foo-book') }
-  let(:foo_organization) { create(:organization, name: 'foo', book: book) }
+  let(:user) { create(:user, permissions: {student: ''}) }
+  let(:user2) { create(:user, permissions: {student: 'pdep/*'}) }
+  let(:user3) { create(:user, permissions: {student: 'pdep/*:foo/*'}) }
 
-  let(:user) { create(:user, permissions: {student: 'pdep/*'}) }
-  let(:user2) { create(:user, permissions: {student: ''}) }
+  before do
+    %w(pdep central foo).each do |it|
+      create(:organization,
+             name: it,
+             book: create(:book,
+                          chapters: [create(:chapter, lessons: [create(:lesson)])],
+                          name: it,
+                          slug: "mumuki/mumuki-the-#{it}-book"))
+    end
+  end
 
-  let(:organization) { create(:organization, name: 'central', book: create(:book, name: 'central', slug: 'mumuki/mumuki-the-book')).switch! }
-  before { create(:organization, name: 'pdep', book: create(:book, name: 'pdep', slug: 'mumuki/mumuki-the-pdep-book')) }
-  before { create(:organization, name: 'central', book: create(:book, name: 'central', slug: 'mumuki/mumuki-the-central-book')).switch! }
+  before { Organization.central.switch! }
   before { set_current_user! user }
 
-  scenario 'when visit implicit central' do
+  scenario 'when visiting with implicit subdomain and no permissions' do
     set_implicit_central!
 
     visit '/'
 
     expect(page).to have_text('Sign Out')
-    expect(page).to have_text('Do you want to go there?')
-    expect(page).to have_text('pdep')
+    expect(page).not_to have_text('Do you want to go there?')
   end
 
-  scenario 'when visit explicit central' do
+  scenario 'when visiting with implicit subdomain and permissions to only one organization' do
+    set_current_user! user2
+    set_implicit_central!
+
+    visit '/'
+
+    expect(page).to have_text('Sign Out')
+    expect(page).to have_text('You were redirected to your organization')
+    expect(page).to have_text('pdep')
+    expect(page).not_to have_text('Do you want to go there?')
+  end
+
+  scenario 'when visiting with implicit subdomain and permissions to two or more organizations' do
+    set_current_user! user3
+    set_implicit_central!
+
+    visit '/'
+
+    expect(page).to have_text('Sign Out')
+    expect(page).not_to have_text('You were redirected to your organization')
+    expect(page).to have_text('Do you want to go there?')
+  end
+
+  scenario 'when visiting central explicitly' do
     set_subdomain_host!('central')
 
     visit '/'
 
     expect(page).not_to have_text('Do you want to go there?')
+    expect(page).not_to have_text('You were redirected to your organization')
     expect(page).not_to have_text('pdep')
   end
 
   scenario 'when visit foo subdomain' do
+    set_current_user! user3
     set_subdomain_host!('foo')
+
     visit '/'
 
     expect(page).not_to have_text('Do you want to go there?')
-    expect(page).not_to have_text('foo')
+    expect(page).not_to have_text('You were redirected to your organization')
+    expect(page).to have_text('foo')
   end
 
-  scenario 'when user does not have any permission' do
-    set_current_user! user2
-    set_implicit_central!
-    visit '/'
-
-    expect(page).not_to have_text('Do you want to go there?')
-  end
 end
