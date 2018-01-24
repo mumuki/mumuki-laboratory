2995ea7dab49c47d14c843acc0fe4dd36dd691ff
Federico Scarpa <fedescarpa@gmail.com>
2018-01-24 14:55:24 -0300

    change autoload paths order

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Mumuki
 
     config.autoload_paths += %W(#{config.root}/app/helpers/concerns)
 
-    %w(evaluation exercise submission status).each do |it|
+    %w(status evaluation exercise submission).each do |it|
       config.autoload_paths += %W(#{config.root}/app/models/#{it})
     end
 
