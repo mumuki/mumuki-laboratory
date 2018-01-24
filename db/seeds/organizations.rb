ee7638f72373ac76177271f0ca1842616f24629c
Federico Scarpa <fedescarpa@gmail.com>
2018-01-24 12:02:39 -0300

    Added private organization to seed

--- db/seeds/organizations.rb
+++ db/seeds/organizations.rb
@@ -5,3 +5,11 @@ Organization.find_or_create_by!(name: 'central') do |org|
   org.login_methods = Mumukit::Login::Settings.login_methods
   org.locale = 'es'
 end
+
+Organization.find_or_create_by!(name: 'private') do |org|
+  org.contact_email = 'issues@mumuki.org'
+  org.book = Book.find_by!(slug: 'mumukiproject/mumuki-libro-aprende-a-programar-recorrido-extendido')
+  org.public = false
+  org.login_methods = Mumukit::Login::Settings.login_methods
+end
+
