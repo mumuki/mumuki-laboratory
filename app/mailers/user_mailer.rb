5aab1663d8eb1498fd8381aa4c00108dd8e9f1f4
Julián Berbel Alt <julianberbel@hotmail.com>
2018-01-25 13:22:25 -0300

    change mcn for muMail and parametrize unsubscription link domain

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,9 @@ class UserMailer < ApplicationMailer
   def we_miss_you_reminder(user, cycles)
     @user = user
     @unsubscribe_code = User.unsubscription_verifier.generate(user.id)
+    @organization = user.last_organization
 
-    I18n.with_locale(user.last_organization.locale) do
+    I18n.with_locale(@organization.locale) do
       mail to: user.email,
            subject: t(:we_miss_you),
            template_name: "#{cycles.ordinalize}_reminder"
