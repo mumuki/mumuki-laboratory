6be0244d7f0f632b52b59b5de1f7ef839211404b
Julián Berbel Alt <julianberbel@hotmail.com>
2018-01-29 12:36:33 -0300

    move seeds to lib

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,3 +1,9 @@
+require 'mumuki/laboratory/seed'
+
+Mumuki::Laboratory::Seed.import_languages!
+
+Mumuki::Laboratory::Seed.import_contents!
+
 Organization.find_or_create_by!(name: 'central') do |org|
   org.contact_email = 'issues@mumuki.org'
   org.book = Book.find_by!(slug: 'mumuki/mumuki-libro-programacion')
