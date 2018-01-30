4766230dd06fb9b3188133c8c36a4b72a9a65a31
Julián Berbel Alt <julianberbel@hotmail.com>
2018-01-30 16:52:51 -0300

    save date when reminded and include in should send reminder logic

--- app/models/concerns/with_reminders.rb
+++ app/models/concerns/with_reminders.rb
@@ -6,19 +6,23 @@ module WithReminders
 
   def send_reminder!(cycles)
     build_reminder(cycles).deliver
+    update! last_reminded: Time.now
   end
 
-  def cycles_since_last_submission
-    (Date.current - last_submission_date.to_date) / Rails.configuration.reminder_frequency
+  def cycles_since(time)
+    ((Date.current - time.to_date) / Rails.configuration.reminder_frequency).to_i
+  end
+
+  def remider_due?
+    last_reminded_date.nil? || cycles_since(last_reminded_date) >= 1
   end
 
   def should_send_reminder?
-    cycles = cycles_since_last_submission
-    cycles.denominator == 1 && cycles.between?(1, 3)
+    remider_due? && cycles_since(last_submission_date).between?(1, 3)
   end
 
   def remind!
-    send_reminder! cycles_since_last_submission.to_i if should_send_reminder?
+    send_reminder! cycles_since(last_submission_date) if should_send_reminder?
   end
 
 end
