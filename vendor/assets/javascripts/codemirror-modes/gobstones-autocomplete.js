2a5c005dc74b237947e42cdd80c383fa2d0772ac
Na <NadiaFinzi@users.noreply.github.com>
2018-02-01 11:58:16 -0300

    Update gobstones-autocomplete.js

--- vendor/assets/javascripts/codemirror-modes/gobstones-autocomplete.js
+++ vendor/assets/javascripts/codemirror-modes/gobstones-autocomplete.js
@@ -1,4 +1,4 @@
 registerAutocomplete('gobstones', (
   'program procedure function if else switch repeat while not return ' +
-  'False True Poner Sacar Mover Verde Rojo Azul Negro Norte Sur Este Oeste'
+  'False True Poner Colocar Sacar Retirar Mover Verde Rojo Vermelho Azul Negro Preto Norte Sur Sul Este Leste Oeste'
 ).split(' '));
