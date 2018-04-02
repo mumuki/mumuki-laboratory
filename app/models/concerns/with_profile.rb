bbada4197189a7474802f4b5d4e2d2da92ef3d33
Franco Bulgarelli <franco@mumuki.org>
2018-04-02 13:12:50 -0300

    Making profile name optional
    Allowing a smoother transition to v5

--- app/models/concerns/with_profile.rb
+++ app/models/concerns/with_profile.rb
@@ -4,9 +4,13 @@ module WithProfile
   module ClassMethods
     def for_profile(profile)
       where(uid: profile.uid).first_or_initialize.tap do |user|
-        user.assign_attributes(profile.to_h.compact)
+        user.assign_attributes(profile.to_h.except(:name).compact)
         user.save_and_notify_changes!
       end
     end
+  end
+
+  def name
+    self[:name] || "#{first_name} #{last_name}"
   end
 end
