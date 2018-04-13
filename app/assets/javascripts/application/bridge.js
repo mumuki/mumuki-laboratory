5608bf34f46492209db1101b47e38d67ce791159
Julián Berbel Alt <julianberbel@hotmail.com>
2018-04-13 17:04:22 -0300

    Modify bridge to return the same response when submitted solution is the same as the last

--- app/assets/javascripts/application/bridge.js
+++ app/assets/javascripts/application/bridge.js
@@ -1,19 +1,27 @@
 var mumuki = mumuki || {};
-(function (mumuki) {
 
+var lastSubmission = {};
+
+(function (mumuki) {
   function Laboratory(exerciseId){
     this.exerciseId = exerciseId;
   }
 
   Laboratory.prototype = {
     runLocalTests: function (solution) {
-      var token = new mumuki.CsrfToken();
-      var request = token.newRequest({
-        type: 'POST',
-        url: window.location.origin + window.location.pathname + '/solutions',
-        data: solution
-      });
-      return $.ajax(request)
+      if(lastSubmission.result && lastSubmission.result.status !== 'aborted' && JSON.stringify(lastSubmission.content) === JSON.stringify(solution)){
+        return $.Deferred().resolve(lastSubmission.result)
+      }else{
+        var token = new mumuki.CsrfToken();
+        var request = token.newRequest({
+          type: 'POST',
+          url: window.location.origin + window.location.pathname + '/solutions',
+          data: solution
+        });
+        return $.ajax(request).done(function (result) {
+          lastSubmission = { content: solution, result: result };
+        });
+      }
     },
     runTests: function(content) {
       return this.runLocalTests({ solution: content });
