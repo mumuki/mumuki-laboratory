45eca2965809f6a206c9dccdbf2c1edd1360deb2
Rodrigo Alfonso <rodri042@gmail.com>
2018-06-01 05:23:20 +0000

    Extracting a method and making others private

--- app/models/concerns/with_reminders.rb
+++ app/models/concerns/with_reminders.rb
@@ -11,6 +11,16 @@ module WithReminders
     update! last_reminded_date: Time.now
   end
 
+  def should_send_reminder?
+    remider_due? && (has_no_submissions? || has_no_recent_submission?)
+  end
+
+  def remind!
+    send_reminder! if should_send_reminder?
+  end
+
+  private
+
   def cycles_since(time)
     ((Date.current - time.to_date) / Rails.configuration.reminder_frequency).to_i
   end
@@ -19,20 +29,16 @@ module WithReminders
     last_reminded_date.nil? || cycles_since(last_reminded_date) >= 1
   end
 
+  def can_still_remind?(date)
+    cycles_since(date).between?(1, 3)
+  end
+
   def has_no_submissions?
-    last_submission_date.nil? && cycles_since(created_at).between?(1, 3)
+    last_submission_date.nil? && can_still_remind?(created_at)
   end
 
   def has_no_recent_submission?
-    !last_submission_date.nil? && cycles_since(last_submission_date).between?(1, 3)
-  end
-
-  def should_send_reminder?
-    remider_due? && (has_no_submissions? || has_no_recent_submission?)
-  end
-
-  def remind!
-    send_reminder! if should_send_reminder?
+    !last_submission_date.nil? && can_still_remind?(last_submission_date)
   end
 
 end
