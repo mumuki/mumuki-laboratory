a9dd419e198cbb8b2700626bcc401131cb28c474
Franco Bulgarelli <franco@mumuki.org>
2018-07-20 18:17:21 -0300

    Adding and fixing tests

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -2,8 +2,16 @@ require 'spec_helper'
 
 describe ChaptersController, type: :controller, organization_workspace: :test do
   describe 'when chapter does not exist' do
-    before { get(:show, params: {id: 0}) }
+    before { get :show, params: {id: 0, debatable_class: 'Chapter'} }
 
     it { expect(response.status).to eq 404 }
+  end
+
+  describe 'when chapter exists' do
+    let!(:chapter) { create(:chapter) }
+
+    before { get :show, params: {id: chapter.id, debatable_class: 'Chapter'} }
+
+    it { expect(response.status).to eq 200 }
   end
 end
