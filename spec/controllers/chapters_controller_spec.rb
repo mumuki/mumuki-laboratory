3a48d97e66192ef2fd6db98c1c1b874056c5e427
Franco Bulgarelli <franco@mumuki.org>
2018-07-20 18:46:24 -0300

    Removing unnecessary param

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ChaptersController, type: :controller, organization_workspace: :test do
   describe 'when chapter does not exist' do
-    before { get :show, params: {id: 0, debatable_class: 'Chapter'} }
+    before { get :show, params: {id: 0} }
 
     it { expect(response.status).to eq 404 }
   end
@@ -10,7 +10,7 @@ describe ChaptersController, type: :controller, organization_workspace: :test do
   describe 'when chapter exists' do
     let!(:chapter) { create(:chapter) }
 
-    before { get :show, params: {id: chapter.id, debatable_class: 'Chapter'} }
+    before { get :show, params: {id: chapter.id} }
 
     it { expect(response.status).to eq 200 }
   end
