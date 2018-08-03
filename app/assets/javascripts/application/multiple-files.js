44d19eabd3cb58b95f4dcf99bd9f7391a9a7e8c3
Rodrigo Alfonso <rodri042@gmail.com>
2018-08-03 17:42:16 -0300

    Refactor: Extracting a File class

--- app/assets/javascripts/application/multiple-files.js
+++ app/assets/javascripts/application/multiple-files.js
@@ -1,19 +1,95 @@
 mumuki.load(function () {
 
+  function File(tab, editor) {
+    this.tab = tab;
+    this.editor = editor;
+  }
+
+  File.FILE_NAME_CLASS = '.file-name';
+  File.DELETE_BUTTON_CLASS = '.delete-file-button';
+
+  File.prototype = {
+    initialize: function(name) {
+      this.setName(name);
+      this.unselect();
+
+      return this;
+    },
+
+    getName: function() {
+      return this.tab.find(File.FILE_NAME_CLASS).text();
+    },
+
+    setName: function(name) {
+      return this.tab.find(File.FILE_NAME_CLASS).text(name);
+    },
+
+    isSelected: function() {
+      return this.tab.hasClass("active");
+    },
+
+    setUpOnRemove: function(handler) {
+      this.tab.find(File.DELETE_BUTTON_CLASS).click(function() {
+        handler(this);
+      }.bind(this));
+    },
+
+    remove: function() {
+      const wasSelected = this.isSelected();
+
+      this.tab.remove();
+      this.editor.remove();
+
+      return wasSelected;
+    },
+
+    select: function() {
+      this._selectElement(this.tab);
+      this._selectElement(this.editor);
+    },
+
+    unselect: function() {
+      this._unselectElement(this.tab);
+      this._unselectElement(this.editor);
+    },
+
+    _selectElement: function(element) {
+      element.addClass('active');
+      element.addClass('in');
+    },
+
+    _unselectElement: function(element) {
+      element.removeClass('active');
+      element.removeClass('in');
+    }
+  };
+
   function FileControls(tabsContainer, editorsContainer) {
     this.tabsContainer = tabsContainer;
     this.editorsContainer = editorsContainer;
 
     this.MAX_TABS = 5;
-    this.DELETE_FILE = '.delete-file-button';
-    this.FILE_NAME = '.file-name';
 
     this._addFileButton = this.tabsContainer.siblings('.add-file-button');
+    this._updateButtonsVisibility();
   }
 
   FileControls.prototype = {
-    tabs: function() { return this.tabsContainer.children(); },
-    editors: function() { return this.editorsContainer.find('.file-editor'); },
+    files: function() {
+      const editors = this.editors();
+
+      return this.tabs().map(function(i, tab) {
+        return new File($(tab), $(editors[i]));
+      });
+    },
+
+    tabs: function() {
+      return this.tabsContainer.children();
+    },
+
+    editors: function() {
+      return this.editorsContainer.find('.file-editor')
+    },
 
     setUpAddFile: function() {
       this._addFileButton.click(function() {
@@ -22,15 +98,13 @@ mumuki.load(function () {
     },
 
     setUpDeleteFiles: function() {
-      this.tabs().each(function(i, element) {
-        this.setUpDeleteFile($(element));
+      this.files().each(function(i, file) {
+        this.setUpDeleteFile(file);
       }.bind(this));
     },
 
-    setUpDeleteFile: function(tab) {
-      tab.find(this.DELETE_FILE).click(function() {
-        this._deleteFile(this._getFileName(tab));
-      }.bind(this));
+    setUpDeleteFile: function(file) {
+      file.setUpOnRemove(this._deleteFile.bind(this));
     },
 
     _addFile: function() {
@@ -42,30 +116,22 @@ mumuki.load(function () {
       this.editorsContainer.append(this._createEditor(name, id));
       this.editors().last().remove();
 
+      const file = this.files().last().get(0).initialize(name);
+      this.setUpDeleteFile(file);
+
       this._updateButtonsVisibility();
     },
 
-    _deleteFile: function(name) {
-      var tab = this.tabs().find(this.FILE_NAME + ":contains('" + name + "')").parent();
-      if (!tab.length) return;
-
-      const editor = $(tab.attr('data-target'));
-      const wasSelected = this._isSelected(tab);
-
-      tab.remove();
-      editor.remove();
-
-      if (wasSelected) {
-        this._select(this.tabs().last());
-        this._select(this.editors().last());
-      }
+    _deleteFile: function(file) {
+      const wasSelected = file.remove();
+      if (wasSelected) this.files().last().get(0).select();
 
       this._updateButtonsVisibility();
     },
 
     _updateButtonsVisibility: function() {
       var filesCount = this._getFilesCount();
-      var deleteButtons = this.tabs().find(this.DELETE_FILE);
+      var deleteButtons = this.tabs().find(File.DELETE_BUTTON_CLASS);
 
       this._setVisibility(this._addFileButton, filesCount < this.MAX_TABS);
       this._setVisibility(deleteButtons, filesCount > 1);
@@ -73,10 +139,7 @@ mumuki.load(function () {
 
     _createTab: function(name, id) {
       var tab = this.tabs().last().clone();
-      this._setFileName(tab, name);
       tab.attr('data-target', '#' + id);
-      this._unselect(tab);
-      this.setUpDeleteFile(tab);
 
       return tab;
     },
@@ -85,7 +148,6 @@ mumuki.load(function () {
       var editor = this.editors().last().clone();
       editor.attr('id', id);
       editor.find('.CodeMirror').remove();
-      this._unselect(editor);
 
       var textarea = editor.children().first();
       textarea.attr('id', 'solution_content[' + name + ']');
@@ -95,52 +157,31 @@ mumuki.load(function () {
       // TODO: A veces no serializa el content nuevo
       // TODO: Detectar content type según extensión
 
-      new mumuki.editor.CodeMirrorBuilder(textarea[0])
+      new mumuki.editor.CodeMirrorBuilder(textarea.get(0))
         .setup(textarea.data('lines'));
 
       return editor;
     },
 
     _getFilesCount: function() {
-      return this.editors().length;
-    },
-
-    _setFileName: function(tab, name) {
-      return tab.find(this.FILE_NAME).text(name);
-    },
-
-    _getFileName: function(tab) {
-      return tab.find(this.FILE_NAME).text();
+      return this.files().length;
     },
 
     _setVisibility: function(element, isVisible) {
       if (isVisible) element.show(); else element.hide();
-    },
-
-    _isSelected: function(element) {
-      return element.hasClass("active");
-    },
-
-    _select: function(element) {
-      element.addClass('active');
-      element.addClass('in');
-    },
-
-    _unselect: function(element) {
-      element.removeClass('active');
-      element.removeClass('in');
     }
   };
 
-var setUpTabsBehavior = function(event) {
-  var tabsContainer = $('.nav-tabs');
-  if (!tabsContainer.length) return;
-  var editorsContainer = $('.tab-content');
+  var setUpTabsBehavior = function() {
+    var tabsContainer = $('.nav-tabs');
+    if (!tabsContainer.length) return;
+    var editorsContainer = $('.tab-content');
 
-  var controls = new FileControls(tabsContainer, editorsContainer);
+    var controls = new FileControls(tabsContainer, editorsContainer);
 
-  controls.setUpAddFile();
-  controls.setUpDeleteFiles();
-};
-$(document).ready(setUpTabsBehavior);
+    controls.setUpAddFile();
+    controls.setUpDeleteFiles();
+  };
+
+  $(document).ready(setUpTabsBehavior);
 });
