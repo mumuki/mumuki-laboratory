e98b1aa30b427ce39797ea04f7e9925634a903f4
luchotc <luchotc@users.noreply.github.com>
2018-08-10 17:59:50 -0300

    Ignoring guide private when importing from bibliotheca

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -58,7 +58,7 @@ class Guide < Content
   end
 
   def import_from_json!(json)
-    self.assign_attributes json.except('exercises', 'language', 'id_format', 'id', 'teacher_info', 'collaborators')
+    self.assign_attributes json.except('exercises', 'language', 'id_format', 'id', 'teacher_info', 'collaborators', 'private')
     self.language = Language.for_name(json['language'])
     self.save!
 
