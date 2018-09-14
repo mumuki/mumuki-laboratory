83e2f24ebbdc9d20df4133495d432a090a3c2388
Julián Berbel Alt <julianberbel@hotmail.com>
2018-09-14 18:26:43 -0300

    skip action only if in development

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,7 @@
 class LoginController < ApplicationController
   Mumukit::Login.configure_login_controller! self
 
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, if: lambda { Rails.env.development? }
 
   private
 
