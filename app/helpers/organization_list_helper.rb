c1c9664e70ebdd7329fdc252646aa5df8c6e2e27
Franco Bulgarelli <franco@mumuki.org>
2018-09-16 13:36:42 -0300

    Redirecting to root path on profile orgs switcher
    Fixes #906

--- app/helpers/organization_list_helper.rb
+++ app/helpers/organization_list_helper.rb
@@ -2,4 +2,8 @@ module OrganizationListHelper
   def organizations_for(user)
     (user.accessible_organizations + [Organization.central]).uniq.compact
   end
+
+  def organization_switch_url(organization)
+    organization.url_for(controller_name == 'users' ? root_path : request.path)
+  end
 end
