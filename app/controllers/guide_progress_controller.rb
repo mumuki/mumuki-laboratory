b5ee69cae9349d1727d9b5a0494c117adbca35fb
Julián Berbel Alt <julianberbel@hotmail.com>
2018-09-21 17:02:54 -0300

    add exam check to GuideProgressController

--- app/controllers/guide_progress_controller.rb
+++ app/controllers/guide_progress_controller.rb
@@ -1,7 +1,18 @@
 class GuideProgressController < ApplicationController
+  before_action :check_not_in_exam!
+
   def destroy
-    guide = Guide.find(params[:guide_id])
     guide.clear_progress! current_user
     redirect_back fallback_location: url_for(guide)
+  end
+
+  private
+
+  def check_not_in_exam!
+    raise Mumuki::Laboratory::ForbiddenError if guide.exam_in_organization?
+  end
+
+  def guide
+    @guide ||= Guide.find(params[:guide_id])
   end
 end
