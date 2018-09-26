a980e8749bfd7ab225aa40eba121dd0d2412cb8b
Franco Bulgarelli <franco@mumuki.org>
2018-09-25 22:14:31 -0300

    Fixing bug

--- lib/mumuki/laboratory/controllers/dynamic_errors.rb
+++ lib/mumuki/laboratory/controllers/dynamic_errors.rb
@@ -16,29 +16,37 @@ module Mumuki::Laboratory::Controllers::DynamicErrors
   end
 
   def not_found
-    render 'errors/not_found', status: 404, formats: [:html]
+    render_error 'not_found', 404, formats: [:html]
   end
 
   def internal_server_error(exception)
     Rails.logger.error "Internal server error: #{exception} \n#{exception.backtrace.join("\n")}"
-    render 'errors/internal_server_error', status: 500
+    render_error 'internal_server_error', 500
   end
 
   def unauthorized
-    render 'errors/unauthorized', status: 401
+    render_error 'unauthorized', 401
   end
 
   def forbidden
     Rails.logger.info "Access to organization #{Organization.current} was forbidden to user #{current_user} with permissions #{current_user.permissions}"
-    render 'errors/forbidden', status: 403, locals: { explanation: :forbidden_explanation }
+    render_error 'forbidden', 403, locals: { explanation: :forbidden_explanation }
   end
 
   def blocked_forum
-    render 'errors/forbidden', status: 403, locals: { explanation: :blocked_forum_explanation }
+    render_error 'forbidden', 403, locals: { explanation: :blocked_forum_explanation }
   end
 
   def gone
-    render 'errors/gone', status: 410
+    render_error 'gone', 410
+  end
+
+  def render_error(template, status, options={})
+    if request.path.start_with? '/api/'
+      render json: { message: template.gsub('_', ' ') }, status: status
+    else
+      render "errors/#{template}", options.merge(status: status)
+    end
   end
 
 end
