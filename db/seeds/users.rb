6a77c72e6b39ffa099fa311559380f7820c3e08f
Franco Bulgarelli <franco@mumuki.org>
2018-10-16 13:58:45 -0300

    Moving migrations

--- db/seeds/users.rb
+++
@@ -1,3 +0,0 @@
-User.find_or_create_by!(uid: 'dev.student@mumuki.org') { |org| org.permissions = {student: 'central/*'} }
-User.find_or_create_by!(uid: 'dev.teacher@mumuki.org') { |org| org.permissions = {teacher: 'private/*'} }
-User.find_or_create_by!(uid: 'dev.owner@mumuki.org') { |org| org.permissions = {owner: '*/*'} }
--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -17,3 +17,7 @@ Organization.find_or_create_by!(name: 'private') do |org|
 end
 
 Organization.find_or_create_by!(name: 'central')
+
+User.find_or_create_by!(uid: 'dev.student@mumuki.org') { |org| org.permissions = {student: 'central/*'} }
+User.find_or_create_by!(uid: 'dev.teacher@mumuki.org') { |org| org.permissions = {teacher: 'private/*'} }
+User.find_or_create_by!(uid: 'dev.owner@mumuki.org') { |org| org.permissions = {owner: '*/*'} }
