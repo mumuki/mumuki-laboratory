33d92fd5518432d22b68ffc81bcb9cbfdd18dac3
Julián Berbel Alt <julianberbel@hotmail.com>
2018-11-01 14:17:22 -0300

    hide button if exercise was already confirmed

--- app/helpers/concerns/with_student_path_navigation/finish_navigation.rb
+++ app/helpers/concerns/with_student_path_navigation/finish_navigation.rb
@@ -1,7 +1,7 @@
 module WithStudentPathNavigation
   class FinishNavigation < Navigation
     def button(navigable)
-      return unless navigable.is_a? Reading
+      return unless navigable.is_a?(Reading) && !navigable.status_for(current_user).like?(:passed)
       content_tag 'a', t(:finish), merge_confirmation_classes(navigable, class: clazz)
     end
 
