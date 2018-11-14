380283adcd69bd7791452080f180f8cbba6e5f58
Julián Berbel Alt <julianberbel@hotmail.com>
2018-11-14 18:15:04 -0300

    expose default_content in exercise and extra in both exercise and guide

--- domain/app/models/guide.rb
+++ domain/app/models/guide.rb
@@ -80,7 +80,7 @@ class Guide < Content
   end
 
   def to_resource_h
-    as_json(only: %i(beta type id_format private expectations corollary teacher_info authors collaborators))
+    as_json(only: %i(beta type id_format private expectations corollary teacher_info authors collaborators extra))
       .symbolize_keys
       .merge(super)
       .merge(exercises: exercises.map(&:to_resource_h))
