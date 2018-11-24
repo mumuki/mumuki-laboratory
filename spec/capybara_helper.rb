bded93faae8ed820136fb0ded16fa8f9b41dad3b
Franco Bulgarelli <franco@mumuki.org>
2018-11-24 19:30:42 -0300

    Using laboratory reference instead of application

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -2,11 +2,11 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 def set_subdomain_host!(subdomain)
-  @request.try { |it| it.host = Mumukit::Platform.application.organic_domain subdomain }
-  Capybara.app_host = Mumukit::Platform.application.organic_url subdomain
+  @request.try { |it| it.host = Mumukit::Platform.laboratory.organic_domain subdomain }
+  Capybara.app_host = Mumukit::Platform.laboratory.organic_url subdomain
 end
 
 def set_implicit_central!
-  @request.try { |it| it.host = Mumukit::Platform.application.domain }
-  Capybara.app_host = Mumukit::Platform.application.url
+  @request.try { |it| it.host = Mumukit::Platform.laboratory.domain }
+  Capybara.app_host = Mumukit::Platform.laboratory.url
 end
