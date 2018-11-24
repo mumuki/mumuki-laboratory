8ff278e23de78b6be8bf2ce4db279c76402a28cc
Franco Bulgarelli <franco@mumuki.org>
2018-11-24 18:35:35 -0300

    Welcome 6.1.4!

--- domain/lib/mumuki/domain/version.rb
+++ domain/lib/mumuki/domain/version.rb
@@ -1,5 +1,5 @@
 module Mumuki
   module Domain
-    VERSION = '6.1.3'
+    VERSION = '6.1.4'
   end
 end
--- lib/mumuki/laboratory/version.rb
+++ lib/mumuki/laboratory/version.rb
@@ -1,5 +1,5 @@
 module Mumuki
   module Laboratory
-    VERSION = '6.1.3'
+    VERSION = '6.1.4'
   end
 end
