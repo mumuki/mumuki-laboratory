3fd0a553b718cdd526902594f3967b4ca4a73019
Franco Bulgarelli <franco@mumuki.org>
2019-03-19 11:35:55 -0300

    Adding test for topics controller

---
+++ spec/features/topic_flow_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'Topic Flow', organization_workspace: :test do
+  let(:user) { create(:user) }
+  let(:problem) { build(:problem) }
+  let(:chapter) { create(:chapter, name: 'Functional Programming', lessons: [ create(:lesson, exercises: [ problem ]) ]) }
+
+  let!(:topic_in_path) { chapter.topic }
+  let(:topic_not_in_path) { create(:topic, name: 'Logic Programming') }
+
+  before { reindex_current_organization! }
+  before { set_current_user! user }
+
+  scenario 'visit topic in path transparently' do
+    visit "/topics/#{topic_in_path.transparent_id}"
+
+    expect(page).to have_text('Functional Programming')
+    expect(page).to have_text('Content')
+  end
+
+  scenario 'visit topic in path not transparently' do
+    visit "/topics/#{topic_not_in_path.transparent_id}"
+
+    expect(page).to_not have_text('Logic Programming')
+    expect(page).to_not have_text('Content Programming')
+  end
+end
