ac3d7b2fc212ab5b6c429d5446e2d9905b0a50b5
Rodrigo Alfonso <rodri042@gmail.com>
2019-04-22 17:30:57 -0300

    Adding invitations flow spec

---
+++ spec/features/invitations_flow_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+feature 'Invitations Flow', organization_workspace: :test do
+  let(:organization) { Organization.current }
+
+  let(:guide) { create(:guide) }
+  let(:chapter) {
+    create(:chapter, lessons: [
+      create(:lesson, guide: guide)
+    ]) }
+  let(:book) { organization.book }
+  before {
+    book.chapters = [chapter]
+    book.save!
+  }
+
+  let(:permissions) { { } }
+  let(:user) { create(:user, permissions: permissions) }
+  before { set_current_user!(user) }
+
+  let(:nodejs_course) { create(:course, slug: 'test/nodejs', name: 'Curso de NodeJS', organization: organization) }
+  let(:python_course) { create(:course, slug: 'test/python', name: 'Curso de Python', organization: organization) }
+  let!(:nodejs_invitation) { create(:invitation, code: 'invitacionAlNodejs', course: nodejs_course) }
+  let!(:python_invitation) { create(:invitation, code: 'invitacionAlPython', course: python_course) }
+
+  context 'with no memberships' do
+    scenario 'visit invitation' do
+      visit '/join/invitacionAlNodejs'
+      expect(page).to have_text('Join Curso de NodeJS')
+    end
+  end
+
+  context 'with existing memberships' do
+    let(:permissions) { { student: 'test/nodejs' } }
+
+    scenario 'visit invitation, already joined' do
+      visit '/join/invitacionAlNodejs'
+      expect(page).to have_text('Start Practicing!')
+    end
+
+    scenario 'visit invitation, not joined' do
+      visit '/join/invitacionAlPython'
+      expect(page).to have_text('Join Curso de Python')
+    end
+  end
+end
