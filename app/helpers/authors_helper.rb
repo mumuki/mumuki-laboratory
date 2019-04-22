540f77b6868059c93c02c1bf673d904763a095b5
Felipe Calvo <felipe@mumuki.org>
2019-04-22 16:28:36 -0300

    made authoring notes html safe

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -2,9 +2,9 @@ module AuthorsHelper
 
   def attribution_caption(guide)
     if guide.collaborators.present?
-      t(:authoring_note_with_collaborators, authors: guide.authors, collaborators: "https://raw.githubusercontent.com/#{guide.slug}/master/COLLABORATORS.txt")
+      t(:authoring_note_with_collaborators_html, authors: guide.authors, collaborators: "https://raw.githubusercontent.com/#{guide.slug}/master/COLLABORATORS.txt")
     else
-      t(:authoring_note, authors: guide.authors)
+      t(:authoring_note_html, authors: guide.authors)
     end
   end
 end
