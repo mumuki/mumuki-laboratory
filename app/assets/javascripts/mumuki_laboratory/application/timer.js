cf867730b233c71b135b0d16f748109b4e2da4dd
Gustavo Trucco <gustavo@mumuki.org>
2019-07-22 16:55:04 -0300

    two digit format for time

--- app/assets/javascripts/mumuki_laboratory/application/timer.js
+++ app/assets/javascripts/mumuki_laboratory/application/timer.js
@@ -8,10 +8,14 @@ var mumuki = mumuki || {};
     var duration = moment.duration(diffTime, 'milliseconds');
     var interval = 1000;
 
+    function formatNumber (number) {
+      return ("0" + number).slice(-2);
+    }
+
     setInterval(function () {
       duration = moment.duration(duration - interval, 'milliseconds');
       if(duration.milliseconds() <= 0) window.location.reload();
-      $('#timer').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
+      $('#timer').text(formatNumber(duration.hours()) + ":" + formatNumber(duration.minutes()) + ":" + formatNumber(duration.seconds()))
     }, interval);
   };
 })(mumuki);
