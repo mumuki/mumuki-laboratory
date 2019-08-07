ebc3e5d860ca8a8f6c275035e757dfb1bdd06ed1
Lucho Cannavo <luchotc@hotmail.com.ar>
2019-08-07 11:36:56 -0300

    using content_tag helper and renaming helper to li_tag

--- app/helpers/menu_bar_helper.rb
+++ app/helpers/menu_bar_helper.rb
@@ -10,15 +10,15 @@ module MenuBarHelper
   end
 
   def menu_bar_list_items
-    menu_bar_links.compact.map { |link| list_item(link) }.join.html_safe
+    menu_bar_links.compact.map { |link| li_tag(link) }.join.html_safe
   end
 
-  def list_item(link)
-    "<li>#{link}</li>"
+  def li_tag(link)
+    content_tag :li, link
   end
 
   def link_to_profile
-    list_item(menu_item 'user-o', :profile, user_path)
+    li_tag menu_item('user-o', :profile, user_path)
   end
 
   def link_to_classroom
