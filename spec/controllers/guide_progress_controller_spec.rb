b25bd9f7991ad5675fdf1cf0da8773a19fead993
Lucho Cannavo <luchotc@hotmail.com.ar>
2019-08-12 17:15:29 -0300

    Adding test for guide_progress reset

--- spec/controllers/guide_progress_controller_spec.rb
+++ spec/controllers/guide_progress_controller_spec.rb
@@ -5,17 +5,36 @@ describe GuideProgressController, organization_workspace: :test do
 
   let(:exam_problem) { build(:problem) }
   let(:test_organization) { Organization.find_by_name('test') }
-  let(:exam) { create(:exam, organization: test_organization, guide: guide) }
-  let(:guide) { create(:guide, exercises: [exam_problem])}
-
-  before { exam.index_usage! test_organization }
-
-  before { reindex_current_organization! }
+  let(:exam) { create(:exam, organization: test_organization, guide: exam_guide) }
+  let(:lesson) { create(:lesson, guide: lesson_guide) }
 
   describe 'delete' do
     before { set_current_user! user }
-    before { delete :destroy, params: { guide_id: guide.id } }
 
-    it { expect(response.status).to eq 403 }
+    describe 'an exam guide progress' do
+      let(:exam_guide) { create(:guide, exercises: [exam_problem])}
+      before { exam.index_usage! test_organization }
+      before { reindex_current_organization! }
+      before { delete :destroy, params: { guide_id: exam_guide.id } }
+
+      it { expect(response.status).to eq 403 }
+    end
+
+    describe 'a lesson guide progress' do
+      let(:book) { create(:book,
+                          slug: 'mumuki/book',
+                          chapters: [
+                            create(:chapter,
+                                   slug: 'mumuki/topic1',
+                                   lessons: [lesson])]) }
+
+      let(:lesson_guide) { create(:guide, exercises: [exam_problem])}
+      before { test_organization.update! book: book }
+      before { lesson.index_usage! test_organization }
+      before { reindex_current_organization! }
+      before { delete :destroy, params: { guide_id: lesson_guide.id } }
+
+      it { expect(response.status).to eq 302 }
+    end
   end
 end
