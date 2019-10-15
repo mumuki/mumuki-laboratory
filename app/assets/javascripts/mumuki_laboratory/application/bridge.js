fa85250f03a9cc3e9b75a1585bc02ac047921b73
Franco Bulgarelli <franco@mumuki.org>
2019-10-15 18:14:15 -0300

    Refactor: introducing SubmissionsStore

--- app/assets/javascripts/mumuki_laboratory/application/bridge.js
+++ app/assets/javascripts/mumuki_laboratory/application/bridge.js
@@ -1,38 +1,5 @@
-var mumuki = mumuki || {};
-
-(function (mumuki) {
-  var lastSubmission = {};
-
-  function Laboratory(){
-    this.exerciseId = $('#mu-exercise-id')[0].value;
-  }
-
-  function asString(json){
-    return JSON.stringify(json);
-  }
-
-  function sameAsLastSolution(newSolution){
-    return asString(lastSubmission.content) === asString(newSolution);
-  }
-
-  function lastSubmissionFinishedSuccessfully(){
-    return lastSubmission.result && lastSubmission.result.status !== 'aborted';
-  }
-
-  function progressListItemClassForStatus(status) {
-    return `progress-list-item text-center ${classForStatus(status)}`;
-  }
-
-  function classForStatus(status) {
-    switch (status) {
-      case "passed": return "success";
-      case "failed": return "danger";
-      case "passed_with_warnings": return "warning";
-      case "errored": return "broken";
-    }
-  }
-
-  function messageForStatus(status) {
+(() => {
+  function _messageForStatus(status) {
     switch (status) {
       case "passed": return "¡Muy bien! Tu solución pasó todas las pruebas";
       case "failed": return "Tu solución no pasó las pruebas";
@@ -41,84 +8,53 @@ var mumuki = mumuki || {};
     }
   }
 
-  function titleHtmlForStatus(status) {
-    return `<h4 class=\"text-${classForStatus(status)}\">\n  <strong><i class=\"fa fa-times-circle\"></i>${messageForStatus(status)}</strong>\n</h4>\n`;
+  function _titleHtmlForStatus(status) {
+    return `<h4 class=\"text-${mumuki.classForStatus(status)}\">\n  <strong><i class=\"fa fa-times-circle\"></i>${_messageForStatus(status)}</strong>\n</h4>\n`;
   }
 
-  function sendNewSolution(exerciseId, solution) {
-    const status = "failed"; // FIXME hardcoded
+  function _sendNewSolution(exerciseId, solution) {
+    const status = "passed"; // FIXME hardcoded
     const result = {
-      "status":status,
+      "status": status,
       "guide_finished_by_solution": false,
-      "class_for_progress_list_item": progressListItemClassForStatus(status),
       "html":"\n<div class=\"mu-kids-callout-danger\">\n  \n</div>\n\n  <img class=\"capital-animation mu-kids-corollary-animation\"/>\n  \n        <div class=\"mu-last-box\">\n          <p><p>Como ves, para que el tractor siembre lechuga, tenemos que decirle a Mukinita que ponga 2 bolitas verdes.  Y en este ejemplo acabamos de sembrar una hilera de 4 lechugas. </p>\n<p>Pero fue un poco difícil entenderlo, ¿no? </p>\n</p>\n        </div>\n      \n",
-      "title_html": titleHtmlForStatus(status),
+      "title_html": _titleHtmlForStatus(status),
       "button_html":"<div class=\"row\">\n  <div class=\"col-md-12\">\n    <div class=\"actions\">\n        <button class=\"btn btn-success btn-block submission-control\" id=\"kids-btn-retry\" data-dismiss=\"modal\" aria-label=\"Reintentar\"> Reintentar </button>\n    </div>\n  </div>\n</div>\n",
       "expectations_html":"",
       "remaining_attempts_html":null,
-      "test_results":[
-         {
-            "title":null,
-            "status":"failed",
-            "result":"<style>\n\n  .boards-container {\n    display: inline-flex;\n  }\n\n  .error-text {\n    color: #d9534f;\n  }\n\n  .boom-text {\n    margin-top: 20px;\n  }\n\n  .title {\n    color: #b4bcc2;\n    text-align: left;\n    font-family: Arial, Helvetica, sans-serif;\n    font-size: 9pt;\n  }\n\n  .board {\n    margin: 0 15px;\n  }\n\n</style>\n\n\n  <p class=\"error-text\">\n    Se obtuvo un tablero distinto al esperado.\n  </p>\n\n\n<div class=\"boards-container\">\n\n  \n    <div class=\"board\">\n      <p class=\"title\">Tablero inicial</p>\n      <p class=\"initial_board\">\n        \n          <gs-board >\n            GBB/1.0\nsize 4 2\nhead 0 0\n\n          </gs-board>\n        \n      </p>\n    </div>\n  \n    <div class=\"board\">\n      <p class=\"title\">Tablero final esperado</p>\n      <p class=\"expected_board\">\n        \n          <gs-board without-header>\n            GBB/1.0\nsize 4 2\ncell 0 0 Verde 2\ncell 1 0 Verde 2\ncell 2 0 Verde 2\ncell 3 0 Verde 2\nhead 3 0\n\n          </gs-board>\n        \n      </p>\n    </div>\n  \n    <div class=\"board\">\n      <p class=\"title\">Tablero final obtenido</p>\n      <p class=\"actual_board\">\n        \n          <gs-board >\n            GBB/1.0\nsize 4 2\ncell 0 0 Verde 1\nhead 0 0\n\n          </gs-board>\n        \n      </p>\n    </div>\n  \n\n</div>\n\n\n"
-         }
-      ]
+      "test_results":[]
     };
     lastSubmission = { content: solution, result: result };
-    window.localStorage.setItem(localStorageExerciseKey(exerciseId), JSON.stringify(lastSubmission));
-    result.class_for_progress_list_item += ' active';
+    mumuki.SubmissionsStore.setLastSubmission(exerciseId, lastSubmission);
     return Promise.resolve(result);
   }
 
-  function localStorageExerciseKey(exerciseId) {
-    return `/exercise/${exerciseId}/submission`;
-  }
-
-  function lastSubmissionForExercise(exerciseId) {  
-    return window.localStorage.getItem(localStorageExerciseKey(exerciseId));
-
-  }
-
-  function updateProgressListItemClass(a) {
-    const submission = lastSubmissionForExercise(a.data('mu-exercise-id'));
-
-    if (submission) {
-      a.attr('class', JSON.parse(submission).result.class_for_progress_list_item);
-    }
-  }
-
-  mumuki.load(function () {
-    lastSubmission = {};
-    $('.progress-list-item').each((_, it) => updateProgressListItemClass($(it)));
-  });
-
-  Laboratory.prototype = {
-
+  class Laboratory {
     // ==========
     // Public API
     // ==========
 
     // Runs tests for the current exercise using the given submission
     // content.
-    runTests: function(content) {
+    runTests(content) {
       return this._submitSolution({ solution: content });
-    },
+    }
 
     // ===========
     // Private API
     // ===========
 
-    _submitSolution: function (solution) {
-      if(lastSubmissionFinishedSuccessfully() && sameAsLastSolution(solution)){
-        return $.Deferred().resolve(lastSubmission.result);
-      } else {
-        return sendNewSolution(this.exerciseId, solution);
+    _submitSolution(solution) {
+      const cachedSolution = mumuki.SubmissionsStore.getCachedResultFor(mumuki.currentExerciseId, solution);
+      if(cachedSolution){
+        return $.Deferred().resolve(cachedSolution);
       }
-    },
-  };
+
+      return _sendNewSolution(mumuki.currentExerciseId, solution);
+    }
+  }
 
   mumuki.bridge = {
     Laboratory: Laboratory
   };
-
-}(mumuki));
+})();
--- app/assets/javascripts/mumuki_laboratory/application/progress.js
+++ app/assets/javascripts/mumuki_laboratory/application/progress.js
@@ -1,10 +1,41 @@
-var mumuki = mumuki || {};
-
-(function (mumuki) {
-
-  mumuki.updateProgressBarAndShowModal = function (data) {
-    $('.progress-list-item.active').attr('class', data.class_for_progress_list_item);
-    if(data.guide_finished_by_solution) $('#guide-done').modal();
+(() => {
+  // Updates the current exercise progress indicator
+  mumuki.updateProgressBarAndShowModal = function (submission) {
+    $('.progress-list-item.active').attr('class', mumuki.progressListItemClassForStatus(submission.status, true));
+    if(submission.guide_finished_by_solution) $('#guide-done').modal();
   };
 
-})(mumuki);
+  mumuki.progressListItemClassForStatus = function (status, active = false) {
+    return `progress-list-item text-center ${mumuki.classForStatus(status)} ${active ? 'active' : ''}`;
+  };
+
+  mumuki.classForStatus = function (status) {
+    switch (status) {
+      case "passed": return "success";
+      case "failed": return "danger";
+      case "passed_with_warnings": return "warning";
+      case "errored": return "broken";
+      case "pending": return "muted";
+    }
+  };
+
+  function _updateProgressListItemClass(a) {
+    const exerciseId = a.data('mu-exercise-id');
+    const status = mumuki.SubmissionsStore.getLastSubmissionStatus(exerciseId);
+
+    a.attr('class', mumuki.progressListItemClassForStatus(status, exerciseId == mumuki.currentExerciseId));
+  }
+
+  mumuki.load(() => {
+    // Set global currentExerciseId
+    const $muExerciseId = $('#mu-exercise-id')[0];
+    if ($muExerciseId) {
+      mumuki.currentExerciseId = $muExerciseId.value;
+    } else {
+      mumuki.currentExerciseId = null;
+    }
+
+    // Update all exercises progress indicators
+    $('.progress-list-item').each((_, it) => _updateProgressListItemClass($(it)));
+  })
+})();
