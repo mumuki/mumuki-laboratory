a18ba122f03ea2c175e77c3378b2138ca98e4fc0
Julián Berbel Alt <julian@mumuki.org>
2020-03-19 13:38:51 -0300

    Verify user name on create and update

--- app/controllers/concerns/users_controller_template.rb
+++ app/controllers/concerns/users_controller_template.rb
@@ -6,16 +6,21 @@ module UsersControllerTemplate
     before_action :set_user!, only: [:show, :update]
     before_action :set_new_user!, only: :create
     before_action :protect_permissions_assignment!, only: [:create, :update]
+    after_action :verify_user_name!, only: [:create, :update]
   end
 
   private
+
+  def verify_user_name!
+    @user.verify_name!
+  end
 
   def protect_permissions_assignment!
     current_user.protect_permissions_assignment! user_params[:permissions], @user.permissions_was
   end
 
   def permissible_params
-    super + [:email, :image_url, permissions: Mumukit::Auth::Roles::ROLES]
+    super + [:email, :image_url, :verified_first_name, :verified_last_name, permissions: Mumukit::Auth::Roles::ROLES]
   end
 
   def set_user!
