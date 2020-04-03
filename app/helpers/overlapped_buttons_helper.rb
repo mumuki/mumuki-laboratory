3aa4e02f98925c33b79510214bb6bdf34efcfa3d
Franco Bulgarelli <franco@mumuki.org>
2020-04-03 15:32:53 -0300

    Extracting duplications

--- app/helpers/overlapped_buttons_helper.rb
+++ app/helpers/overlapped_buttons_helper.rb
@@ -1,13 +1,17 @@
 module OverlappedButtonsHelper
   def restart_icon
-    fa_icon('undo', title: t(:restart), class: 'fa-fw', role: 'button', tabindex: '0', 'aria-label': t(:restart))
+    overlapped_button_icon :restart, :undo
   end
 
   def format_icon
-    fa_icon('indent', title: t(:format), class: 'fa-fw', role: 'button', tabindex: '0', 'aria-label': t(:indent))
+    overlapped_button_icon :format, :indent
   end
 
   def restart_guide_link(guide)
     link_to restart_icon, guide_progress_path(guide), class: 'mu-content-toolbar-item', data: {confirm: t(:confirm_restart)}, method: :delete
+  end
+
+  def overlapped_button_icon(key, icon)
+    fa_icon(icon, title: t(key), class: 'fa-fw', role: 'button', 'aria-label': t(key))
   end
 end
