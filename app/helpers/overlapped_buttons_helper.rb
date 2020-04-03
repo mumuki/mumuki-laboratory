0a414b5bdde2a0f0765deb53fd2bc18eb5fa7fe6
Franco Bulgarelli <franco@mumuki.org>
2020-04-03 15:32:31 -0300

    Introducing format button

--- app/helpers/overlapped_buttons_helper.rb
+++ app/helpers/overlapped_buttons_helper.rb
@@ -1,10 +1,13 @@
-module ResetButtonHelper
+module OverlappedButtonsHelper
   def restart_icon
     fa_icon('undo', title: t(:restart), class: 'fa-fw', role: 'button', tabindex: '0', 'aria-label': t(:restart))
+  end
+
+  def format_icon
+    fa_icon('indent', title: t(:format), class: 'fa-fw', role: 'button', tabindex: '0', 'aria-label': t(:indent))
   end
 
   def restart_guide_link(guide)
     link_to restart_icon, guide_progress_path(guide), class: 'mu-content-toolbar-item', data: {confirm: t(:confirm_restart)}, method: :delete
   end
-
 end
