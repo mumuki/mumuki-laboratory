4e23cc4221b632c76305cb7ff560e5bed4ecd130
Franco Bulgarelli <franco@mumuki.org>
2020-05-06 17:26:01 -0300

    Appending first summary in kids layout

--- app/assets/javascripts/mumuki_laboratory/application/kids.js
+++ app/assets/javascripts/mumuki_laboratory/application/kids.js
@@ -173,13 +173,17 @@ mumuki.load(function () {
     },
 
     _showMessageOnCharacterBubble: function (data) {
-      var $bubble = mumuki.kids._getCharacterBubble();
+      let $bubble = mumuki.kids._getCharacterBubble();
+      let $failedSppechBubble = $bubble.find('.mu-kids-character-speech-bubble-failed');
+
       $bubble.find('.mu-kids-character-speech-bubble-tabs').hide();
       $bubble.find('.mu-kids-character-speech-bubble-normal').hide();
-      $bubble.find('.mu-kids-character-speech-bubble-failed').show().html(data.title_html);
+      $failedSppechBubble.show().html(data.title_html);
       $bubble.addClass(data.status);
       if (data.status === 'passed_with_warnings') {
-        $bubble.find('.mu-kids-character-speech-bubble-failed').append(data.expectations_html);
+        this._appendFirstExpectationHtml($failedSppechBubble, data);
+      } else if (data.status === 'failed') {
+        this._appendFirstSummaryMessage($failedSppechBubble, data);
       }
       if (this._shouldDisplayDiscussionsLink(data.status)) {
         $bubble.append(discussionsLinkHtml);
@@ -189,6 +193,17 @@ mumuki.load(function () {
 
     _shouldDisplayDiscussionsLink: function (status) {
       return ['failed', 'passed_with_warnings'].some(it => it === status);
+    },
+
+    _appendFirstExpectationHtml($failedSppechBubble, data) {
+      $failedSppechBubble.append(data.expectations_html);
+    },
+
+    _appendFirstSummaryMessage($failedSppechBubble, data) {
+      let summary = data.test_results[0].summary;
+      if (summary) {
+        $failedSppechBubble.append(summary.message);
+      }
     },
 
     _showOnSuccessPopup: function (data) {
