2ebc9db7b78e22596c9e1adaf1243b32a8b017e1
Felipe Calvo <felipe@mumuki.org>
2020-06-08 17:25:00 -0300

    Append avatar selection on user form at submission
    Previously appended the hidden field as many times as avatars were selected;
    also, the ID could be manually edited to an invalid value before submission

--- app/assets/javascripts/mumuki_laboratory/application/avatar.js
+++ app/assets/javascripts/mumuki_laboratory/application/avatar.js
@@ -1,13 +1,19 @@
 mumuki.load(function() {
+
+  var avatar_id = $(this).attr('avatar_id');
+
   $("#edit-avatar-icon").on('click', function(){
     $('#avatar-picker').modal();
+  });
+
+  $("#user-form").on('submit', function(){
+    $(this).append('<input type="hidden" name="user[avatar_id]" value="'+ avatar_id + '"/>');
   });
 
   $('.avatar-item').on('click', function(){
     $('#user-avatar').attr('src', $(this).attr('src'));
     $('#avatar-picker').modal('hide');
 
-    const avatar_id = $(this).attr('avatar_id');
-    $('#user-form').append('<input type="hidden" name="user[avatar_id]" value="'+ avatar_id + '"/>');
+    avatar_id = $(this).attr('avatar_id');
   });
 });
