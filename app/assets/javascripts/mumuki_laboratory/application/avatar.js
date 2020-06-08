ddfb5c1d9b1a923aa0fc03e4664d51630b6c0b5c
Felipe Calvo <felipe@mumuki.org>
2020-06-08 11:44:28 -0300

    Include chosen avatar on user profile form

--- app/assets/javascripts/mumuki_laboratory/application/avatar.js
+++ app/assets/javascripts/mumuki_laboratory/application/avatar.js
@@ -6,5 +6,8 @@ mumuki.load(function() {
   $('.avatar-item').on('click', function(){
     $('#user-avatar').attr('src', $(this).attr('src'));
     $('#avatar-picker').modal('hide');
+
+    const avatar_id = $(this).attr('avatar_id');
+    $('#user-form').append('<input type="hidden" name="user[avatar_id]" value="'+ avatar_id + '"/>');
   });
 });
