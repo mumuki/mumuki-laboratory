e110b0239058158ae4c43ab215632c88bb12d0c6
Felipe Calvo <felipe@mumuki.org>
2020-06-08 11:43:14 -0300

    Show new avatar on profile once selected

--- app/assets/javascripts/mumuki_laboratory/application/avatar.js
+++ app/assets/javascripts/mumuki_laboratory/application/avatar.js
@@ -2,4 +2,9 @@ mumuki.load(function() {
   $("#edit-avatar-icon").on('click', function(){
     $('#avatar-picker').modal();
   });
+
+  $('.avatar-item').on('click', function(){
+    $('#user-avatar').attr('src', $(this).attr('src'));
+    $('#avatar-picker').modal('hide');
+  });
 });
