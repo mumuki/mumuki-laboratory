fcdb55c6d403d5a2c2187e4ed63a4c2f6a3cdf5d
Felipe Calvo <felipe@mumuki.org>
2020-06-08 17:45:22 -0300

    Allow an user's default image to be selected as an avatar

--- app/assets/javascripts/mumuki_laboratory/application/avatar.js
+++ app/assets/javascripts/mumuki_laboratory/application/avatar.js
@@ -1,19 +1,28 @@
 mumuki.load(function() {
-
-  var avatar_id = $(this).attr('avatar_id');
+  let avatar_id = "";
 
   $("#edit-avatar-icon").on('click', function(){
     $('#avatar-picker').modal();
   });
 
   $("#user-form").on('submit', function(){
-    $(this).append('<input type="hidden" name="user[avatar_id]" value="'+ avatar_id + '"/>');
+    if (!avatar_id) {
+      const image_url = $('#user-avatar').attr('src');
+      $(this).append('<input type="hidden" name="user[image_url]" value="' + image_url + '"/>');
+    }
+
+    $(this).append('<input type="hidden" name="user[avatar_id]" value="' + avatar_id + '"/>');
   });
 
   $('.avatar-item').on('click', function(){
     $('#user-avatar').attr('src', $(this).attr('src'));
     $('#avatar-picker').modal('hide');
 
-    avatar_id = $(this).attr('avatar_id');
+    const item_avatar = $(this).attr('avatar_id');
+    if (item_avatar) {
+      avatar_id = item_avatar;
+    } else {
+      avatar_id = "";
+    }
   });
 });
