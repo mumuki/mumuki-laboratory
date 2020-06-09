f29dea5b0ee599e013c26b83aff001f135a98c6c
Franco Bulgarelli <franco@mumuki.org>
2020-06-09 14:59:38 -0300

    Adding test

--- spec/features/home_public_flow_spec.rb
+++ spec/features/home_public_flow_spec.rb
@@ -4,9 +4,12 @@ feature 'public org', organization_workspace: :test do
   let!(:exercise) { build(:exercise) }
   let(:guide) { create(:guide) }
   let(:exam) { create(:exam) }
-  let!(:chapter) {
-    create(:chapter, lessons: [
+  let!(:chapter_1) {
+    create(:chapter, name: 'First Steps', lessons: [
       create(:lesson, guide: guide)]) }
+  let!(:chapter_2) {
+      create(:chapter, name: 'Functional Programming', lessons: [
+        create(:lesson, guide: guide)]) }
   let!(:current_organization) { Organization.current }
   let(:book) { current_organization.book }
 
@@ -19,6 +22,8 @@ feature 'public org', organization_workspace: :test do
       visit '/'
 
       expect(page).to have_text('ム mumuki')
+      expect(page).to have_text('First Steps')
+      expect(page).to have_text('Functional Programming')
       expect(page).to have_text(current_organization.book.name)
       expect(page).not_to have_text('Exams')
     end
@@ -29,6 +34,8 @@ feature 'public org', organization_workspace: :test do
       visit '/'
 
       expect(page).to have_text('ム mumuki')
+      expect(page).to have_text('First Steps')
+      expect(page).to have_text('Functional Programming')
       expect(page).to have_text(current_organization.book.name)
     end
   end
@@ -44,6 +51,8 @@ feature 'public org', organization_workspace: :test do
       visit '/'
 
       expect(page).to have_text('ム mumuki')
+      expect(page).to have_text('First Steps')
+      expect(page).to have_text('Functional Programming')
       expect(page).to have_text(current_organization.book.name)
       expect(user.reload.last_organization).to eq current_organization
       expect(page).to have_text('Exams')
@@ -55,6 +64,27 @@ feature 'public org', organization_workspace: :test do
       visit '/'
 
       expect(page).to have_text('ム mumuki')
+      expect(page).to have_text('First Steps')
+      expect(page).to have_text('Functional Programming')
+      expect(page).to have_text(current_organization.book.name)
+      expect(user.reload.last_organization).to eq current_organization
+    end
+  end
+
+  context 'logged visitor in progressive display organization' do
+    let(:user) { create(:user) }
+    before { current_organization.enable_progressive_display! }
+    before { set_current_user! user }
+    before { exam.authorize! user }
+
+    scenario 'from inside' do
+      Capybara.current_session.driver.header 'Referer', 'http://google.com'
+
+      visit '/'
+
+      expect(page).to have_text('ム mumuki')
+      expect(page).to have_text('First Steps')
+      expect(page).to_not have_text('Functional Programming')
       expect(page).to have_text(current_organization.book.name)
       expect(user.reload.last_organization).to eq current_organization
     end
