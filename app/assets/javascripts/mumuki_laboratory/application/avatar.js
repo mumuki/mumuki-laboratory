a35a59a7e09d0ded2f4bcd8b8c7bd8def837ccb4
Felipe Calvo <felipe@mumuki.org>
2020-06-09 17:12:16 -0300

    Prefix all avatar-related classes and ids with mu-

--- app/assets/javascripts/mumuki_laboratory/application/avatar.js
+++ app/assets/javascripts/mumuki_laboratory/application/avatar.js
@@ -1,13 +1,13 @@
 mumuki.load(function() {
   let avatarId = "";
-  let $avatarPicker = $('#avatar-picker');
-  let $userAvatar = $('#user-avatar');
+  let $avatarPicker = $('#mu-avatar-picker');
+  let $userAvatar = $('#mu-user-avatar');
 
-  $("#edit-avatar-icon").on('click', function(){
+  $("#mu-edit-avatar-icon").on('click', function(){
     $avatarPicker.modal();
   });
 
-  $("#user-form").on('submit', function(){
+  $("#mu-user-form").on('submit', function(){
     if (!avatarId) {
       const imageUrl = $userAvatar.attr('src');
       $(this).append(`<input type="hidden" name="user[image_url]" value="${imageUrl}"/>`);
@@ -16,11 +16,11 @@ mumuki.load(function() {
     $(this).append(`<input type="hidden" name="user[avatar_id]" value="${avatarId}"/>`);
   });
 
-  $('.avatar-item').on('click', function(){
+  $('.mu-avatar-item').on('click', function(){
     $userAvatar.attr('src', $(this).attr('src'));
     $avatarPicker.modal('hide');
 
-    const clickedAvatarId = $(this).attr('avatar_id');
+    const clickedAvatarId = $(this).attr('mu-avatar-id');
     avatarId = clickedAvatarId || "";
   });
 });
