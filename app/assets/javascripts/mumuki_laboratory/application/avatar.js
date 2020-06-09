f4a9ff6b81ecfc226d96e767fbd9dd775f05f8ae
Felipe Calvo <felipe@mumuki.org>
2020-06-09 16:35:05 -0300

    Use proper JS case

--- app/assets/javascripts/mumuki_laboratory/application/avatar.js
+++ app/assets/javascripts/mumuki_laboratory/application/avatar.js
@@ -1,28 +1,28 @@
 mumuki.load(function() {
-  let avatar_id = "";
+  let avatarId = "";
 
   $("#edit-avatar-icon").on('click', function(){
     $('#avatar-picker').modal();
   });
 
   $("#user-form").on('submit', function(){
-    if (!avatar_id) {
-      const image_url = $('#user-avatar').attr('src');
-      $(this).append(`<input type="hidden" name="user[image_url]" value="${image_url}"/>`);
+    if (!avatarId) {
+      const imageUrl = $('#user-avatar').attr('src');
+      $(this).append(`<input type="hidden" name="user[image_url]" value="${imageUrl}"/>`);
     }
 
-    $(this).append(`<input type="hidden" name="user[avatar_id]" value="${avatar_id}"/>`);
+    $(this).append(`<input type="hidden" name="user[avatar_id]" value="${avatarId}"/>`);
   });
 
   $('.avatar-item').on('click', function(){
     $('#user-avatar').attr('src', $(this).attr('src'));
     $('#avatar-picker').modal('hide');
 
-    const item_avatar = $(this).attr('avatar_id');
-    if (item_avatar) {
-      avatar_id = item_avatar;
+    const clickedAvatarId = $(this).attr('avatar_id');
+    if (clickedAvatarId) {
+      avatarId = clickedAvatarId;
     } else {
-      avatar_id = "";
+      avatarId = "";
     }
   });
 });
