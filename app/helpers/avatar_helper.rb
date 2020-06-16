82e06c895842ed988ed7f3798bb80be3fa090e13
Felipe Calvo <felipe@mumuki.org>
2020-06-16 14:35:57 -0300

    Extract showing avatar to helper

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -2,4 +2,8 @@ module AvatarHelper
   def avatars_for(user)
     Avatar.all
   end
+
+  def show_avatar_item(item)
+    "<img src='#{item.image_url}' alt='#{item.description}' mu-avatar-id='#{item.id}' class='mu-avatar-item'>".html_safe
+  end
 end
