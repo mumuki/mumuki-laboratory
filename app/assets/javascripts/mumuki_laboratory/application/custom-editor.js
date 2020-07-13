e23e10e89f05c58c6d086019e9bcc348271cd5ec
Franco Bulgarelli <franco@mumuki.org>
2020-07-13 19:43:41 -0300

    Ensuring clean CustomEditor sources on init

--- app/assets/javascripts/mumuki_laboratory/application/custom-editor.js
+++ app/assets/javascripts/mumuki_laboratory/application/custom-editor.js
@@ -3,19 +3,57 @@ var mumuki = mumuki || {};
 (function (mumuki) {
 
   var CustomEditor = {
+    /**
+     * @typedef {{getContent: () => {name: string, value: string}}} CustomEditorSource
+     */
+
+    /**
+     * @type {CustomEditorSource[]}
+     */
     sources: [],
 
-    addSource: function (source) {
+    /**
+     * @param {CustomEditorSource} source
+     */
+    addSource(source) {
       CustomEditor.sources.push(source);
     },
 
-    // Each external source must implement getContent method
+    /**
+     * @deprecated use getContents instead
+     */
+    getContent() {
+      return this.getContents();
+    },
 
-    getContent: function () {
+    /**
+     * @returns {{name: string, value: string}[]}
+     */
+    getContents() {
       return CustomEditor.sources.map( e => e.getContent() );
+    },
+
+    clearSources() {
+      this.sources = [];
+    },
+
+    get hasSources() {
+      return this.sources.length > 0;
     }
   };
 
-  mumuki.CustomEditor = CustomEditor;
+  mumuki.load(() => {
+    mumuki.CustomEditor.clearSources();
+  });
 
+  /**
+   * This module allows custom editors to register
+   * content sources that can not me mapped to standard selectors {@code mu-custom-editor-value},
+   * {@code mu-custom-editor-extra} and {@code mu-custom-editor-test}
+   *
+   * CustomEditor sources are cleared after page reload even when using turbolinks
+   *
+   * @module mumuki.CustomEditor
+   */
+  mumuki.CustomEditor = CustomEditor;
 }(mumuki));
