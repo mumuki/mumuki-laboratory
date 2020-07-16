1ad11555c164372027ffb9fc8308813440d80b99
Julián Berbel Alt <julian@mumuki.org>
2020-07-16 15:52:54 -0300

    Refactor user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,19 @@
 class UserMailer < ApplicationMailer
 
   def we_miss_you_reminder(user, cycles)
-    send_reminder! user, :we_miss_you, "#{cycles.ordinalize}_reminder"
+    build_email user, :we_miss_you, "#{cycles.ordinalize}_reminder"
   end
 
   def no_submissions_reminder(user)
-    send_reminder! user, :start_using_mumuki, "no_submissions_reminder"
+    build_email user, :start_using_mumuki, 'no_submissions_reminder'
   end
 
   private
 
-  def send_reminder!(user, subject, template_name)
+  def build_email(user, subject, template_name, organization = nil)
     @user = user
     @unsubscribe_code = User.unsubscription_verifier.generate(user.id)
-    @organization = user.last_organization
+    @organization = organization || user.last_organization
 
     I18n.with_locale(@organization.locale) do
       mail to: user.email,
