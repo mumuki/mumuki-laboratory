4cbc434cd7c9b014c0fa253916404f5ceff0c942
Felipe Calvo <felipe@mumuki.org>
2020-07-16 10:23:18 -0300

    Toggle save profile button activation when avatar is changed

--- app/assets/javascripts/mumuki_laboratory/application/profile.js
+++ app/assets/javascripts/mumuki_laboratory/application/profile.js
@@ -1,7 +1,19 @@
 mumuki.load(function() {
   let $userForm = $("#mu-user-form");
   let $editButton = $('#mu-edit-profile-btn');
+  let $userAvatar = $('#mu-user-avatar');
+  let $avatarItem = $('.mu-avatar-item');
+
   let originalData = $userForm.serialize();
+  let originalProfilePicture = $userAvatar.attr('src');
+
+  $avatarItem.on('click', function() {
+    if (avatarChanged()) {
+      $editButton.prop('disabled', false);
+    } else {
+      $editButton.prop('disabled', true);
+    }
+  });
 
   $userForm.on('change keyup', function() {
     if ($userForm.serialize() != originalData) {
@@ -10,4 +22,8 @@ mumuki.load(function() {
       $editButton.prop('disabled', true);
     }
   });
+
+  function avatarChanged() {
+    return ($userAvatar.attr('src') != originalProfilePicture);
+  }
 });
