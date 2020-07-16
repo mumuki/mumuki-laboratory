c8bb0447bee157520689bf75196ac1d15def33a3
Felipe Calvo <felipe@mumuki.org>
2020-07-16 15:47:47 -0300

    Merge profile.js and avatar.js
    They were reusing a few selectors and even acting on the same events.

--- app/assets/javascripts/mumuki_laboratory/application/profile.js
+++ app/assets/javascripts/mumuki_laboratory/application/profile.js
@@ -1,18 +1,28 @@
 mumuki.load(function() {
   let $userForm = $("#mu-user-form");
-  let $editButton = $('#mu-edit-profile-btn');
   let $userAvatar = $('#mu-user-avatar');
+  let $editButton = $('#mu-edit-profile-btn');
+  let $avatarPicker = $('#mu-avatar-picker');
   let $avatarItem = $('.mu-avatar-item');
+
+  let userImage = "";
+  let avatarId = "";
 
   let originalData = $userForm.serialize();
   let originalProfilePicture = $userAvatar.attr('src');
 
-  $avatarItem.on('click', function() {
-    toggleEditButtonIf(avatarChanged());
-  });
-
   $userForm.on('change keyup', function() {
     toggleEditButtonIf(dataChanged());
+  });
+
+  $avatarItem.on('click', function() {
+    $userAvatar.attr('src', $(this).attr('src'));
+    $avatarPicker.modal('hide');
+
+    const clickedAvatarId = $(this).attr('mu-avatar-id');
+    avatarId = clickedAvatarId || "";
+
+    toggleEditButtonIf(avatarChanged());
   });
 
   function toggleEditButtonIf(criteria) {
@@ -23,11 +33,39 @@ mumuki.load(function() {
     }
   }
 
+  function dataChanged() {
+    return $userForm.serialize() != originalData;
+  }
+
   function avatarChanged() {
     return ($userAvatar.attr('src') != originalProfilePicture);
   }
 
-  function dataChanged() {
-    return $userForm.serialize() != originalData;
+  $('#mu-user-image').on('click', function(){
+    userImage = $userAvatar.attr('src');
+  });
+
+  $userForm.on('submit', function(){
+    if (userImage) {
+      setImageUrl($(this), userImage);
+      setAvatarId($(this), "");
+    }
+
+    if (avatarId) {
+      setAvatarId($(this), avatarId);
+    }
+  });
+
+  function setImageUrl(form, url) {
+    form.append(`<input type="hidden" name="user[image_url]" value="${url}"/>`);
   }
+
+  function setAvatarId(form, id) {
+    form.append(`<input type="hidden" name="user[avatar_id]" value=${id}/>`);
+  }
+
+  $("#mu-edit-avatar-icon").on('click', function(){
+    $avatarPicker.modal();
+  });
+
 });
