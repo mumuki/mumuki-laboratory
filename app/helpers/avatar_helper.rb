87b97041de0cd77d13dd1031dcc3a9feaa94c510
Felipe Calvo <felipe@mumuki.org>
2020-07-17 12:55:32 -0300

    Show current avatar on picker even if target doesn't match

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,9 +1,24 @@
+require 'pry'
+
 module AvatarHelper
-  def avatars_for(_user, organization)
-    Avatar.select { |avatar| avatar.target == organization.target }
-  end
+  def avatars_for(user, organization)
+    avatars = target_avatars_for(organization)
+    avatars << user.avatar if exists_and_not_in?(user.avatar, avatars)
+
+    avatars
+    end
 
   def show_avatar_item(item)
     "<img src='#{item.image_url}' alt='#{item.description}' mu-avatar-id='#{item.id}' class='mu-avatar-item'>".html_safe
+  end
+
+  private
+
+  def target_avatars_for(organization)
+    Avatar.select { |avatar| avatar.target == organization.target }
+  end
+
+  def exists_and_not_in?(avatar, avatars)
+    avatar && (!avatar.in? avatars)
   end
 end
