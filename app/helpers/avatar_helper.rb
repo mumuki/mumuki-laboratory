7a38258ba0f9392d1cd0fcaf4127b93bb3300a7e
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-07-17 16:29:35 -0300

    Adding tests and considering current avatar

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,6 +1,6 @@
 module AvatarHelper
   def avatars_for(user)
-    Avatar.with_current_visual_identity_for user
+    (Avatar.with_current_visual_identity_for(user) + [user.avatar]).compact.uniq
   end
 
   def show_avatar_item(item)
