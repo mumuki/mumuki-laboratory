1e99fe5ab27cf1c8de6b2b2311885f9aaa67147e
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-07-23 11:53:19 -0300

    Running domain missing migrations

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717143830) do
+ActiveRecord::Schema.define(version: 20200702165503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
