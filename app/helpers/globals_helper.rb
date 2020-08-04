e3506a5831583a931107d60a1186f9c8231950a8
Franco Bulgarelli <franco@mumuki.org>
2020-08-04 20:19:28 -0300

    Refactoring globals

--- app/helpers/globals_helper.rb
+++ app/helpers/globals_helper.rb
@@ -1,10 +1,14 @@
-module LocaleHelper
-  def locale_tags
+module GlobalsHelper
+  def globals_tags
     %Q{
       <script type="text/javascript">
         window.mumukiLocale = #{raw Organization.current.locale_json};
         mumuki.locale = '#{Organization.current.locale}';
         moment.locale('#{Organization.current.locale}');
+
+        mumuki.incognitoMode = #{incognito_mode?};
+
+        mumuki.version = '#{Mumuki::Laboratory::VERSION}';
       </script>
     }.html_safe
   end
--- app/helpers/version_helper.rb
+++
@@ -1,5 +0,0 @@
-module VersionHelper
-  def version_tags
-    %Q{<script type="text/javascript">mumuki.version = '#{Mumuki::Laboratory::VERSION}';</script>}.html_safe
-  end
-end
