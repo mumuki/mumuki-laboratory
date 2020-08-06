cff1de7fcb27e11f411d26e564c635868e913ef6
Franco Bulgarelli <franco@mumuki.org>
2020-08-06 15:51:58 -0300

    Adding command to check JS async errors

--- app/assets/javascripts/mumuki_laboratory/application/codemirror.js
+++ app/assets/javascripts/mumuki_laboratory/application/codemirror.js
@@ -80,7 +80,7 @@
 
   mumuki.load(() => {
     mumuki.page.editors = createCodeMirrors();
-    mumuki.submission.registerContentSyncer(mumuki.editor.syncContent);
+    mumuki.editors.registerContentSyncer(mumuki.editor.syncContent);
     updateCodeMirrorLanguage();
     onSelectUpdateCodeMirror();
 
