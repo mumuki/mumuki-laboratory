37030bf718f5bcd266396ecef00676958731e637
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-08-06 12:26:37 -0300

    Adding more tests for discussions messages controller

---
+++ spec/controllers/discussions_messages_controller_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe DiscussionsMessagesController, organization_workspace: :test do
+  let(:student) { create(:user, permissions: {student: 'test/*'}) }
+  let(:moderator) { create(:user, permissions: {moderator: 'test/*', student: 'test/*'}) }
+
+  let(:exercise) { create(:exercise) }
+  let(:discussion) { create(:discussion, item: exercise, organization: Organization.current) }
+
+  before { Organization.current.update! forum_enabled: true }
+
+  describe 'post' do
+    describe 'for student' do
+      before { set_current_user! student }
+      before { allow_any_instance_of(DiscussionsMessagesController).to receive(:message_params).and_return content: 'Need help' }
+      before { post :create, params: {discussion_id: discussion.id} }
+
+      it { expect(response.status).to eq 302 }
+      it { expect(discussion.messages.size).to eq 1 }
+      it { expect(discussion.messages.last.content).to eq 'Need help' }
+    end
+
+    describe 'for moderator' do
+      before { set_current_user! moderator }
+      before { allow_any_instance_of(DiscussionsMessagesController).to receive(:message_params).and_return content: 'Do this!' }
+      before { post :create, params: {discussion_id: discussion.id} }
+
+      it { expect(response.status).to eq 302 }
+      it { expect(discussion.messages.size).to eq 1 }
+      it { expect(discussion.messages.last.content).to eq 'Do this!' }
+    end
+  end
+
+  describe 'approve' do
+    let(:message) { create(:message, discussion: discussion, sender: student.uid) }
+
+    describe 'for student' do
+      before { set_current_user! student }
+      before { post :approve, params: {id: message.id, discussion_id: discussion.id} }
+
+      it { expect(response.status).to eq 403 }
+      it { expect(message.reload.approved).to be false }
+    end
+
+    describe 'for student' do
+      before { set_current_user! moderator }
+      before { post :approve, params: {id: message.id, discussion_id: discussion.id} }
+
+      it { expect(response.status).to eq 200 }
+      it { expect(message.reload.approved).to be true }
+    end
+  end
+
+  describe 'question' do
+    let(:message) { create(:message, discussion: discussion, sender: student.uid) }
+
+    describe 'for student' do
+      before { set_current_user! student }
+      before { post :question, params: {id: message.id, discussion_id: discussion.id} }
+
+      it { expect(response.status).to eq 403 }
+      it { expect(message.reload.not_actually_a_question).to be false }
+    end
+
+    describe 'for student' do
+      before { set_current_user! moderator }
+      before { post :question, params: {id: message.id, discussion_id: discussion.id} }
+
+      it { expect(response.status).to eq 200 }
+      it { expect(message.reload.not_actually_a_question).to be true }
+    end
+  end
+end
