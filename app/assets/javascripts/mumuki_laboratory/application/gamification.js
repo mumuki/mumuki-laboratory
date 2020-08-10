b9906812bb79413f40a5c08b1eaab99d5cb6a56a
Felipe Calvo <felipe@mumuki.org>
2020-08-09 21:05:50 -0300

    Add base formula for level calculation

---
+++ app/assets/javascripts/mumuki_laboratory/application/gamification.js
@@ -0,0 +1,5 @@
+class Formula {
+  static get QUADRATIC_COEFFICIENT()  { return 25; }
+  static get LINEAR_COEFFICIENT()     { return 100; }
+  static get CONSTANT_TERM()          { return -125; }
+}
