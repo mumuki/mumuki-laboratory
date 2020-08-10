812dd1d326d0ef40ea0dffa9a2871e5baeb391af
Franco Bulgarelli <franco@mumuki.org>
2020-08-10 12:27:38 -0300

    Simplifying new modules

--- app/assets/javascripts/mumuki_laboratory/application/current-exercise.js
+++ app/assets/javascripts/mumuki_laboratory/application/current-exercise.js
@@ -1,13 +1,11 @@
 /** @type {number} */
 mumuki.currentExerciseId = null;
-(() => {
-  mumuki.load(() => {
-    // Set global currentExerciseId
-    const $muExerciseId = $('#mu-exercise-id');
-    if ($muExerciseId) {
-      mumuki.currentExerciseId = Number($muExerciseId.val());
-    } else {
-      mumuki.currentExerciseId = null;
-    }
-  })
-})();
+mumuki.load(() => {
+  // Set global currentExerciseId
+  const $muExerciseId = $('#mu-exercise-id');
+  if ($muExerciseId) {
+    mumuki.currentExerciseId = Number($muExerciseId.val());
+  } else {
+    mumuki.currentExerciseId = null;
+  }
+})
--- app/assets/javascripts/mumuki_laboratory/application/submissions-store.js
+++ app/assets/javascripts/mumuki_laboratory/application/submissions-store.js
@@ -1,93 +1,89 @@
-mumuki.SubmissionsStore = (() => {
-  const SubmissionsStore = new class {
-    /**
-     * Returns the submission's result status for the last submission to
-     * the given exercise, or pending, if not present
-     *
-     * @param {number} exerciseId
-     * @returns {SubmissionStatus}
-     */
-    getLastSubmissionStatus(exerciseId) {
-      const submission = this.getLastSubmissionAndResult(exerciseId);
-      return submission ? submission.result.status : 'pending';
-    }
+mumuki.SubmissionsStore = new class {
+  /**
+   * Returns the submission's result status for the last submission to
+   * the given exercise, or pending, if not present
+   *
+   * @param {number} exerciseId
+   * @returns {SubmissionStatus}
+   */
+  getLastSubmissionStatus(exerciseId) {
+    const submission = this.getLastSubmissionAndResult(exerciseId);
+    return submission ? submission.result.status : 'pending';
+  }
 
-    /**
-     * Returns the submission and result for the last submission to
-     * the given exercise
-     *
-     * @param {number} exerciseId
-     * @returns {SubmissionAndResult}
-     */
-    getLastSubmissionAndResult(exerciseId) {
-      const submissionAndResult = window.localStorage.getItem(this._keyFor(exerciseId));
-      if (!submissionAndResult) return null;
-      return JSON.parse(submissionAndResult);
-    }
+  /**
+   * Returns the submission and result for the last submission to
+   * the given exercise
+   *
+   * @param {number} exerciseId
+   * @returns {SubmissionAndResult}
+   */
+  getLastSubmissionAndResult(exerciseId) {
+    const submissionAndResult = window.localStorage.getItem(this._keyFor(exerciseId));
+    if (!submissionAndResult) return null;
+    return JSON.parse(submissionAndResult);
+  }
 
-    /**
-     * Saves the result for the given exercise
-     *
-     * @param {number} exerciseId
-     * @param {SubmissionAndResult} submissionAndResult
-     */
-    setSubmissionResultFor(exerciseId, submissionAndResult) {
-      window.localStorage.setItem(this._keyFor(exerciseId), this._asString(submissionAndResult));
-    }
+  /**
+   * Saves the result for the given exercise
+   *
+   * @param {number} exerciseId
+   * @param {SubmissionAndResult} submissionAndResult
+   */
+  setSubmissionResultFor(exerciseId, submissionAndResult) {
+    window.localStorage.setItem(this._keyFor(exerciseId), this._asString(submissionAndResult));
+  }
 
-    /**
-     * Retrieves the last cached, non-aborted result for the given submission of the given exercise
-     *
-     * @param {number} exerciseId
-     * @param {Submission} submission
-     * @returns {SubmissionResult} the cached result for this submission
-     */
-    getSubmissionResultFor(exerciseId, submission) {
-      const last = this.getLastSubmissionAndResult(exerciseId);
-      if (!last
-          || last.result.status === 'aborted'
-          || !this.submissionSolutionEquals(last.submission, submission)) {
-        return null;
-      }
-      return last.result;
+  /**
+   * Retrieves the last cached, non-aborted result for the given submission of the given exercise
+   *
+   * @param {number} exerciseId
+   * @param {Submission} submission
+   * @returns {SubmissionResult} the cached result for this submission
+   */
+  getSubmissionResultFor(exerciseId, submission) {
+    const last = this.getLastSubmissionAndResult(exerciseId);
+    if (!last
+        || last.result.status === 'aborted'
+        || !this.submissionSolutionEquals(last.submission, submission)) {
+      return null;
     }
-
-     /**
-     * Extract the submission's solution content
-     *
-     * @param {Submission} submission
-     * @returns {string}
-     */
-    submissionSolutionContent(submission) {
-      if (submission.solution) {
-        return submission.solution.content;
-      } else {
-        return submission['solution[content]'];
-      }
-    }
+    return last.result;
+  }
 
     /**
-     * Compares two solutions to determine if they are equivalent
-     * from the point of view of the code evaluation
-     *
-     * @param {Submission} one
-     * @param {Submission} other
-     * @returns {boolean}
-     */
-    submissionSolutionEquals(one, other) {
-      return this.submissionSolutionContent(one) === this.submissionSolutionContent(other);
+   * Extract the submission's solution content
+   *
+   * @param {Submission} submission
+   * @returns {string}
+   */
+  submissionSolutionContent(submission) {
+    if (submission.solution) {
+      return submission.solution.content;
+    } else {
+      return submission['solution[content]'];
     }
+  }
 
-    // private API
+  /**
+   * Compares two solutions to determine if they are equivalent
+   * from the point of view of the code evaluation
+   *
+   * @param {Submission} one
+   * @param {Submission} other
+   * @returns {boolean}
+   */
+  submissionSolutionEquals(one, other) {
+    return this.submissionSolutionContent(one) === this.submissionSolutionContent(other);
+  }
 
-    _asString(object) {
-      return JSON.stringify(object);
-    }
+  // private API
 
-    _keyFor(exerciseId) {
-      return `/exercise/${exerciseId}/submission`;
-    }
-  };
+  _asString(object) {
+    return JSON.stringify(object);
+  }
 
-  return SubmissionsStore;
-})();
+  _keyFor(exerciseId) {
+    return `/exercise/${exerciseId}/submission`;
+  }
+};
