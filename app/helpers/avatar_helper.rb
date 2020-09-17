d9171945f86238365bb883b2f8e6760314cd2853
Felipe Calvo <felipe@mumuki.org>
2020-09-17 17:25:32 -0300

    Add locked medals on avatar picker

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -3,7 +3,15 @@ module AvatarHelper
     (Avatar.with_current_audience_for(user) + user.acquired_medals + [user.avatar]).compact.uniq
   end
 
-  def show_avatar_item(item)
-    avatar_image(item.image_url, alt: item.description, 'mu-avatar-id': item.id, class: 'mu-avatar-item', type: item.class.name)
+  def locked_avatars_for(user)
+    user.unacquired_medals.compact.uniq
+  end
+
+  def show_locked_avatar_item(item)
+    show_avatar_item(item, class: 'mu-avatar-item mu-locked')
+  end
+
+  def show_avatar_item(item, **options)
+    avatar_image(item.image_url, alt: item.description, 'mu-avatar-id': item.id, class: 'mu-avatar-item', type: item.class.name, **options)
   end
 end
