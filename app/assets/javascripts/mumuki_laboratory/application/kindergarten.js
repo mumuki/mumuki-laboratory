20461c40e6889a040523f72eb1faf8d23f6cb21c
Federico Scarpa <fedescarpa@gmail.com>
2020-09-25 15:32:39 -0300

    Add light speach bubble

--- app/assets/javascripts/mumuki_laboratory/application/kindergarten.js
+++ app/assets/javascripts/mumuki_laboratory/application/kindergarten.js
@@ -1,8 +1,8 @@
 mumuki.load(() => {
   mumuki.kindergarten = {
-    speak(text, locale) {
+    speak(selector, locale) {
       const msg = new SpeechSynthesisUtterance();
-      msg.text = text;
+      msg.text = $(selector).text();
       msg.lang = locale.split('_')[0];
       msg.pitch = 0;
       window.speechSynthesis.speak(msg);
@@ -17,6 +17,15 @@ mumuki.load(() => {
         $button.css('cursor', 'not-allowed');
         $button.children('i').removeClass('fa-volume-up').addClass('fa-volume-off');
       }
+    },
+    toggleHint() {
+      $('.mu-kindergarten-light-speech-bubble').toggleClass('open');
+    },
+    toggleHintMedia() {
+      const $hintMedia = $('.mu-kindergarten-hint-media');
+      const $i = $('.expand-or-collapse-hint-media').children('i');
+      $i.toggleClass('fa-caret-down').toggleClass('fa-caret-up');
+      $hintMedia.toggleClass('open');
     }
   };
 
