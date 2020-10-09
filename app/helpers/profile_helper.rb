b28268e6d9d8fac2b346150f3e836c9a1931acfd
Felipe Calvo <felipe@mumuki.org>
2020-10-09 12:23:32 -0300

    Extract cancel/save profile buttons to helper

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -2,4 +2,12 @@ module ProfileHelper
   def edit_profile_button
     link_to t(:edit_profile), edit_user_path, class: 'btn btn-success'
   end
+
+  def cancel_edit_profile_button
+    link_to t(:cancel), :back, class: 'btn btn-default' if current_user.profile_completed?
+  end
+
+  def save_edit_profile_button(form)
+    form.submit t(:save), disabled: true, class: 'btn btn-success mu-edit-profile-btn'
+  end
 end
