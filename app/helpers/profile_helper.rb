3c24dbf422fc7112484bb6889b9e68bcbac01258
luchotc <luchotc@users.noreply.github.com>
2020-10-14 14:47:10 -0300

    Changing back for user_path in cancel_edit_profile_button
    Co-authored-by: felipecalvo <felipecalvo@users.noreply.github.com>

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -4,7 +4,7 @@ module ProfileHelper
   end
 
   def cancel_edit_profile_button
-    link_to t(:cancel), :back, class: 'btn btn-default' if current_user.profile_completed?
+    link_to t(:cancel), :user_path, class: 'btn btn-default' if current_user.profile_completed?
   end
 
   def save_edit_profile_button(form)
