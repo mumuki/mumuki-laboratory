883535380ceb84084144e86439a870e609d4feda
Federico Aloi <federico.aloi@gmail.com>
2020-10-19 13:07:39 -0300

    Travis - Run Capybara tests on Safari

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -1,25 +1,7 @@
 require 'capybara/rails'
 require 'capybara/rspec'
 
-def selected_driver
-  ENV['MUMUKI_CAPYBARA_DRIVER']&.to_sym || :rack_test
-end
-
-def run_with_selenium?
-  selected_driver.to_s.start_with? 'selenium'
-end
-
-Capybara.default_driver = selected_driver
-
-if run_with_selenium?
-  # TODO: fix the tests that depend on hidden elements and remove this
-  Capybara.ignore_hidden_elements = false
-
-  # Include port on the URL, so we don't need to forward it via nginx or so
-  Capybara.always_include_port = true
-end
-
-puts "Running Capybara tests with #{selected_driver}, #{Capybara.ignore_hidden_elements ? '' : 'not '}ignoring hidden elements"
+# Test helpers
 
 def set_subdomain_host!(subdomain)
   @request.try { |it| it.host = Mumukit::Platform.laboratory.organic_domain subdomain }
@@ -31,3 +13,41 @@ def set_implicit_central!
   @request.try { |it| it.host = Mumukit::Platform.laboratory.domain }
   Capybara.app_host = Mumukit::Platform.laboratory.url
 end
+
+# Config helpers
+
+def selected_driver
+  ENV['MUMUKI_CAPYBARA_DRIVER']&.to_sym || :rack_test
+end
+
+def run_with_selenium?
+  selected_driver.to_s.start_with? 'selenium'
+end
+
+def register_safari_driver!
+  # No official docs for this, code was taken from https://github.com/teamcapybara/capybara/blob/master/spec/selenium_spec_safari.rb
+  if ::Selenium::WebDriver::Service.respond_to? :driver_path=
+    ::Selenium::WebDriver::Safari::Service
+  else
+    ::Selenium::WebDriver::Safari
+  end.driver_path = '/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver'
+
+  Capybara.register_driver :selenium_safari do |app|
+    Capybara::Selenium::Driver.new(app, browser: :safari, options: browser_options, timeout: 30)
+  end
+end
+
+# Configuration
+
+register_safari_driver! if selected_driver == :selenium_safari
+Capybara.default_driver = selected_driver
+
+if run_with_selenium?
+  # TODO: fix the tests that depend on hidden elements and remove this
+  Capybara.ignore_hidden_elements = false
+
+  # Include port on the URL, so we don't need to forward it via nginx or so
+  Capybara.always_include_port = true
+end
+
+puts "Running Capybara tests with #{selected_driver}, #{Capybara.ignore_hidden_elements ? '' : 'not '}ignoring hidden elements"
