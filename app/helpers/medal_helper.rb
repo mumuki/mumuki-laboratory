c315337e745ac6edd2109f764392b5bacaea8665
Felipe Calvo <felipe@mumuki.org>
2020-10-22 12:19:24 -0300

    Show medal on guide view

---
+++ app/helpers/medal_helper.rb
@@ -0,0 +1,11 @@
+module MedalHelper
+  def content_medal_for(content, user)
+    image_tag content.medal.image_url, class: "mu-medal content #{completion_class_for content, user}"
+  end
+
+  private
+
+  def completion_class_for(content, user)
+    content.once_completed_for?(user, Organization.current) ? '' : 'unacquired'
+  end
+end
