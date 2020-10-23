fe3ce23210c73588dd2e6a2227a5147385343c5d
Felipe Calvo <felipe@mumuki.org>
2020-10-23 14:40:26 -0300

    Extract duplicated logic on MedalHelper

--- app/helpers/medal_helper.rb
+++ app/helpers/medal_helper.rb
@@ -1,13 +1,17 @@
 module MedalHelper
   def corollary_medal_for(content)
-    image_tag content.medal.image_url, class: 'mu-medal corollary'
+    medal_image_for content, 'corollary'
   end
 
   def content_medal_for(content, user)
-    image_tag content.medal.image_url, class: "mu-medal content #{completion_class_for content, user}"
+    medal_image_for content, "content #{completion_class_for content, user}"
   end
 
   private
+
+  def medal_image_for(content, clazz)
+    image_tag content.medal.image_url, class: "mu-medal #{clazz}"
+  end
 
   def completion_class_for(content, user)
     content.once_completed_for?(user, Organization.current) ? '' : 'unacquired'
