2617cb93d39e00141828f3f11554d356d9704d1a
Felipe Calvo <felipe@mumuki.org>
2020-10-30 17:12:26 -0300

    Render whole medal in one line

--- app/helpers/medal_helper.rb
+++ app/helpers/medal_helper.rb
@@ -3,26 +3,39 @@ module MedalHelper
     content.medal.present? && organization.gamification_enabled?
   end
 
-  def corollary_medal_for(content)
-    medal_image_for content, 'inlay corollary'
-  end
-
   def content_medal_for(content, user)
-    medal_image_for content, "inlay content #{completion_class_for content, user}"
+    completion = completion_class_for content, user
+    "#{content_medal_outline completion} #{content_medal_inlay_for(content, completion)}".html_safe
   end
 
-  def content_medal_outline
-    image_tag '/medal/outline.svg', class: "mu-medal outline content"
-  end
-
-  def corollary_medal_outline
-    image_tag '/medal/outline.svg', class: "mu-medal outline corollary"
+  def corollary_medal_for(content)
+    "#{corollary_medal_outline} #{corollary_medal_inlay_for(content)}".html_safe
   end
 
   private
 
-  def medal_image_for(content, clazz)
+  def content_medal_inlay_for(content, completion)
+    inlay_image_for content, "inlay content #{completion}"
+  end
+
+  def corollary_medal_inlay_for(content)
+    inlay_image_for content, 'inlay corollary'
+  end
+
+  def content_medal_outline(completion)
+    outline_image "content #{completion}"
+  end
+
+  def corollary_medal_outline
+    outline_image "corollary"
+  end
+
+  def inlay_image_for(content, clazz)
     image_tag content.medal.image_url, class: "mu-medal #{clazz}"
+  end
+
+  def outline_image(clazz)
+    image_tag '/medal/outline.svg', class: "mu-medal outline #{clazz}"
   end
 
   def completion_class_for(content, user)
