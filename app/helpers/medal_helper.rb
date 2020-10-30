e067070e2d98cc9a5836d49ff99022334584069c
Felipe Calvo <felipe@mumuki.org>
2020-10-30 17:58:32 -0300

    Remove duplicated logic in medal helper

--- app/helpers/medal_helper.rb
+++ app/helpers/medal_helper.rb
@@ -4,41 +4,33 @@ module MedalHelper
   end
 
   def content_medal_for(content, user)
-    completion = completion_class_for content, user
-    "#{content_medal_outline completion} #{content_medal_inlay_for(content, completion)}".html_safe
+    medal_for content, 'content', completion_class_for(content, user)
   end
 
   def corollary_medal_for(content)
-    "#{corollary_medal_outline} #{corollary_medal_inlay_for(content)}".html_safe
+    medal_for content, 'corollary'
   end
 
   private
 
-  def content_medal_inlay_for(content, completion)
-    inlay_image_for content, "inlay content #{completion}"
+  def completion_class_for(content, user)
+    content.once_completed_for?(user, Organization.current) ? '' : 'unacquired'
   end
 
-  def corollary_medal_inlay_for(content)
-    inlay_image_for content, 'inlay corollary'
-  end
-
-  def content_medal_outline(completion)
-    outline_image "content #{completion}"
-  end
-
-  def corollary_medal_outline
-    outline_image "corollary"
-  end
-
-  def inlay_image_for(content, clazz)
-    image_tag content.medal.image_url, class: "mu-medal #{clazz}"
+  def medal_for(content, view, completion='')
+    %Q{
+      <div class="mu-medal #{completion}">
+        #{outline_image(view)}
+        #{inlay_image_for(content, view)}
+      </div>
+    }.html_safe
   end
 
   def outline_image(clazz)
     image_tag '/medal/outline.svg', class: "mu-medal outline #{clazz}"
   end
 
-  def completion_class_for(content, user)
-    content.once_completed_for?(user, Organization.current) ? '' : 'unacquired'
+  def inlay_image_for(content, clazz)
+    image_tag content.medal.image_url, class: "mu-medal inlay #{clazz}"
   end
 end
