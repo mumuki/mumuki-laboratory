2342952b5d5ce6b5798c9d8199e4e245c190b3bc
Felipe Calvo <felipe@mumuki.org>
2020-11-17 09:51:37 -0300

    Consider a context may be gamified if the user has game mode enabled

--- app/helpers/gamification_helper.rb
+++ app/helpers/gamification_helper.rb
@@ -1,5 +1,13 @@
 module GamificationHelper
-  def in_gamified_context?
+  def in_gamified_context?(user)
+    gamified_organization? || gamified_user?(user)
+  end
+
+  def gamified_organization?
     Organization.current.gamification_enabled?
+  end
+
+  def gamified_user?(user)
+    UserStats.game_mode_enabled_for? user
   end
 end
