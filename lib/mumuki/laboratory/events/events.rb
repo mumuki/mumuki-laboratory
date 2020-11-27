a051f7b8f59543d37840c6290f0df06f03d17382
Julián Berbel Alt <julian@mumuki.org>
2020-11-27 10:07:39 -0300

    Delete deprecated events

--- lib/mumuki/laboratory/events/events.rb
+++ lib/mumuki/laboratory/events/events.rb
@@ -1,15 +1,6 @@
 Mumukit::Nuntius::EventConsumer.handle do
-
   # Emitted by assigment manual evaluation in classroom
   event 'AssignmentManuallyEvaluated' do |payload|
     Assignment.evaluate_manually! payload.deep_symbolize_keys[:assignment]
-  end
-
-  [Book, Topic, Guide].each do |it|
-    event "#{it.name}Changed" do |data|
-      Mumukit::Sync::Syncer.new(
-        Mumukit::Sync::Store::Bibliotheca.new(
-          Mumukit::Platform.bibliotheca_bridge)).locate_and_import! it, data[:slug]
-    end
   end
 end
