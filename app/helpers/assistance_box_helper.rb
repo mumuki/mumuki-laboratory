03cd70fc229ecf872b35d6d5c40e86b560ad83ec
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-11-27 17:48:42 -0300

    Remove unnecessary if

--- app/helpers/assistance_box_helper.rb
+++ app/helpers/assistance_box_helper.rb
@@ -1,10 +1,8 @@
 module AssistanceBoxHelper
   def assistance_box(assignment)
-    if assignment.tips.present?
-      %Q{<div class="mu-tips-box">
-        #{Mumukit::Assistant::Narrator.random.compose_explanation_html assignment.tips}
-      </div>}.html_safe
-    end
+    %Q{<div class="mu-tips-box">
+      #{Mumukit::Assistant::Narrator.random.compose_explanation_html assignment.tips}
+    </div>}.html_safe
   end
 
   def should_display_assistance_box?(assignment)
