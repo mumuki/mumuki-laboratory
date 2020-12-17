1ce4c261092ad1653f598473235015f199b51e4a
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-12-17 14:37:02 -0300

    Use domain updated method to simplify logic

--- app/controllers/guide_container_controller.rb
+++ app/controllers/guide_container_controller.rb
@@ -4,18 +4,13 @@ class GuideContainerController < ApplicationController
   before_action :set_guide
 
   def show
-    if current_user?
-      @stats = subject.stats_for(current_user)
-      @next_exercise = subject.next_exercise(current_user)
-    else
-      @next_exercise = subject.first_exercise
-    end
+    @stats = subject.stats_for(current_user)
+    @next_exercise = subject.next_exercise(current_user)
   end
 
   private
 
   def set_guide
-    raise Mumuki::Domain::NotFoundError if subject.nil?
     @guide = subject.guide
   end
 end
