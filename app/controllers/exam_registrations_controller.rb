6ec2a66b801c701b12d5e42e7fc1930b496fcdfe
Federico Aloi <federico.aloi@gmail.com>
2021-01-29 16:06:30 -0300

    Delegate auth request creation to model

--- app/controllers/exam_registrations_controller.rb
+++ app/controllers/exam_registrations_controller.rb
@@ -1,6 +1,6 @@
 class ExamRegistrationsController < ApplicationController
   def show
     @registration = ExamRegistration.find(params[:id])
-    @authorization_request = @registration.authorization_request_for(current_user) || ExamAuthorizationRequest.new(exam_registration: @registration)
+    @authorization_request = @registration.authorization_request_for(current_user)
   end
 end
