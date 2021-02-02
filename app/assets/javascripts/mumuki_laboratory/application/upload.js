e4930ea79a35164ad3e9d5084e0ab1d97cc8554f
Felipe Calvo <felipe@mumuki.org>
2021-02-02 20:28:51 -0300

    Prevent uploading of files over 256kb

--- app/assets/javascripts/mumuki_laboratory/application/upload.js
+++ app/assets/javascripts/mumuki_laboratory/application/upload.js
@@ -1,11 +1,28 @@
 mumuki.load(() => {
-  $('#upload-input').change(function (evt) {
+  let $uploadInput = $('#upload-input');
+  let maxFileSize = $uploadInput.attr("mu-upload-file-limit");
+  let $uploadFileLimitExceeded = $('#mu-upload-file-limit-exceeded');
+  let $uploadLabel = $('#mu-upload-label span');
+  let $uploadLabelText = $uploadLabel.text();
+  let $uploadIcon = $('#mu-upload-icon');
+  let $btnSubmit = $('.btn-submit');
+
+  $uploadInput.change(function (evt) {
     var file = evt.target.files[0];
     if (!file) return;
 
-    $('.btn-submit').removeClass('disabled');
-    $('#mu-upload-text').removeClass('fa-upload').addClass('fa-file-alt');
-    $('#mu-upload-label span').text(file.name);
+    if (file.size > maxFileSize) {
+      $uploadFileLimitExceeded.removeClass('hidden');
+      $uploadLabel.text($uploadLabelText);
+      $uploadIcon.addClass('fa-upload').removeClass('fa-file-alt');
+      $btnSubmit.addClass('disabled');
+      return;
+    }
+
+    $uploadFileLimitExceeded.addClass('hidden');
+    $uploadLabel.text(file.name);
+    $uploadIcon.removeClass('fa-upload').addClass('fa-file-alt');
+    $btnSubmit.removeClass('disabled');
 
     var reader = new FileReader();
     reader.onload = function (e) {
