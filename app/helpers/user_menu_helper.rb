e20a1c73808ad7dc014dcbb4eb873e5a9c2e4cec
Felipe Calvo <felipecalvo239@gmail.com>
2021-02-03 20:38:05 -0300

    Add messages and discussions to user menu

--- app/helpers/user_menu_helper.rb
+++ app/helpers/user_menu_helper.rb
@@ -2,4 +2,12 @@ module UserMenuHelper
   def profile_user_menu_link
     link_to t(:profile), user_path
   end
+
+  def messages_user_menu_link
+    link_to t(:messages), messages_user_path
+  end
+
+  def discussions_user_menu_link
+    link_to t(:discussions), discussions_user_path
+  end
 end
