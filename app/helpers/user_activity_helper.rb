f637d0b16fe6e26ee1e17d4ffd53e5a0ec7aa825
Federico Aloi <federico.aloi@gmail.com>
2021-02-11 18:50:28 -0300

    Add feature specs for activity

--- app/helpers/user_activity_helper.rb
+++ app/helpers/user_activity_helper.rb
@@ -12,6 +12,30 @@ module UserActivityHelper
     active_period?(period_start) && 'class=active'
   end
 
+  def solved_exercises_percentage
+    percentage = @activity[:exercises][:solved_count].to_f / @activity[:exercises][:count] * 100
+    "#{percentage.ceil}%"
+  end
+
+  def exercises_activity_stats
+    solved_count = {
+        name: t(:solved_exercises_count, count: @activity[:exercises][:solved_count]),
+        value: @activity[:exercises][:solved_count] }
+    solved_percentage = {
+        name: t(:solved_exercises_percentage),
+        value: solved_exercises_percentage }
+
+    @date_from ? [solved_count] : [solved_count, solved_percentage]
+  end
+
+  def messages_activity_stats
+    count = @activity[:messages][:count]
+    approved = @activity[:messages][:approved]
+
+    [{name: t(:messages_pluralized, count: count), value: count},
+     {name: t(:approved_messages, count: approved), value: approved}]
+  end
+
   private
 
   def active_period?(period_start)
---
+++ spec/features/user_activity_flow_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+feature 'User Activity Flow', organization_workspace: :test do
+  let(:user) { create(:user) }
+  before { set_current_user!(user) }
+
+  let(:fake_stats) { double('stats') }
+  before do
+    allow(UserStats).to receive(:stats_for).and_return(fake_stats)
+    allow(fake_stats).to receive(:activity).and_return(
+        { exercises: { solved_count: 8, count: 10 },
+          messages: { count: 12, approved: 6 } })
+  end
+
+  let(:week_start) { Time.new(2020, 10, 12) }
+  before do
+    allow(Time).to receive(:now).and_return(week_start)
+  end
+
+  context 'total' do
+    before { visit activity_user_path }
+
+    scenario 'displays exercises done percentage' do
+      expect(page).to have_text('80%')
+      expect(page).to have_text('done')
+    end
+
+    scenario 'displays exercises solved count' do
+      expect(page).to have_text('8')
+      expect(page).to have_text('solved')
+    end
+
+    scenario 'displays messages count' do
+      expect(page).to have_text('12')
+      expect(page).to have_text('messages')
+    end
+
+    scenario 'displays validated messages count' do
+      expect(page).to have_text('6')
+      expect(page).to have_text('validated')
+    end
+
+    scenario 'displays recent weeks' do
+      expect(page).to have_text 'Week of 2020-10-12'
+      expect(page).to have_text 'Week of 2020-10-05'
+      expect(page).to have_text 'Week of 2020-09-28'
+    end
+  end
+
+  context 'selecting a specific week' do
+    before do
+      visit activity_user_path
+      click_on 'Week of 2020-10-12'
+    end
+
+    scenario "redirects to week activity" do
+      expect(page).to have_current_path('/user/activity?date_from=2020-10-12&date_to=2020-10-19')
+    end
+
+    scenario "doesn't display exercises done percentage" do
+      expect(page).not_to have_text('80%')
+      expect(page).not_to have_text('done')
+    end
+  end
+end
