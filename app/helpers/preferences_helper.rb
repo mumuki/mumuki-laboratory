d1e76caa51456f34a6debbca4064ab051a1625df
Felipe Calvo <felipe@mumuki.org>
2021-02-12 12:44:47 -0300

    Make uppercase text default for kindergarten organizations

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -1,5 +1,9 @@
 module PreferencesHelper
-  def uppercase_enabled?(user)
-    user.preferences.uppercase? if user
+  def uppercase_enabled?(user, organization)
+    uppercase_organization?(organization) || (user.preferences.uppercase? if user)
+  end
+
+  def uppercase_organization?(organization)
+    organization.kindergarten?
   end
 end
