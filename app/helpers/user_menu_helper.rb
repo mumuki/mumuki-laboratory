bef9d2acbd50314f096ce5a321d926a9b67142f8
Felipe Calvo <felipe@mumuki.org>
2021-03-03 10:53:34 -0300

    Extract repeated logic in user menu items

--- app/helpers/user_menu_helper.rb
+++ app/helpers/user_menu_helper.rb
@@ -1,22 +1,24 @@
 module UserMenuHelper
   def profile_user_menu_link
-    user_menu_link t(:my_profile), user_path, 'show'
+    user_menu_item t(:my_profile), user_path, 'show'
   end
 
   def messages_user_menu_link
-    user_menu_link t(:messages), messages_user_path, 'messages'
+    user_menu_item t(:messages), messages_user_path, 'messages'
   end
 
   def discussions_user_menu_link
-    user_menu_link t(:discussions), discussions_user_path, 'discussions'
+    user_menu_item t(:discussions), discussions_user_path, 'discussions' if current_user&.can_discuss_here?
   end
 
   def certificates_user_menu_link
-    user_menu_link t(:certificates), certificates_user_path, 'certificates'
+    user_menu_item t(:certificates), certificates_user_path, 'certificates'
   end
 
-  def user_menu_link(label, path, active_on)
+  private
+
+  def user_menu_item(label, path, active_on)
     link_klass = 'active' if action_name == active_on
-    link_to label, path, { class: link_klass }.compact
+    content_tag :div, link_to(label, path, { class: link_klass }.compact), class: 'mu-user-menu-item'
   end
 end
