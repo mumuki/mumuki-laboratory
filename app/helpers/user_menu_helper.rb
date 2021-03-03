65f7c271972f2c6989c8b196ccd1548a21326f5e
Lucho Cannavo <luchotc@hotmail.com.ar>
2021-03-03 12:49:00 -0300

    Add active class for active menu_link

--- app/helpers/user_menu_helper.rb
+++ app/helpers/user_menu_helper.rb
@@ -1,13 +1,18 @@
 module UserMenuHelper
   def profile_user_menu_link
-    link_to t(:my_profile), user_path
+    user_menu_link t(:my_profile), user_path, 'show'
   end
 
   def messages_user_menu_link
-    link_to t(:messages), messages_user_path
+    user_menu_link t(:messages), messages_user_path, 'messages'
   end
 
   def discussions_user_menu_link
-    link_to t(:discussions), discussions_user_path
+    user_menu_link t(:discussions), discussions_user_path, 'discussions'
+  end
+
+  def user_menu_link(label, path, active_on)
+    link_klass = 'active' if action_name == active_on
+    link_to label, path, { class: link_klass }.compact
   end
 end
