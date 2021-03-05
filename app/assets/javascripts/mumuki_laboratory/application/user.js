47c07fa92ba45a1e02a9025f2d10d65a570ffdae
Felipe Calvo <felipe@mumuki.org>
2021-03-04 21:13:59 -0300

    Show user menu on profile screen by default

--- app/assets/javascripts/mumuki_laboratory/application/user.js
+++ app/assets/javascripts/mumuki_laboratory/application/user.js
@@ -1,6 +1,12 @@
 mumuki.load(() => {
-  $('.mu-user-menu-header').click(() => {
+  const $userMenuHeader = $('.mu-user-menu-header');
+
+  $userMenuHeader.click(() => {
     $('.mu-user-menu').toggleClass('hidden-sm-screen');
     $('#mu-user-menu-header-icon').toggleClass('fa-chevron-up fa-chevron-down');
   });
+
+  if (document.querySelector('.mu-profile-info') !== null) {
+    $userMenuHeader.click();
+  }
 });
