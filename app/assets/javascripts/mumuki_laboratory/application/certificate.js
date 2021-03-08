0af5536057d7e533cba52cc7b56d3bee68a89cc8
Federico Scarpa <fedescarpa@gmail.com>
2021-03-08 17:01:18 -0300

    removing js function from global scope

--- app/assets/javascripts/mumuki_laboratory/application/certificate.js
+++ app/assets/javascripts/mumuki_laboratory/application/certificate.js
@@ -1,17 +1,17 @@
 mumuki.load(() => {
   scaleCertificate();
   mumuki.resize(scaleCertificate);
-})
 
-function scaleCertificate() {
-  const $certPreview = $('.certificate-preview');
-  const $muCertificate = $('.mu-certificate-box');
-  $muCertificate.css('transform', 'scale(1)');
+  function scaleCertificate() {
+    const $certPreview = $('.certificate-preview');
+    const $muCertificate = $('.mu-certificate-box');
+    $muCertificate.css('transform', 'scale(1)');
 
-  const scaleWidth = $muCertificate.width() / $certPreview.width();
-  $muCertificate.css({
-    'transform': `scale(${1 / scaleWidth})`,
-    'transform-origin': `0 0`,
-  });
-  $certPreview.height($muCertificate.height());
-}
+    const scaleWidth = $muCertificate.width() / $certPreview.width();
+    $muCertificate.css({
+      'transform': `scale(${1 / scaleWidth})`,
+      'transform-origin': `0 0`,
+    });
+    $certPreview.height($muCertificate.height());
+  }
+});
