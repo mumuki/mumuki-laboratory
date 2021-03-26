3212c0f9cd610a3e24d9c097ed8b2057db9c7330
Felipe Calvo <felipe@mumuki.org>
2021-03-26 18:31:21 -0300

    Use fa_icon helper on editor tab titles
    This gives some pixels of padding between the icon and the text,
    which we lost when we upgraded to FontAwesome 5.

--- app/helpers/editor_tabs_helper.rb
+++ app/helpers/editor_tabs_helper.rb
@@ -2,7 +2,7 @@ module EditorTabsHelper
   def extra_code_tab
     "<li role='presentation'>
         <a class='editor-tab nav-link' data-bs-target='#visible-extra' aria-controls='visible-extra' role='tab' data-bs-toggle='tab'>
-          #{fa_icon 'code'} #{t 'activerecord.attributes.exercise.extra'}
+          #{fa_icon 'code', text: (t 'activerecord.attributes.exercise.extra')}
         </a>
     </li>".html_safe
   end
@@ -10,7 +10,7 @@ module EditorTabsHelper
   def console_tab(active: false)
     "<li role='presentation'>
         <a class='editor-tab nav-link #{'active' if active}' data-bs-target='#console' aria-controls='console' tabindex='0' role='tab' data-bs-toggle='tab' >
-          #{fa_icon 'terminal'}#{t :console }
+          #{fa_icon 'terminal', text: (t :console)}
         </a>
      </li>".html_safe
   end
@@ -18,7 +18,7 @@ module EditorTabsHelper
   def messages_tab(exercise, organization = Organization.current)
     "<li id='messages-tab' role='presentation'>
         <a class='editor-tab nav-link' data-bs-target='#messages' tabindex='0' aria-controls='console' role='tab' data-bs-toggle='tab'>
-          #{fa_icon 'comments', type: :regular} #{t :messages }
+          #{fa_icon 'comments', type: :regular, text: (t :messages)}
         </a>
      </li>".html_safe if organization.raise_hand_enabled? && exercise.has_messages_for?(current_user)
   end
