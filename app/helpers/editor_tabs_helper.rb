905eb1574855527264f4733b9616e60767f11f07
Felipe Calvo <felipe@mumuki.org>
2021-03-26 18:22:25 -0300

    Add styles for navigation tabs through nav-link class

--- app/helpers/editor_tabs_helper.rb
+++ app/helpers/editor_tabs_helper.rb
@@ -1,11 +1,15 @@
 module EditorTabsHelper
   def extra_code_tab
-    "<li role='presentation'> <a data-bs-target='#visible-extra' aria-controls='visible-extra' role='tab' data-bs-toggle='tab' class='editor-tab'>#{fa_icon 'code'} #{t 'activerecord.attributes.exercise.extra'}</a> </li>".html_safe
+    "<li role='presentation'>
+        <a class='editor-tab nav-link' data-bs-target='#visible-extra' aria-controls='visible-extra' role='tab' data-bs-toggle='tab'>
+          #{fa_icon 'code'} #{t 'activerecord.attributes.exercise.extra'}
+        </a>
+    </li>".html_safe
   end
 
   def console_tab(active: false)
-    "<li role='presentation' class='#{'active' if active}'>
-        <a data-bs-target='#console' aria-controls='console' tabindex='0' role='tab' data-bs-toggle='tab' class='editor-tab'>
+    "<li role='presentation'>
+        <a class='editor-tab nav-link #{'active' if active}' data-bs-target='#console' aria-controls='console' tabindex='0' role='tab' data-bs-toggle='tab' >
           #{fa_icon 'terminal'}#{t :console }
         </a>
      </li>".html_safe
@@ -13,7 +17,7 @@ module EditorTabsHelper
 
   def messages_tab(exercise, organization = Organization.current)
     "<li id='messages-tab' role='presentation'>
-        <a data-bs-target='#messages' tabindex='0' aria-controls='console' role='tab' data-bs-toggle='tab' class='editor-tab'>
+        <a class='editor-tab nav-link' data-bs-target='#messages' tabindex='0' aria-controls='console' role='tab' data-bs-toggle='tab'>
           #{fa_icon 'comments', type: :regular} #{t :messages }
         </a>
      </li>".html_safe if organization.raise_hand_enabled? && exercise.has_messages_for?(current_user)
