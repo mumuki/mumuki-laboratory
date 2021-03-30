21025dfef44c47c9372b544d0f115b474994c41c
Felipe Calvo <felipe@mumuki.org>
2021-03-30 16:12:49 -0300

    Delegate toast creation to helper

---
+++ app/helpers/toast_helper.rb
@@ -0,0 +1,23 @@
+module ToastHelper
+  def toast_notice(text)
+    toast_for('success', 'check-circle', text)
+  end
+
+  def toast_alert(text)
+    toast_for('error', 'times-circle', text)
+  end
+
+  private
+
+  def toast_for(type, icon, text)
+    "<div class='toast mu-toast-#{type}' role='alert'>
+       <div class='toast-body'>
+         <div class='d-flex align-items-center'>
+           #{fa_icon icon, class: 'fa-lg me-3'}
+           <span class='flex-grow-1 text-break'>#{text}</span>
+           <button type='button' class='btn-close btn-close-white mx-2 flex-shrink-0' data-bs-dismiss='toast' aria-label='Close'></button>
+         </div>
+       </div>
+     </div>".html_safe
+  end
+end
