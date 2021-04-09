5e34ed9523b0ee095b7caaec2938f07ef0497662
Lucho Cannavo <luchotc@hotmail.com.ar>
2021-04-09 13:16:56 -0300

    Add organization in want_permissions mail template

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -12,9 +12,9 @@ See #{transparent_exercise_url(assignment.exercise.transparent_params)}
 EOM
   end
 
-  def permissions_help_email_body(user)
+  def permissions_help_email_body(user, organization)
 <<EOM
-#{t :want_permissions}: #{user.try(:uid)} - #{user.try(:email)}
+#{t :want_permissions} #{organization.name}: #{user.try(:uid)} - #{user.try(:email)}
 EOM
   end
 end
