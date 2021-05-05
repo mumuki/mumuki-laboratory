c43c5eb10fe9e5dce2f22115689d780d3594bdb1
Federico Scarpa <fedescarpa@gmail.com>
2021-05-05 14:58:45 -0300

    Fixing tests

--- spec/controllers/exam_authorization_requests_controller_spec.rb
+++ spec/controllers/exam_authorization_requests_controller_spec.rb
@@ -7,23 +7,6 @@ describe ExamAuthorizationRequestsController, type: :controller, organization_wo
 
   before { set_current_user! user }
 
-  describe 'show' do
-    context 'when authorization does not exist' do
-      before { get :show, params: {id: 0} }
-      it { expect(response.status).to eq 404 }
-    end
-
-    context 'when authorization exists' do
-      let(:exam_authorization_request) { create(:exam_authorization_request, user: user, exam_registration: exam_registration) }
-      let!(:notification) { create(:notification, target: exam_authorization_request, user: user) }
-
-      before { get :show, params: {id: exam_authorization_request.id} }
-
-      it { expect(response.status).to eq 200 }
-      it { expect(notification.reload.read).to be_truthy }
-    end
-  end
-
   describe 'create' do
     let!(:notification) { create(:notification, target: exam_registration, user: user) }
 
@@ -33,7 +16,7 @@ describe ExamAuthorizationRequestsController, type: :controller, organization_wo
       }
     end
 
-    it { expect(response.status).to eq 204 }
+    it { expect(response.status).to eq 302 }
     it { expect(exam_registration.authorization_requests.length).to be 1 }
     it { expect(notification.reload.read).to be_truthy }
   end
