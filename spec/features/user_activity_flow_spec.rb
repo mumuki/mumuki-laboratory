9ba2b44afcf768d4aeb1ab58f221d710a3496938
Felipe Calvo <felipe@mumuki.org>
2021-05-10 15:28:42 -0300

    Add tests for Activity flow on organizations with and without forum

--- spec/features/user_activity_flow_spec.rb
+++ spec/features/user_activity_flow_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 feature 'User Activity Flow', organization_workspace: :test do
   let(:user) { create(:user) }
+  let(:organization) { Organization.locate!('test') }
+
   before { set_current_user!(user) }
 
   let(:fake_stats) { double('stats') }
@@ -30,21 +32,39 @@ feature 'User Activity Flow', organization_workspace: :test do
       expect(page).to have_text('solved')
     end
 
-    scenario 'displays messages count' do
-      expect(page).to have_text('12')
-      expect(page).to have_text('messages')
-    end
-
-    scenario 'displays validated messages count' do
-      expect(page).to have_text('6')
-      expect(page).to have_text('validated')
-    end
-
     scenario 'displays recent weeks' do
       expect(page).to have_text 'Week of 2020-10-12'
       expect(page).to have_text 'Week of 2020-10-05'
       expect(page).to have_text 'Week of 2020-09-28'
     end
+
+    context 'on organization with no forum' do
+      scenario 'does not display messages count' do
+        expect(page).to_not have_text('messages')
+      end
+
+      scenario 'does not display validated messages count' do
+        expect(page).to_not have_text('validated')
+      end
+    end
+
+    context 'on organization with forum' do
+      before do
+        organization.update! forum_enabled: true
+        visit activity_user_path
+      end
+
+      scenario 'displays messages count' do
+        expect(page).to have_text('12')
+        expect(page).to have_text('messages')
+      end
+
+      scenario 'displays validated messages count' do
+        expect(page).to have_text('6')
+        expect(page).to have_text('validated')
+      end
+    end
+
   end
 
   context 'selecting a specific week' do
