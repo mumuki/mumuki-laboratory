592962db6867794ad1136076578e8eca7353761f
Felipe Calvo <felipe@mumuki.org>
2021-05-31 10:00:50 -0300

    Let new toasts be added dynamically

--- app/assets/javascripts/mumuki_laboratory/application/toast.js
+++ app/assets/javascripts/mumuki_laboratory/application/toast.js
@@ -1,6 +1,11 @@
 mumuki.toast = {
   load() {
     document.querySelectorAll('.toast').forEach((toast) => new bootstrap.Toast(toast).show());
+  },
+
+  addToast(content) {
+    $('.toast-container').html(content);
+    mumuki.toast.load();
   }
 };
 
