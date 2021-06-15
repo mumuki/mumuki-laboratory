381c4f825531033a20ef391e1c384a7ec4161750
Federico Scarpa <fedescarpa@gmail.com>
2021-06-15 16:18:17 -0300

    Added helpers and translations

---
+++ app/helpers/exam_registration_helper.rb
@@ -0,0 +1,9 @@
+module ExamRegistrationHelper
+  def exam_registration_view
+    if @registration.end_time.past?
+      { icon: :times_circle, class: :danger, t: :exam_registration_finished_html }
+    else
+      { icon: :info_circle, class: :info, t: :exam_registration_explanation_html }
+    end
+  end
+end
