82401d3f01da1df0f0eccda4aadd404618f0bd65
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-06-29 16:57:56 -0300

    Move current_time_zone helper

--- app/helpers/exam_registration_helper.rb
+++ app/helpers/exam_registration_helper.rb
@@ -6,8 +6,4 @@ module ExamRegistrationHelper
       { icon: :info_circle, class: :info, t: :exam_registration_explanation_html }
     end
   end
-
-  def current_time_zone_html
-    %Q{(<span class="select-date-timezone">#{Organization.current.time_zone}</span>)}.html_safe
-  end
 end
