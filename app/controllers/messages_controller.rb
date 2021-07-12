4c295bb10675018f356e0e0872b2f8d400777f17
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-07-12 12:05:59 -0300

    Fix broken endpoint
    This 500 predates this PR

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < AjaxController
   before_action :set_exercise!, only: [:create, :read_messages]
 
   def index
-    render json: {has_messages: has_messages?, messages_count: messages_count}
+    pending_messages = current_user.unread_messages
+    render json: {has_messages: pending_messages.present?, messages_count: pending_messages.count}
   end
 
   def read_messages
