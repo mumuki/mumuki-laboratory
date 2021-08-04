c695e928e84bfa1a2b8455fee3990d882d95f65a
Federico Scarpa <fedescarpa@gmail.com>
2021-08-04 16:32:55 -0300

    Extracting to mail template

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/we_miss_you_notification
   def we_miss_you_reminder
-    UserMailer.we_miss_you_reminder(User.new, 1)
+    UserMailer.we_miss_you_reminder user, 1
   end
 
   def custom_content_plain_text_notification
@@ -21,8 +21,31 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   def certificate
-    certificate = Certificate.last
+    UserMailer.certificate new_certificate
+  end
 
-    UserMailer.certificate(certificate)
+  private
+
+  def user
+    User.new uid: 'some_user@gmail.com',
+             first_name: 'Some',
+             last_name: 'User',
+             verified_first_name: 'Jane',
+             verified_last_name: 'Doe',
+             last_organization: organization
+  end
+
+  def organization
+    Organization.central
+  end
+
+  def new_certificate
+    certificate_program = CertificateProgram.find_or_create_by!(title: 'Mumuki Certificate',
+                                                                description: 'Functional Programming',
+                                                                organization: organization,
+                                                                start_date: 1.minute.ago,
+                                                                end_date: 1.hour.since)
+
+    Certificate.new user: user, certificate_program:  certificate_program
   end
 end
