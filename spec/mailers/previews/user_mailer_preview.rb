09f12ace468fde789dd5a74170df31096779215f
Federico Scarpa <fedescarpa@gmail.com>
2021-08-05 12:26:14 -0300

    Improve notifications mailer preview

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -7,21 +7,15 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   def custom_content_plain_text_notification
-    notification = Notification.new user: user,
-                                    organization: organization,
-                                    subject: :custom,
-                                    custom_content_plain_text: 'Welcome to Mumuki!',
-                                    custom_title: 'Welcome!'
-
     UserMailer.notification notification
   end
 
   def custom_content_html_notification
-    #TODO
+    UserMailer.notification notification(custom_content_html: 'This is <em>the text</em> of the mail. <strong>Awesome!</strong>')
   end
 
-  def certificate
-    UserMailer.certificate new_certificate
+  def certificate_preview
+    UserMailer.certificate certificate
   end
 
   private
@@ -39,7 +33,7 @@ class UserMailerPreview < ActionMailer::Preview
     Organization.central
   end
 
-  def new_certificate
+  def certificate
     certificate_program = CertificateProgram.find_or_create_by!(title: 'Mumuki Certificate',
                                                                 description: 'Functional Programming',
                                                                 organization: organization,
@@ -47,5 +41,13 @@ class UserMailerPreview < ActionMailer::Preview
                                                                 end_date: 1.hour.since)
 
     Certificate.new user: user, certificate_program:  certificate_program
+  end
+
+  def notification(**hash)
+    Notification.new({user: user,
+                     organization: organization,
+                     subject: :custom,
+                     custom_content_plain_text: 'This is the text of the mail. Awesome!',
+                     custom_title: 'This is the title!'}.merge hash)
   end
 end
