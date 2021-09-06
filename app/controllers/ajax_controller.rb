8de2d9a495aa6cee2ca75f26109625f52b54160b
Federico Scarpa <fedescarpa@gmail.com>
2021-09-06 11:52:39 -0300

    Fix organization disabled from while page loaded

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,9 +1,14 @@
 class AjaxController < ApplicationController
   before_action :authenticate!
+  before_action :validate_organization_enabled!, on: :create
 
   private
 
   def authenticate!
     head 403 unless current_user?
+  end
+
+  def validate_organization_enabled!
+    Organization.current.validate_enabled!
   end
 end
