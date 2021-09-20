087b7801396b95095bc5108cc0263731ec63c69b
Federico Scarpa <fedescarpa@gmail.com>
2021-09-20 13:45:04 -0300

    Create events handlers

--- lib/mumuki/laboratory/events/events.rb
+++ lib/mumuki/laboratory/events/events.rb
@@ -3,4 +3,13 @@ Mumukit::Nuntius::EventConsumer.handle do
   event 'AssignmentManuallyEvaluated' do |payload|
     Assignment.evaluate_manually! payload.deep_symbolize_keys[:assignment]
   end
+
+  event 'CreateMassiveNotifications' do |payload|
+    body = payload.with_indifferent_access
+    Notification.create_massive_notifications_for!(body[:notification], body[:uids])
+  end
+
+  event 'SendNotificationEmail' do |payload|
+    Notification.notify_via_email! payload.with_indifferent_access[:notification_id]
+  end
 end
