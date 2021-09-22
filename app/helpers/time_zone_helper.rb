d447afec72b2cd2c818f9f44ea2d908c8b1e765c
Felipe Calvo <felipe@mumuki.org>
2021-09-22 16:33:51 -0300

    Add timezone-less local time helper

--- app/helpers/time_zone_helper.rb
+++ app/helpers/time_zone_helper.rb
@@ -1,5 +1,9 @@
 module TimeZoneHelper
   def local_time(time, time_zone = Time.zone.name)
-    "#{l(time.in_time_zone(time_zone), format: :long)} (#{time_zone})"
+    "#{local_time_without_time_zone(time, time_zone)} (#{time_zone})"
+  end
+
+  def local_time_without_time_zone(time, time_zone = Time.zone.name)
+    l(time.in_time_zone(time_zone), format: :long)
   end
 end
