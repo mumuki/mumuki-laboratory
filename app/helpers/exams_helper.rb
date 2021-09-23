9b709faaf6d378fa6c7e51bb07d9ed09fbc073cc
Felipe Calvo <felipe@mumuki.org>
2021-09-22 21:14:38 -0300

    Show exam's course on book view only if current user is teacher

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -1,9 +1,7 @@
 module ExamsHelper
   def exam_information_for(user, exam)
     %Q{
-      #{fa_icon('graduation-cap', class: 'fa-fw',
-                text: "<strong>#{t :course}:</strong> #{exam.course.canonical_code}".html_safe)}
-      <br>
+      #{course_information_for(user, exam)}
       #{fa_icon('calendar-alt', class: 'fa-fw',
                 text: "<strong>#{t :date_and_time}:</strong> #{local_time_without_time_zone(exam.start_time)} - #{local_time(exam.end_time)}".html_safe)}
       <br>
@@ -11,5 +9,15 @@ module ExamsHelper
                 text: "<strong>#{t :available_time}:</strong> #{t :time_in_minutes, time: exam.duration}".html_safe)}
       <br>
     }.html_safe
+  end
+
+  private
+
+  def course_information_for(user, exam)
+    if user.teacher_here?
+      "#{fa_icon('graduation-cap', class: 'fa-fw',
+                 text: "<strong>#{t :course}:</strong> #{exam.course.canonical_code}".html_safe)}
+      <br>"
+    end
   end
 end
