5a481b8492610e8fe6246754440082da12ec2457
Julián Berbel Alt <julian@mumuki.org>
2021-09-23 15:32:28 -0300

    Add options parameter to menu_item and menu_text_item

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -4,7 +4,7 @@ module NotificationsHelper
   end
 
   def notification_preview_item(icon, name, url, **translation_params)
-    menu_item icon, name, url, 'mu-notification-preview', **translation_params
+    menu_item icon, name, url, 'mu-notification-preview', translation_params
   end
 
   def notification_text_preview_item(icon, text, url)
