5440d1d4a7f4a1e13f85ef24084d4770ec68a77d
Federico Scarpa <fedescarpa@gmail.com>
2021-09-24 14:07:59 -0300

    Jobs now user CustomNotification model instead of Notification

--- lib/mumuki/laboratory/jobs/jobs.rb
+++ lib/mumuki/laboratory/jobs/jobs.rb
@@ -1,11 +1,12 @@
 Mumukit::Nuntius::JobConsumer.handle do
   # Emitted by assigment manual evaluation in classroom
-  job 'CreateMassiveNotifications' do |payload|
+  job 'CustomNotificationCreated' do |payload|
     body = payload.with_indifferent_access
-    Notification.create_massive_notifications_for!(body[:notification], body[:uids])
+    CustomNotification.notify_users_to_add!(body[:custom_notification_id], body[:uids])
   end
 
-  job 'SendNotificationEmail' do |payload|
-    Notification.notify_via_email! payload.with_indifferent_access[:notification_id]
+  job 'CustomNotificationUserAdded' do |payload|
+    body = payload.with_indifferent_access
+    CustomNotification.add_user_and_notify_via_email!(body[:custom_notification_id], body[:uid])
   end
 end
