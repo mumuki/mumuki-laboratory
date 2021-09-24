baad2fd9957651d302f87c8d7741b8dfe29fa670
Felipe Calvo <felipe@mumuki.org>
2021-09-24 13:22:29 -0300

    Extract date information on exam helper

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -2,9 +2,7 @@ module ExamsHelper
   def exam_information_for(user, exam)
     %Q{
       #{course_information_for(user, exam)}
-      #{fa_icon('calendar-alt', class: 'fa-fw',
-                text: "<strong>#{t :date_and_time}:</strong> #{local_time_without_time_zone(exam.start_time)} - #{local_time(exam.end_time)}".html_safe)}
-      <br>
+      #{date_information_for(exam)}
       #{duration_information_for(exam)}
     }.html_safe
   end
@@ -17,6 +15,12 @@ module ExamsHelper
                  text: "<strong>#{t :course}:</strong> #{exam.course.canonical_code}".html_safe)}
       <br>"
     end
+  end
+
+  def date_information_for(exam)
+    "#{fa_icon('calendar-alt', class: 'fa-fw',
+               text: "<strong>#{t :date_and_time}:</strong> #{local_time_without_time_zone(exam.start_time)} - #{local_time(exam.end_time)}".html_safe)}
+    <br>"
   end
 
   def duration_information_for(exam)
