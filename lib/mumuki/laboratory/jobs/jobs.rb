0a39cdf324338d78bded8621d6fa2afd78a62aa0
Federico Scarpa <fedescarpa@gmail.com>
2021-10-05 16:10:55 -0300

    Rename jobs handlers

--- lib/mumuki/laboratory/jobs/jobs.rb
+++ lib/mumuki/laboratory/jobs/jobs.rb
@@ -1,12 +1,12 @@
 Mumukit::Nuntius::JobConsumer.handle do
   # Emitted by assigment manual evaluation in classroom
-  job 'CustomNotificationCreated' do |payload|
+  job 'MassiveJobCreated' do |payload|
     body = payload.with_indifferent_access
-    CustomNotification.notify_users_to_add!(body[:custom_notification_id], body[:uids])
+    MassiveJob.find(body[:massive_job_id]).notify_users_to_add!(body[:uids])
   end
 
-  job 'CustomNotificationUserAdded' do |payload|
+  job 'UserAddedMassiveJob' do |payload|
     body = payload.with_indifferent_access
-    CustomNotification.add_user_and_notify_via_email!(body[:custom_notification_id], body[:uid])
+    MassiveJob.find(body[:massive_job_id]).process!(body[:uid])
   end
 end
