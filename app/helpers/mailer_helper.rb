285d4e7116f1b7349d5eade6e9a256ef60952b9b
Julián Berbel Alt <julian@mumuki.org>
2021-10-12 11:18:16 -0300

    Fix url generation for mailer

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,11 +1,5 @@
 module MailerHelper
   def delete_account_url_for(user)
-    delete_confirmation_user_url host: organic_domain_for(user), token: user.delete_account_token
-  end
-
-  private
-
-  def organic_domain_for(user)
-    Mumukit::Platform.laboratory.organic_domain(user.last_organization)
+    @organization.url_for("/user/delete_confirmation?token=#{user.delete_account_token}")
   end
 end
