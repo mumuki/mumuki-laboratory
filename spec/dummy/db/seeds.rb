f824e542a3f950fda83c844bfbd51723dd3c8bf0
Julián Berbel Alt <julian@mumuki.org>
2021-10-12 18:26:10 -0300

    Fix seeds to prevent breaking on devinit

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -9,22 +9,29 @@ Organization.find_or_create_by!(name: 'base') do |org|
   org.public = true
   org.login_methods = Mumukit::Login::Settings.login_methods
   org.locale = 'es'
+  org.time_zone = 'Buenos Aires'
 end
 
 Organization.find_or_create_by!(name: 'private') do |org|
-  org.book = Book.find_by!(slug: 'mumukiproject/mumuki-libro-aprende-a-programar-recorrido-extendido')
+  org.book = Book.find_by!(slug: 'mumuki/mumuki-libro-programacion')
   org.public = false
+  org.time_zone = 'Buenos Aires'
 end
 
-  Organization.find_or_create_by!(name: 'primaria') do |org|
-    org.book = Book.find_by!(slug: 'mumukiproject/mumuki-libro-primaria')
-    org.login_methods = Mumukit::Login::Settings.login_methods
-    org.public = true
-  end
+Organization.find_or_create_by!(name: 'primaria') do |org|
+  org.book = Book.find_by!(slug: 'mumukiproject/mumuki-libro-primaria')
+  org.login_methods = Mumukit::Login::Settings.login_methods
+  org.public = true
+  org.time_zone = 'Buenos Aires'
+end
 
-Organization.find_or_create_by!(name: 'central')
+Organization.find_or_create_by!(name: 'central') do |org|
+  org.book = Book.find_by!(slug: 'mumuki/mumuki-libro-programacion')
+  org.public = true
+  org.time_zone = 'Buenos Aires'
+end
 
-User.find_or_create_by!(uid: 'dev.student@mumuki.org') { |org| org.permissions = {student: 'central/*'} }
+User.find_or_create_by!(uid: 'dev.student@mumuki.org') { |org| org.permissions = {student: 'private/*'} }
 User.find_or_create_by!(uid: 'dev.teacher@mumuki.org') { |org| org.permissions = {teacher: 'private/*'} }
 User.find_or_create_by!(uid: 'dev.admin@mumuki.org') { |org| org.permissions = {admin: '*/*'} }
 User.find_or_create_by!(uid: 'dev.owner@mumuki.org') { |org| org.permissions = {owner: '*/*'} }
