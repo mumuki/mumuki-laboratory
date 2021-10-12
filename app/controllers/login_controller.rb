831f68fc900377a956614bd95853706f581196ad
Julián Berbel Alt <julian@mumuki.org>
2021-10-12 11:21:41 -0300

    Skip actions which require user to exist on login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,9 +2,12 @@ class LoginController < ApplicationController
   Mumukit::Login.configure_login_controller! self
 
   skip_before_action :verify_authenticity_token, if: lambda { Rails.env.development? }
-  skip_before_action :validate_user_profile!
-  skip_before_action :validate_accepted_role_terms!
-  skip_before_action :validate_active_organization!
+  skip_before_action :validate_user_profile!,
+                     :validate_accepted_role_terms!,
+                     :validate_active_organization!,
+                     :redirect_to_proper_context!,
+                     :ensure_restore_progress!,
+                     :visit_organization!
 
   private
 
