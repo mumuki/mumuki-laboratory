111e84f26b1c31dfac1ffe240e905387d760a242
Federico Scarpa <fedescarpa@gmail.com>
2021-10-15 14:09:49 -0300

    Use Class methods insted of find instance

--- lib/mumuki/laboratory/jobs/jobs.rb
+++ lib/mumuki/laboratory/jobs/jobs.rb
@@ -2,11 +2,11 @@ Mumukit::Nuntius::JobConsumer.handle do
   # Emitted by assigment manual evaluation in classroom
   job 'MassiveJobCreated' do |payload|
     body = payload.with_indifferent_access
-    MassiveJob.find(body[:massive_job_id]).notify_users_to_add!(body[:uids])
+    MassiveJob.notify_users_to_add!(body[:massive_job_id], body[:uids])
   end
 
   job 'UserAddedMassiveJob' do |payload|
     body = payload.with_indifferent_access
-    MassiveJob.find(body[:massive_job_id]).process!(body[:uid])
+    MassiveJob.process!(body[:massive_job_id], body[:uid])
   end
 end
