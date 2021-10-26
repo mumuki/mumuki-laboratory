9076c53d31731a8ec397c2a2684af31b5f589dfd
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-10-26 17:29:53 -0300

    Allow complements to work like lessons

--- app/controllers/complements_controller.rb
+++ app/controllers/complements_controller.rb
@@ -1,8 +1,17 @@
 class ComplementsController < GuideContainerController
+  include Mumuki::Laboratory::Controllers::ValidateAccessMode
 
   private
 
   def subject
     @complement ||= Complement.find_by(id: params[:id])
+  end
+
+  def authorization_minimum_role
+    :ex_student
+  end
+
+  def subject_container
+    subject.guide
   end
 end
