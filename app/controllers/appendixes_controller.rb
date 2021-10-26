f4b3ea93c7806e964cc1174b315f67b8c3a98b11
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-10-26 17:20:36 -0300

    Make method private

--- app/controllers/appendixes_controller.rb
+++ app/controllers/appendixes_controller.rb
@@ -1,9 +1,12 @@
 class AppendixesController < ApplicationController
-  def authorization_minimum_role
-    :ex_student
-  end
 
   def show
     @chapter = Chapter.find(params[:chapter_id])
+  end
+
+  private
+
+  def authorization_minimum_role
+    :ex_student
   end
 end
